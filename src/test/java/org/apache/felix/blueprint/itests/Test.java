ed6ed2ba0f6a7d6bc2374372535acadbff6709f0
Guillaume Nodet <gnodet@apache.org>
2009-04-13 18:49:20 +0000

    Reorganize to add integration tests, remove iPojo dep, add xbean-reflect dependency
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/sandbox/gnodet/blueprint/itests@764556 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/felix/blueprint/itests/Test.java
@@ -0,0 +1,61 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.felix.blueprint.itests;
+
+import org.apache.servicemix.kernel.testing.support.AbstractIntegrationTest;
+
+public class Test extends AbstractIntegrationTest {
+
+    public void test() throws Exception {
+    }
+
+    /**
+	 * The manifest to use for the "virtual bundle" created
+	 * out of the test classes and resources in this project
+	 *
+	 * This is actually the boilerplate manifest with one additional
+	 * import-package added. We should provide a simpler customization
+	 * point for such use cases that doesn't require duplication
+	 * of the entire manifest...
+	 */
+	protected String getManifestLocation() {
+		return "classpath:org/apache/felix/blueprint/MANIFEST.MF";
+	}
+
+	/**
+	 * The location of the packaged OSGi bundles to be installed
+	 * for this test. Values are Spring resource paths. The bundles
+	 * we want to use are part of the same multi-project maven
+	 * build as this project is. Hence we use the localMavenArtifact
+	 * helper method to find the bundles produced by the package
+	 * phase of the maven build (these tests will run after the
+	 * packaging phase, in the integration-test phase).
+	 *
+	 * JUnit, commons-logging, spring-core and the spring OSGi
+	 * test bundle are automatically included so do not need
+	 * to be specified here.
+	 */
+	protected String[] getTestBundlesNames() {
+        return new String[] {
+                getBundle("org.apache.xbean", "xbean-reflect"),
+                getBundle("org.apache.felix", "org.apache.felix.blueprint"),
+		};
+	}
+
+}
