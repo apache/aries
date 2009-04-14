f5b7f158c1dc06eeea8d2287e7d9a93f46e5b2ba
Jarek Gawor <gawor@apache.org>
2009-04-14 00:29:03 +0000

    copy blueprint api
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/sandbox/blueprint@764653 13f79535-47bb-0310-9956-ffa450edef68

---
+++ blueprint-api/src/main/java/org/osgi/service/blueprint/context/ModuleContext.java
@@ -0,0 +1,46 @@
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.osgi.service.blueprint.context;
+
+import java.util.Collection;
+import java.util.Set;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.blueprint.reflect.ComponentMetadata;
+
+public interface ModuleContext  {
+    
+   static final int CONFIGURATION_ADMIN_OBJECT_DELTED = 1;
+   
+   static final int BUNDLE_STOPPING = 2;
+
+   BundleContext getBundleContext();
+   
+   Object getComponent(String name) throws NoSuchComponentException;
+   
+   ComponentMetadata getComponentMetadata(String name) throws NoSuchComponentException;
+    
+   Set getComponentNames();
+   
+   Collection getExportedServicesMetadata();
+   
+   Collection getLocalComponentsMetadata();
+   
+   Collection getReferencedServicesMetadata();
+   
+   
+}
