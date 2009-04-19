a152ffb7a80fb0a4098d9d92249bb8a5f2feca0f
Guillaume Nodet <gnodet@apache.org>
2009-04-19 20:06:56 +0000

    Modify the author tags and remove a TODO
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/sandbox/blueprint@766507 13f79535-47bb-0310-9956-ffa450edef68

--- blueprint-core/src/main/java/org/apache/geronimo/blueprint/reflect/RegistrationListenerMetadataImpl.java
+++ blueprint-core/src/main/java/org/apache/geronimo/blueprint/reflect/RegistrationListenerMetadataImpl.java
@@ -24,7 +24,7 @@ import org.osgi.service.blueprint.reflect.Value;
 /**
  * TODO: javadoc
  *
- * @author <a href="mailto:dev@felix.apache.org">Apache Felix Project</a>
+ * @author <a href="mailto:dev@geronimo.apache.org">Apache Geronimo Project</a>
  * @version $Rev: 760378 $, $Date: 2009-03-31 11:31:38 +0200 (Tue, 31 Mar 2009) $
  */
 public class RegistrationListenerMetadataImpl implements RegistrationListenerMetadata {
