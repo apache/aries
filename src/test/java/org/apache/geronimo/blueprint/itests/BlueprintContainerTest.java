be013f0fcee2c0596b6b0dd08fc647afe3ce3a39
Guillaume Nodet <gnodet@apache.org>
2009-05-29 09:32:43 +0000

    Add support for optionally proxying classes and collection greedy proxying through the ext namespace, remove cglib from blueprint-bundle by default
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/sandbox/blueprint/blueprint-itests@779901 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/geronimo/blueprint/itests/BlueprintContainerTest.java
+++ src/test/java/org/apache/geronimo/blueprint/itests/BlueprintContainerTest.java
@@ -128,8 +128,7 @@ public class BlueprintContainerTest extends AbstractIntegrationTest {
             mavenBundle("org.apache.geronimo", "blueprint-bundle"),
             mavenBundle("org.apache.geronimo", "blueprint-sample").noStart(),
 
-            //vmOption("-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"),
-
+//            org.ops4j.pax.exam.container.def.PaxRunnerOptions.vmOption("-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"),
 
             felix(), equinox() //, knopflerfish()
         );
