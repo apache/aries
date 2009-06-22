603c40223cbfcc2a46ba0801556bdd7f50c58ed4
Guillaume Nodet <gnodet@apache.org>
2009-06-22 15:03:53 +0000

    Update API to use CollapsedType
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/sandbox/blueprint/blueprint-api@787264 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/osgi/service/blueprint/container/CollapsedType.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) OSGi Alliance (2008, 2009). All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.osgi.service.blueprint.container;
+
+/**
+ * Provides access to a concrete type and its optional generic type argument.
+ *
+ * A Collapsed Type converts a type declaration to a generalized form that is
+ * directly usable for conversion.It provides access to the concrete Class of
+ * the type as well of the optional type arguments.
+ *
+ * In Java 1.4, this class only works on non-generic types. In those cases, a
+ * Collapsed Type provides to the class and has no type arguments. Blueprint
+ * extender implementations can subclass this class and provide access to the
+ * generics type graph if used in a declaration. Such a subclass must
+ * <em>collapse<em> the different <code>Type</code> instances into the
+ * collapsed form. That is, a form where the raw Class is available with its optional type arguments.
+ *
+ *@Immutable
+ */
+
+public class CollapsedType {
+	final static CollapsedType ALL = new CollapsedType(Object.class);
+
+	private final Class clazz;
+
+	/**
+	 * Create a Collapsed Type for a normal Java class without any generics
+	 * information.
+	 *
+	 * @param clazz
+	 *            The class that is the collapsed type.
+	 */
+	public CollapsedType(Class clazz) {
+		this.clazz = clazz;
+	}
+
+	/**
+	 * Access to the raw class.
+	 *
+	 * The raw class represents the concrete class that is associated with a
+	 * type declaration. This class could have been deduced from the generics
+	 * type graph of the declaration. For example, in the following example:
+	 *
+	 * <pre>
+	 * Map&lt;String, Object&gt; map;
+	 * </pre>
+	 *
+	 * The raw class is the Map class.
+	 *
+	 * @return the collapsed raw class that represents this type.
+	 */
+	public Class getRawClass() {
+		return clazz;
+	}
+
+	/**
+	 * Access to a type argument.
+	 *
+	 * The type argument refers to a argument in a generic type declaration
+	 * given by index <code>i</code>. This method returns a Collapsed Type
+	 * that has Object as class when no generic type information is available.
+	 * Any object is assignable to Object and therefore no conversion is then
+	 * necessary, this is compatible with older Javas.
+	 *
+	 * This method should be overridden by a subclass that provides access to
+	 * the generic information.
+	 *
+	 * For example, in the following example:
+	 *
+	 * <pre>
+	 * Map&lt;String, Object&gt; map;
+	 * </pre>
+	 *
+	 * The type argument 0 is <code>String</code>, and type argument 1 is
+	 * <code>Object</code>.
+	 *
+	 * @param i
+	 *            The index of the type argument
+	 * @return A Collapsed Type that represents a type argument. If
+	 */
+	public CollapsedType getActualTypeArgument(int i) {
+		return ALL;
+	}
+
+    /**
+     * Number of type arguments.
+     *
+     * @return number of type arguments
+     */
+    public int size() {
+        return 0;
+    }
+
+}
--- src/main/java/org/osgi/service/blueprint/container/Converter.java
+++ src/main/java/org/osgi/service/blueprint/container/Converter.java
@@ -17,33 +17,38 @@ package org.osgi.service.blueprint.container;
 
 /**
  * Provides access to the type conversions (both predefined and user registered)
- * that are defined for the blueprint container.
+ * that are defined for the Blueprint Container.
  */
 public interface Converter {
 
 	/**
-	 * Check if the converter is able to convert the given value to the specified
-	 * type.
-	 * 
-	 * @param fromValue the object to be converted
-     * @param toType the type that the instance is to be converted to.
-     *        This will be either a Class object or a Type object.	 
-	 * @return <code>true</code> if the conversion is possible, <code>false</code> otherwise.
+	 * Check if the converter is able to convert the given value to the
+	 * specified type.
+	 *
+	 * @param s
+	 *            The source object to convert from
+	 * @param T
+	 *            The target type
+	 *
+	 * @return <code>true</code> if the conversion is possible,
+	 *         <code>false</code> otherwise.
 	 */
-	boolean canConvert(Object fromValue, Object toType);
+	boolean canConvert(Object s, CollapsedType T);
 
 	/**
-	 * Convert an object to an instance of the given class, using the built-in and 
-	 * user-registered type converters as necessary.
-	 * 
-	 * @param fromValue the object to be converted
-	 * @param toType the type that the instance is to be converted to.
-	 *        This will be either a Class object or a Type object.
+	 * Convert an object to an instance of the given class, using the built-in
+	 * and user-registered type converters as necessary.
+	 *
+	 * @param s
+	 *            The source object to convert from
+	 * @param T
+	 *            The target type
 	 * @return an instance of the class 'toType'
-	 * @throws Exception if the conversion cannot succeed. This exception is
-	 * checked because callers should expect that not all source objects
-	 * can be successfully converted.
+	 * @throws Exception
+	 *             if the conversion cannot succeed. This exception is checked
+	 *             because callers should expect that not all source objects can
+	 *             be successfully converted.
 	 */
-	Object convert(Object fromValue, Object toType) throws Exception;
-	
+	Object convert(Object s, CollapsedType T) throws Exception;
+
 }
