a40c8be5f05f32775e1f70560032ea83d3ba5fea
Guillaume Nodet <gnodet@apache.org>
2009-06-22 15:03:53 +0000

    Update API to use CollapsedType
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/sandbox/blueprint/blueprint-sample@787264 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/geronimo/blueprint/sample/DateTypeConverter.java
+++ src/main/java/org/apache/geronimo/blueprint/sample/DateTypeConverter.java
@@ -21,6 +21,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.osgi.service.blueprint.container.Converter;
+import org.osgi.service.blueprint.container.CollapsedType;
 
 public class DateTypeConverter implements Converter {
 
@@ -30,12 +31,12 @@ public class DateTypeConverter implements Converter {
         dateFormat = new SimpleDateFormat(format);
     }
     
-    public Object convert(Object source, Object toType) throws Exception {
+    public Object convert(Object source, CollapsedType toType) throws Exception {
         return dateFormat.parse(source.toString());
     }
 
-    public boolean canConvert(Object fromValue, Object toType) {
-        return Date.class.isAssignableFrom((Class) toType);
+    public boolean canConvert(Object fromValue, CollapsedType toType) {
+        return Date.class.isAssignableFrom(toType.getRawClass());
     }
 
 }
