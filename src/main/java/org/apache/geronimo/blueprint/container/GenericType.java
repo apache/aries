f1dec20a8c2e1f1c3492e065e3892db34fa23a28
Jarek Gawor <gawor@apache.org>
2009-06-26 00:55:59 +0000

    added a note on array handling that needs to be checked
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/sandbox/blueprint/blueprint-core@788556 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/geronimo/blueprint/container/GenericType.java
+++ src/main/java/org/apache/geronimo/blueprint/container/GenericType.java
@@ -1,3 +1,20 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.apache.geronimo.blueprint.container;
 
 import java.lang.reflect.Array;
@@ -14,6 +31,11 @@ import org.apache.geronimo.blueprint.di.ExecutionContext;
 import org.osgi.framework.Bundle;
 import org.osgi.service.blueprint.container.CollapsedType;
 
+/**
+ * XXXX: Currently, in case of arrays getActualTypeArgument(0) returns something similar to what
+ * Class.getComponentType() does for arrays.  I don't think this is quite right since getActualTypeArgument()
+ * should return the given parameterized type not the component type. Need to check this behavior with the spec.
+ */
 public class GenericType extends CollapsedType {
 
 	private static final GenericType[] EMPTY = new GenericType[0];
