3a8d64bba7b95974e3e7f7e5324526931c479dc9
Guillaume Nodet <gnodet@apache.org>
2009-07-27 13:51:49 +0000

    Quick revert of 797601
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/sandbox/blueprint@798132 13f79535-47bb-0310-9956-ffa450edef68

--- blueprint-core/src/main/java/org/apache/geronimo/blueprint/container/ServiceRecipe.java
+++ blueprint-core/src/main/java/org/apache/geronimo/blueprint/container/ServiceRecipe.java
@@ -131,9 +131,9 @@ public class ServiceRecipe extends AbstractRecipe {
         }
         ServiceRegistrationProxy proxy = new ServiceRegistrationProxy();
         addObject(proxy, true);
-        if (blueprintContainer.isServiceEnabled(this)) {
+        //if (blueprintContainer.isServiceEnabled(this)) {
             register();
-        }
+        //}
         internalGetService(null, null); // null bundle means we don't want to retrieve the actual service when used with a ServiceFactory
         return proxy;
     }
