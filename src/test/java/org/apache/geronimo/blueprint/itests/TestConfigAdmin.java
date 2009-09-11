1b213180a86a4fe564f584ab94f469d2f33f09f3
Guillaume Nodet <gnodet@apache.org>
2009-09-11 14:26:20 +0000

    Change artifactsIds to geronimo-blueprint(-api/-core/-cm), update README to use the just released Felix Framework 2.0.0, and bundles symbolic names to org.apache.geronimo.blueprint(.api/.core/.cm)
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/components/blueprint/trunk/blueprint-itests@813852 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/geronimo/blueprint/itests/TestConfigAdmin.java
+++ src/test/java/org/apache/geronimo/blueprint/itests/TestConfigAdmin.java
@@ -182,7 +182,7 @@ public class TestConfigAdmin extends AbstractIntegrationTest {
             systemProperty("org.ops4j.pax.logging.DefaultServiceLog.level").value("DEBUG"),
 
             // Bundles
-            mavenBundle("org.apache.geronimo.blueprint", "org.apache.geronimo.blueprint.bundle"),
+            mavenBundle("org.apache.geronimo.blueprint", "geronimo-blueprint"),
             mavenBundle("org.apache.geronimo.blueprint", "org.apache.geronimo.blueprint.sample").noStart(),
 
 //            org.ops4j.pax.exam.container.def.PaxRunnerOptions.vmOption("-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"),
--- src/test/java/org/apache/geronimo/blueprint/itests/TestReferences.java
+++ src/test/java/org/apache/geronimo/blueprint/itests/TestReferences.java
@@ -144,7 +144,7 @@ public class TestReferences extends AbstractIntegrationTest {
             systemProperty("org.ops4j.pax.logging.DefaultServiceLog.level").value("INFO"),
 
             // Bundles
-            mavenBundle("org.apache.geronimo.blueprint", "org.apache.geronimo.blueprint.bundle"),
+            mavenBundle("org.apache.geronimo.blueprint", "geronimo-blueprint"),
             mavenBundle("org.apache.geronimo.blueprint", "org.apache.geronimo.blueprint.sample"),
 
 //            org.ops4j.pax.exam.container.def.PaxRunnerOptions.vmOption("-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"),
