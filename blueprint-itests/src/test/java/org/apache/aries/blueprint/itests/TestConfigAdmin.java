68c765c2f555e47cce73ed82babee45959ce596b
Lin Sun <linsun@apache.org>
2009-12-01 03:24:08 +0000

    update blueprint jar file names per discussion on dev list
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk/blueprint@885662 13f79535-47bb-0310-9956-ffa450edef68

--- blueprint-itests/src/test/java/org/apache/aries/blueprint/itests/TestConfigAdmin.java
+++ blueprint-itests/src/test/java/org/apache/aries/blueprint/itests/TestConfigAdmin.java
@@ -182,7 +182,7 @@ public class TestConfigAdmin extends AbstractIntegrationTest {
             systemProperty("org.ops4j.pax.logging.DefaultServiceLog.level").value("DEBUG"),
 
             // Bundles
-            mavenBundle("org.apache.aries.blueprint", "aries-blueprint"),
+            mavenBundle("org.apache.aries.blueprint", "org.apache.aries.blueprint"),
             mavenBundle("org.apache.aries.blueprint", "org.apache.aries.blueprint.sample").noStart(),
             mavenBundle("org.osgi","org.osgi.compendium"),
 //            org.ops4j.pax.exam.container.def.PaxRunnerOptions.vmOption("-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"),
