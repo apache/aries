9d16c7edb3c3dc220898e50a6070ac4fd0b4c0d3
Mark Nuttall <mnuttall@apache.org>
2009-12-08 14:41:30 +0000

    ARIES-74: Extend Parser and ParserService to also accept an InputStream
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk@888411 13f79535-47bb-0310-9956-ffa450edef68

--- blueprint/blueprint-core/src/main/java/org/apache/aries/blueprint/container/ParserServiceImpl.java
+++ blueprint/blueprint-core/src/main/java/org/apache/aries/blueprint/container/ParserServiceImpl.java
@@ -1,7 +1,10 @@
 package org.apache.aries.blueprint.container;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -10,6 +13,7 @@ import org.apache.aries.blueprint.ParserService;
 import org.apache.aries.blueprint.container.NamespaceHandlerRegistry.NamespaceHandlerSet;
 import org.apache.aries.blueprint.namespace.ComponentDefinitionRegistryImpl;
 import org.osgi.framework.Bundle;
+import org.xml.sax.SAXException;
 
 public class ParserServiceImpl implements ParserService {
 
@@ -18,20 +22,48 @@ public class ParserServiceImpl implements ParserService {
 	public ParserServiceImpl (NamespaceHandlerRegistry nhr) { 
 		_namespaceHandlerRegistry = nhr;
 	}
-	public ComponentDefinitionRegistry parse(List<URL> urls, Bundle clientBundle) throws Exception {
-		return parse(urls, clientBundle, false);
+	
+	public ComponentDefinitionRegistry parse(URL url, Bundle clientBundle) throws Exception {
+    return parse (url, clientBundle, false);
+  }
+
+  public ComponentDefinitionRegistry parse(URL url, Bundle clientBundle, boolean validate)
+      throws Exception {
+    List<URL> urls = new ArrayList<URL>();
+    urls.add(url);
+    return parse (urls, clientBundle, validate);
+  }
+  
+  public ComponentDefinitionRegistry parse(List<URL> urls, Bundle clientBundle) throws Exception {
+    return parse(urls, clientBundle, false);
+  }
+  
+	public ComponentDefinitionRegistry parse(List<URL> urls, Bundle clientBundle, boolean validate) throws Exception {
+	  Parser parser = new Parser();   
+	  parser.parse(urls);
+	  return validateAndPopulate (parser, clientBundle, validate);
 	}
 	
-	public ComponentDefinitionRegistry parse(List<URL> urls, Bundle clientBundle, boolean validate) throws Exception {
-		Parser parser = new Parser();   
-		parser.parse(urls);
-		Set<URI> nsuris = parser.getNamespaces();
-		NamespaceHandlerSet nshandlers = _namespaceHandlerRegistry.getNamespaceHandlers(nsuris, clientBundle);
-		if (validate) { 
-		  parser.validate( nshandlers.getSchema());
-		}
-		ComponentDefinitionRegistry cdr = new ComponentDefinitionRegistryImpl();
-		parser.populate(nshandlers, cdr);
-		return cdr;		
-	}
+  public ComponentDefinitionRegistry parse(InputStream is, Bundle clientBundle) throws Exception {
+    return parse (is, clientBundle, false);
+  }
+  
+  public ComponentDefinitionRegistry parse(InputStream is, Bundle clientBundle, boolean validate) throws Exception {
+    Parser parser = new Parser();
+    parser.parse(is);
+    return validateAndPopulate (parser, clientBundle, validate);
+  }
+    
+  private ComponentDefinitionRegistry validateAndPopulate (Parser parser, Bundle clientBundle, boolean validate) 
+  throws IOException, SAXException { 
+    Set<URI> nsuris = parser.getNamespaces();
+    NamespaceHandlerSet nshandlers = _namespaceHandlerRegistry.getNamespaceHandlers(nsuris, clientBundle);
+    if (validate) { 
+      parser.validate( nshandlers.getSchema());
+    }
+    ComponentDefinitionRegistry cdr = new ComponentDefinitionRegistryImpl();
+    parser.populate(nshandlers, cdr);
+    return cdr;   
+  }
+
 }
