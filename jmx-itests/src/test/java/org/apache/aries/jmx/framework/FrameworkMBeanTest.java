9388c605ee5152c8e2f104ca16d842551d8aac7b
Adam Wojtuniak <awojtuniak@apache.org>
2009-12-14 23:17:15 +0000

    ARIES-30 initial integration tests for FrameworkMBean and the rest of MBeans.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk/jmx@890548 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jmx-itests/src/test/java/org/apache/aries/jmx/framework/FrameworkMBeanTest.java
@@ -0,0 +1,53 @@
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.aries.jmx.framework;
+
+import java.io.IOException;
+
+import javax.management.openmbean.CompositeData;
+
+import static junit.framework.Assert.*;
+
+import org.apache.aries.jmx.AbstractIntegrationTest;
+import org.apache.aries.jmx.codec.BatchActionResult;
+import org.junit.Assert;
+import org.junit.Test;
+import org.osgi.jmx.framework.FrameworkMBean;
+
+/**
+ * 
+ * 
+ * @version $Rev$ $Date$
+ */
+public class FrameworkMBeanTest extends AbstractIntegrationTest {
+
+    @Test
+    public void testSetBundleStartLevels() throws IOException {
+        FrameworkMBean framework = getMBean(FrameworkMBean.OBJECTNAME, FrameworkMBean.class);
+        assertNotNull(framework);
+        long[] bundleIds = new long[]{1,2};
+        int[] newlevels = new int[]{1,1};
+        CompositeData compData = framework.setBundleStartLevels(bundleIds, newlevels);
+        assertNotNull(compData);
+        BatchActionResult batch2 = BatchActionResult.from(compData);
+        Assert.assertNotNull(batch2.getCompleted());
+        Assert.assertTrue(batch2.isSuccess());
+        Assert.assertNull(batch2.getError());
+        Assert.assertNull(batch2.getRemainingItems());
+    }
+
+}
