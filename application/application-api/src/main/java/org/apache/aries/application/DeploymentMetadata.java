be8c48edb5fcf71584c3b63eef5ee16f00257a91
Mark Nuttall <mnuttall@apache.org>
2009-12-22 15:04:50 +0000

    ARIES-89: Implement application support: service interfaces
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk@893195 13f79535-47bb-0310-9956-ffa450edef68

---
+++ application/application-api/src/main/java/org/apache/aries/application/DeploymentMetadata.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIESOR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.aries.application;
+
+import java.util.List;
+
+import org.osgi.framework.Version;
+
+/**
+ * Represents the parsed contents of a DEPLOYMENT.MF file
+ *
+ */
+public interface DeploymentMetadata {
+
+  /**
+   * get the value of the Application-SymbolicName header
+   * @return the value of the Application-SymbolicName header
+   */
+  public String getApplicationSymbolicName();
+  
+  /**
+   * get the value of the Application-Version header
+   * @return the value of the Application-Version header
+   */
+  public Version getApplicationVersion();
+  
+  /**
+   * get the value of the Deployed-Content header 
+   * @return the list of the deployed content 
+   */
+  public List<DeploymentContent> getApplicationDeploymentContents();
+  
+  /**
+   * return the application this deployment metadata is associated with.
+   * 
+   * @return the application.
+   */
+  public ApplicationMetadata getApplicationMetadata();
+}
