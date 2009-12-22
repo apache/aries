a73aaae18921d99e49e7bb724b6cd1d42e8a7fdc
Ian Robinson <irobins@apache.org>
2009-12-22 12:02:12 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk@893150 13f79535-47bb-0310-9956-ffa450edef68

--- application/application-api/src/main/java/org/apache/aries/application/ApplicationMetadata.java
+++ application/application-api/src/main/java/org/apache/aries/application/ApplicationMetadata.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.aries.application;
+package org.apache.aries.application; 
 
 import java.util.List;
 
