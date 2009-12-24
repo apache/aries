aeb116f4843a749ca046a7bdb65389ca8d7527b4
Mark Nuttall <mnuttall@apache.org>
2009-12-24 12:04:51 +0000

    ARIES-89: Implement application support: BundleManifest unit test
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk@893735 13f79535-47bb-0310-9956-ffa450edef68

---
+++ application/application-utils/src/test/java/org/apache/aries/application/utils/manifest/test/BundleManifestTest.java
@@ -0,0 +1,89 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIESOR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.aries.application.utils.manifest.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.jar.JarInputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import org.apache.aries.application.utils.filesystem.IOUtils;
+import org.apache.aries.application.utils.manifest.BundleManifest;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class BundleManifestTest
+{
+  @BeforeClass
+  public static void setup() throws Exception
+  {
+    ZipOutputStream out = new ZipOutputStream(new FileOutputStream("../src/test/resources/bundles/nonExploded.jar"));
+    ZipEntry ze = new ZipEntry("META-INF/");
+    out.putNextEntry(ze);
+    
+    File f = new File("../src/test/resources/bundles/exploded.jar/META-INF/beforeManifest.file");
+    ze = new ZipEntry("META-INF/beforeManifest.file");
+    ze.setSize(f.length());
+    out.putNextEntry(ze);
+    IOUtils.copy(new FileInputStream(f), out);
+    
+    f = new File("../src/test/resources/bundles/exploded.jar/META-INF/MANIFEST.MF");
+    ze = new ZipEntry("META-INF/MANIFEST.MF");
+    ze.setSize(f.length());
+    out.putNextEntry(ze);
+    IOUtils.copy(new FileInputStream(f), out);    
+    
+    out.close();
+  }
+  
+  @AfterClass
+  public static void cleanup()
+  {
+    new File("../src/test/resources/bundles/nonExploded.jar").delete();
+  }
+  
+  @Test
+  public void testExploded()
+  {
+    BundleManifest sut = BundleManifest.fromBundle(new File("../src/test/resources/bundles/exploded.jar"));
+    assertEquals("com.ibm.test", sut.getSymbolicName());
+    assertEquals("1.0.0", sut.getVersion().toString());
+  }
+  
+  @Test
+  public void testZip() throws Exception
+  {
+    // make sure that the manifest is not the first file in the jar archive
+    JarInputStream jarIs = new JarInputStream(new FileInputStream("../src/test/resources/bundles/nonExploded.jar"));
+    assertNull(jarIs.getManifest());
+    jarIs.close();
+    
+    BundleManifest sut = BundleManifest.fromBundle(new File("../src/test/resources/bundles/nonExploded.jar"));
+    assertEquals("com.ibm.test", sut.getSymbolicName());
+    assertEquals("1.0.0", sut.getVersion().toString());
+  }
+}
+
