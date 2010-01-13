bf8ce4d0591f1975e46c377079c4b89796695826
Mark Nuttall <mnuttall@apache.org>
2010-01-13 17:24:34 +0000

    ARIES-89: Implement application support: Sketch out placeholders for runtime support
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk@898849 13f79535-47bb-0310-9956-ffa450edef68

---
+++ application/application-runtime/src/main/java/org/apache/aries/application/runtime/ApplicationContextImpl.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIESOR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.aries.application.runtime;
+
+import java.util.Set;
+
+import org.apache.aries.application.management.ApplicationContext;
+import org.apache.aries.application.management.AriesApplication;
+import org.osgi.framework.Bundle;
+
+public class ApplicationContextImpl implements ApplicationContext {
+
+  public AriesApplication getApplication() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public Set<Bundle> getApplicationContent() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public ApplicationState getApplicationState() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public void start() {
+    // TODO Auto-generated method stub
+    
+  }
+
+  public void stop() {
+    // TODO Auto-generated method stub
+    
+  } 
+  
+}
