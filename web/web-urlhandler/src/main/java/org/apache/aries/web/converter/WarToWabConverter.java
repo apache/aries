23bfa8f004eb7dd6589476f8c8322e5da823572a
Valentin Mahrwald <mahrwald@apache.org>
2010-01-17 13:30:26 +0000

    ARIES-115 Add a bit of javadoc
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk@900136 13f79535-47bb-0310-9956-ffa450edef68

--- web/web-urlhandler/src/main/java/org/apache/aries/web/converter/WarToWabConverter.java
+++ web/web-urlhandler/src/main/java/org/apache/aries/web/converter/WarToWabConverter.java
@@ -23,16 +23,27 @@ import java.io.InputStream;
 import java.util.Properties;
 import java.util.jar.Manifest;
 
+/**
+ * Service interface for WAR to WAB conversion
+ */
 public interface WarToWabConverter {
+  /**
+   * Support class for WabConverter to allow multiple passes over an input war
+   * archive without requiring in-memory buffering.
+   */
   public static interface InputStreamProvider {
     InputStream getInputStream() throws IOException;
   }
 
+  public static final String WEB_CONTEXT_PATH = "Web-ContextPath";
+  public static final String WEB_JSP_EXTRACT_LOCATION = "Web-JSPExtractLocation";
+
+  
   /**
-   * Generate the new manifest for the 
+   * Generate the new manifest for the converted war file.
    * @param input
    * @param name The name of the war file
-   * @param properties Properties to influence the conversion as defined in RFC66
+   * @param properties Properties to influence the conversion as defined in RFC66 (see also {@link #convert} method)
    * @return
    */
   Manifest generateManifest(InputStreamProvider input, String name, Properties properties) throws IOException;
@@ -42,7 +53,21 @@ public interface WarToWabConverter {
    * and has the new manifest.
    * @param input
    * @param name The name of the war file
-   * @param properties Properties to influence the conversion as defined in RFC66
+   * @param properties Properties to influence the conversion as defined in RFC66. The following
+   * properties are supported
+   * <ul>
+   *    <li>Bundle-ClassPath</li>
+   *    <li>Bundle-ManifestVersion</li>
+   *    <li>Bundle-SymbolicName</li>
+   *    <li>Bundle-Version</li>
+   *    <li>Import-Package</li>
+   *    <li>Web-ContextPath</li>
+   *    <li>Web-JSPExtractLocation</li>
+   * </ul>
+   * Except for Bundle-ClassPath and Import-Package any supplied properties will
+   * overwrite values specified in an existing bundle manifest. For Bundle-ClassPath and Import-Package 
+   * the supplied values will be joined to those specified in a bundle manifest
+   * (if it exists) and also the results of the scan of the WAR file.
    * @return
    */
   InputStream convert(InputStreamProvider input, String name, Properties properties) throws IOException;
