554ffe2771a36758d9142d0e01cc85535b51d053
Timothy James Ward <timothyjward@apache.org>
2010-01-22 14:32:42 +0000

    ARIES-79: Create and manage EntityManagerFactory services for persistence units
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk/jpa/jpa-container-testbundle@902098 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/aries/jpa/container/itest/entities/Car.java
@@ -0,0 +1,50 @@
+package org.apache.aries.jpa.container.itest.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Car {
+  @Id
+  private String numberPlate;
+  
+  private String colour;
+  
+  private int engineSize;
+  
+  private int numberOfSeats;
+
+  public String getNumberPlate() {
+    return numberPlate;
+  }
+
+  public void setNumberPlate(String numberPlate) {
+    this.numberPlate = numberPlate;
+  }
+
+  public String getColour() {
+    return colour;
+  }
+
+  public void setColour(String colour) {
+    this.colour = colour;
+  }
+
+  public int getEngineSize() {
+    return engineSize;
+  }
+
+  public void setEngineSize(int engineSize) {
+    this.engineSize = engineSize;
+  }
+
+  public int getNumberOfSeats() {
+    return numberOfSeats;
+  }
+
+  public void setNumberOfSeats(int numberOfSeats) {
+    this.numberOfSeats = numberOfSeats;
+  }
+  
+  
+}
