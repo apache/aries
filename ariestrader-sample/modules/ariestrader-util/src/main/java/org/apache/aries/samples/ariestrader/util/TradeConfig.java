863e294523cd358e36538edfc21dab0f455ddee5
Joseph Alan Bohn <jbohn@apache.org>
2010-02-05 18:22:17 +0000

    add some new constants for osgi
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk@907035 13f79535-47bb-0310-9956-ffa450edef68

--- ariestrader-sample/modules/ariestrader-util/src/main/java/org/apache/aries/samples/ariestrader/util/TradeConfig.java
+++ ariestrader-sample/modules/ariestrader-util/src/main/java/org/apache/aries/samples/ariestrader/util/TradeConfig.java
@@ -91,6 +91,10 @@ public class TradeConfig {
 	public static String JDBC_PWD = null;
 	public static String DS_NAME = "java:comp/env/jdbc/TradeDataSource";
 
+        /* OSGi specific parameters */
+	public static String OSGI_SERVICE_PREFIX = "aries:services/";
+	public static String OSGI_DS_NAME_FILTER = "(osgi.jndi.serviceName=jdbc/TradeDataSource)";
+
 	/*Trade SOAP specific parameters */
 	private static String SoapURL =
 		"http://localhost:8080/ariestrader/services/TradeWSServices";
