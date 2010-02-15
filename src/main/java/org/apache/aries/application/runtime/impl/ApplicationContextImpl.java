d556afb8349b382fd0f4c3248860b4641b235ddc
Alasdair Nottingham <not@apache.org>
2010-02-15 16:50:42 +0000

    ARIES-162 Do bundle install as a result of installing an app, rather than as a result of starting an app.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk/application/application-runtime@910261 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/aries/application/runtime/impl/ApplicationContextImpl.java
+++ src/main/java/org/apache/aries/application/runtime/impl/ApplicationContextImpl.java
@@ -19,17 +19,26 @@
 
 package org.apache.aries.application.runtime.impl;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.aries.application.DeploymentContent;
+import org.apache.aries.application.DeploymentMetadata;
 import org.apache.aries.application.management.ApplicationContext;
 import org.apache.aries.application.management.AriesApplication;
+import org.apache.aries.application.management.AriesApplicationResolver;
 import org.apache.aries.application.management.BundleInfo;
+import org.apache.aries.application.management.ManagementException;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleException;
+import org.osgi.framework.ServiceException;
+import org.osgi.framework.ServiceReference;
+import org.osgi.framework.Version;
 
 public class ApplicationContextImpl implements ApplicationContext {
   
@@ -38,10 +47,63 @@ public class ApplicationContextImpl implements ApplicationContext {
   private ApplicationState _state;
   private BundleContext _bundleContext;
   
-  public ApplicationContextImpl (BundleContext b, AriesApplication app) { 
+  public ApplicationContextImpl (BundleContext b, AriesApplication app) throws BundleException, ManagementException { 
     _bundleContext = b;
     _application = app;
     _bundles = new HashMap<BundleInfo, Bundle>();
+    
+    DeploymentMetadata meta = _application.getDeploymentMetadata();
+    
+    AriesApplicationResolver resolver = null;
+    
+    ServiceReference ref = b.getServiceReference(AriesApplicationResolver.class.getName());
+
+    if (ref != null) resolver = (AriesApplicationResolver) b.getService(ref);
+    
+    if (resolver == null) {
+      throw new ManagementException(new ServiceException(AriesApplicationResolver.class.getName(), ServiceException.UNREGISTERED));
+    }
+    
+    try {
+      for (DeploymentContent content : meta.getApplicationDeploymentContents()) {
+        String bundleSymbolicName = content.getContentName();
+        Version bundleVersion = content.getExactVersion();
+        
+        BundleInfo bundleInfo = null;
+        
+        for (BundleInfo info : _application.getBundleInfo()) {
+          if (info.getSymbolicName().equals(bundleSymbolicName) &&
+              info.getVersion().equals(bundleVersion)) {
+            bundleInfo = info;
+            break;
+          }
+        }
+        
+        if (bundleInfo == null) {
+          // call out to the bundle repository.
+          bundleInfo = resolver.getBundleInfo(bundleSymbolicName, bundleVersion);
+        }
+        
+        if (bundleInfo == null) {
+          throw new ManagementException("Cound not find bundles: " + bundleSymbolicName + "_" + bundleVersion);
+        }
+        
+        Bundle bundle = _bundleContext.installBundle(bundleInfo.getLocation());
+        
+        _bundles.put(bundleInfo, bundle);
+      }
+    } catch (BundleException be) {
+      for (Bundle bundle : _bundles.values()) {
+        bundle.uninstall();
+      }
+      
+      _bundles.clear();
+      
+      throw be;
+    } finally {
+      if (resolver != null) b.ungetService(ref);
+    }
+    
     _state = ApplicationState.INSTALLED;
   }
 
@@ -61,20 +123,32 @@ public class ApplicationContextImpl implements ApplicationContext {
     return _state;
   }
 
-  public void start() throws BundleException {
-    Set<BundleInfo> bundleInfo = _application.getBundleInfo();
-    for (BundleInfo bi : bundleInfo) { 
-      // TODO: proper synchronisation!
-      if (_bundles.containsKey(bi)) { 
-        Bundle b = _bundles.get(bi);
+  public void start() throws BundleException 
+  {
+    _state = ApplicationState.STARTING;
+    
+    List<Bundle> bundlesWeStarted = new ArrayList<Bundle>();
+    
+    try {
+      for (Bundle b : _bundles.values()) { 
         if (b.getState() != Bundle.ACTIVE) { 
-          b.start();
+          b.start(Bundle.START_ACTIVATION_POLICY);
+          bundlesWeStarted.add(b);
         }
-      } else { 
-        Bundle b = _bundleContext.installBundle(bi.getLocation()); 
-        b.start();
-        _bundles.put(bi, b);
       }
+    } catch (BundleException be) {
+      for (Bundle b : bundlesWeStarted) {
+        try {
+          b.stop();
+        } catch (BundleException be2) {
+          // we are doing tidyup here, so we don't want to replace the bundle exception
+          // that occurred during start with one from stop. We also want to try to stop
+          // all the bundles we started even if some bundles wouldn't stop.
+        }
+      }
+      
+      _state = ApplicationState.INSTALLED;
+      throw be;
     }
     _state = ApplicationState.ACTIVE;
   }
