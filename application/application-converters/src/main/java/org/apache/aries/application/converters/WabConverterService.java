7a75a0bcf7d6126f2987429e241445361be575a2
Valentin Mahrwald <mahrwald@apache.org>
2010-02-18 18:28:49 +0000

    ARIES-115 Log conversion exception and don't try to convert things that are not wars
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk@911511 13f79535-47bb-0310-9956-ffa450edef68

--- application/application-converters/src/main/java/org/apache/aries/application/converters/WabConverterService.java
+++ application/application-converters/src/main/java/org/apache/aries/application/converters/WabConverterService.java
@@ -27,29 +27,37 @@ import org.apache.aries.application.filesystem.IFile;
 import org.apache.aries.application.management.BundleConverter;
 import org.apache.aries.web.converter.WarToWabConverter;
 import org.apache.aries.web.converter.WarToWabConverter.InputStreamProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class WabConverterService implements BundleConverter {
-  private WarToWabConverter wabConverter;
-  
-  public WarToWabConverter getWabConverter() {
-    return wabConverter;
-  }
+    private static final String WAR_FILE_EXTENSION = ".war";
+    private static final Logger LOGGER = LoggerFactory.getLogger(WabConverterService.class);
+    
+    private WarToWabConverter wabConverter;
 
-  public void setWabConverter(WarToWabConverter wabConverter) {
-    this.wabConverter = wabConverter;
-  }
-
-  public InputStream convert(IDirectory parentEba, final IFile toBeConverted) {
-    try {
-      return wabConverter.convert(new InputStreamProvider() {
-        public InputStream getInputStream() throws IOException {
-          return toBeConverted.open();
-        }
-      }, toBeConverted.getName(), new Properties());
-    } catch (IOException e) {
-      // TODO what to do with the Exception
-      return null;
+    public WarToWabConverter getWabConverter() {
+        return wabConverter;
     }
-  }
 
+    public void setWabConverter(WarToWabConverter wabConverter) {
+        this.wabConverter = wabConverter;
+    }
+
+    public InputStream convert(IDirectory parentEba, final IFile toBeConverted) {
+        if (toBeConverted.getName().endsWith(WAR_FILE_EXTENSION)) {
+            try {
+                return wabConverter.convert(new InputStreamProvider() {
+                    public InputStream getInputStream() throws IOException {
+                        return toBeConverted.open();
+                    }
+                }, toBeConverted.getName(), new Properties());
+            } catch (IOException e) {
+                LOGGER.error("Encountered an exception while converting " + toBeConverted.getName() 
+                        + " in " + parentEba.getName(), e);
+            }
+        }
+
+        return null;
+    }
 }
