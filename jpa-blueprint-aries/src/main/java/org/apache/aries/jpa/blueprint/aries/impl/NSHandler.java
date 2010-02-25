ae9bd845b52bfbfbeca2bdd576869c5f55cb1723
Timothy James Ward <timothyjward@apache.org>
2010-02-25 10:55:36 +0000

    ARIES-202 : Separate the JPA blueprint integration from managed persistence contexts
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk/jpa@916245 13f79535-47bb-0310-9956-ffa450edef68

--- jpa-blueprint-aries/src/main/java/org/apache/aries/jpa/blueprint/aries/impl/NSHandler.java
+++ jpa-blueprint-aries/src/main/java/org/apache/aries/jpa/blueprint/aries/impl/NSHandler.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.aries.jpa.container.context.namespace;
+package org.apache.aries.jpa.blueprint.aries.impl;
 
 import java.net.URL;
 import java.util.ArrayList;
