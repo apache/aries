1d75ee54b7738b7065715646549d508892f6f85b
Alasdair Nottingham <not@apache.org>
2010-03-01 23:53:18 +0000

    ARIES-128 Implement OSGi spec url lookup scheme
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk/jndi/jndi-url@917809 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/aries/jndi/url/OsgiNameParser.java
@@ -0,0 +1,79 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.aries.jndi.url;
+
+import javax.naming.InvalidNameException;
+import javax.naming.Name;
+import javax.naming.NameParser;
+import javax.naming.NamingException;
+
+/**
+ * A parser for the aries namespace
+ */
+public final class OsgiNameParser implements NameParser
+{
+  private static final String OSGI_SCHEME = "osgi";
+  private static final String ARIES_SCHEME = "aries";
+  private static final String SERVICE_PATH = "service";
+  private static final String SERVICES_PATH = "services";
+  private static final String SERVICE_LIST_PATH = "servicelist";
+  private static final String FRAMEWORK_PATH = "framework";
+  
+  public Name parse(String name) throws NamingException
+  {
+    OsgiName result = new OsgiName(name);
+    
+    if (result.size() < 2) throw new InvalidNameException(name);
+    
+    String urlScheme = result.getScheme();
+    String schemePath = result.getSchemePath();
+    
+    if (!!!(OSGI_SCHEME.equals(urlScheme) || ARIES_SCHEME.equals(urlScheme))) {
+      throw new InvalidNameException(name);
+    }
+    
+    if (ARIES_SCHEME.equals(urlScheme) && !!!SERVICES_PATH.equals(schemePath)) {
+      throw new InvalidNameException(name);
+    }
+    
+    if (OSGI_SCHEME.equals(urlScheme) && !!!( SERVICE_PATH.equals(schemePath) || 
+                                              SERVICE_LIST_PATH.equals(schemePath) || 
+                                              FRAMEWORK_PATH.equals(schemePath))) {
+      throw new InvalidNameException(name);
+    }
+    
+    if (result.getInterface() == null || result.getInterface().length() == 0) {
+      throw new InvalidNameException(name);
+    }
+    
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object other)
+  {
+    return other instanceof OsgiNameParser;
+  }
+  
+  @Override
+  public int hashCode()
+  {
+    return 100003;
+  }
+}
---
+++ src/test/java/org/apache/aries/jndi/url/OsgiNameParserTest.java
@@ -0,0 +1,144 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.aries.jndi.url;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import javax.naming.InvalidNameException;
+import javax.naming.NameParser;
+import javax.naming.NamingException;
+
+import org.junit.Test;
+
+/**
+ * This is where we test the service registry name parser.
+ */
+public class OsgiNameParserTest
+{
+  /** The parser we are going to use for testing */
+  private NameParser parser = new OsgiNameParser();
+
+  /**
+   * OK, so we check that we can call checkNames multiple times.
+   * @throws NamingException
+   */
+  @Test
+  public void checkValidNames() throws NamingException
+  {
+    checkName("aries","services","java.lang.Runnable","(a=b)");
+    checkName("aries","services","java.lang.Runnable");
+    checkName("osgi","service","java.lang.Runnable");
+    checkName("osgi","service","java.lang.Runnable", "(a=b)");
+    checkName("osgi","servicelist","java.lang.Runnable");
+    checkName("osgi","servicelist","java.lang.Runnable", "(a=b)");
+    checkName("osgi","servicelist","jdbc", "grok", "DataSource");
+    checkName("osgi", "framework", "bundleContext");
+  }
+  
+  /**
+   * Make sure it fails if we try to parse something that isn't in aries:services
+   * @throws NamingException
+   */
+  @Test(expected=InvalidNameException.class)
+  public void checkOutsideNamespace() throws NamingException
+  {
+    checkName("java","comp","env","jms","cf");
+  }
+  
+  /**
+   * Check that it fails if no interface name is provided.
+   * @throws NamingException
+   */
+  @Test(expected=InvalidNameException.class)
+  public void checkMissingInterface() throws NamingException
+  {
+    checkName("aries","services");
+  }
+  
+  /**
+   * Check that it fails if no interface name is provided in a subtly different
+   * way from the previous method.
+   * @throws NamingException
+   */
+  @Test(expected=InvalidNameException.class)
+  public void checkMissingInterface2() throws NamingException
+  {
+    checkName("aries","services", "");
+  }
+  
+  @Test(expected=InvalidNameException.class)
+  public void checkIncorrectPath() throws NamingException
+  {
+    checkName("osgi", "services", "java.lang.Runnable"); 
+  }
+  
+  @Test(expected=InvalidNameException.class)
+  public void checkIllegalPath() throws NamingException
+  {
+    checkName("osgi", "wibble", "java.lang.Runnable"); 
+  }
+  
+  private void checkName(String scheme, String path, String ... elements)
+    throws NamingException
+  {
+    StringBuilder builder = new StringBuilder();
+    StringBuilder serviceName = new StringBuilder();
+    
+    builder.append(scheme);
+    builder.append(':');
+    builder.append(path);
+
+    if (elements.length > 0) {
+      builder.append('/');
+      
+      for (String element : elements) {
+        serviceName.append(element);
+        serviceName.append('/');
+      }
+  
+      serviceName.deleteCharAt(serviceName.length() - 1);
+      
+      builder.append(serviceName);
+    }
+    
+    OsgiName n = (OsgiName) parser.parse(builder.toString());
+    
+    assertEquals(scheme, n.getScheme());
+    assertEquals(path, n.getSchemePath());
+    
+    if (elements.length > 1) {
+      assertEquals(elements[0], n.getInterface());
+    }
+    
+    if (elements.length == 1) {
+      assertFalse(n.hasFilter());
+    }
+    
+    if (elements.length > 2) {
+      if (elements.length == 2) {
+        assertTrue(n.hasFilter());
+        assertEquals(elements[1], n.getFilter());
+      } else assertFalse(n.hasFilter());
+    }
+    
+    assertEquals(serviceName.toString(), n.getServiceName());
+  }
+}
