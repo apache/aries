b4a0a211ee3ec41a3cda430431b5c78581dab34d
Alasdair Nottingham <not@apache.org>
2010-03-03 22:15:28 +0000

    ARIES-174 Deliver some itests for the OBR resolver. These tests check that we can resolve based on internal, and external bundles, and pull in transitive dependencies when resolving.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk/application/application-itests@918735 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/aries/application/runtime/itests/BlogSampleInstalledUsingOBRResolverTest.java
@@ -0,0 +1,225 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.aries.application.runtime.itests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.ops4j.pax.exam.CoreOptions.equinox;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.CoreOptions.systemProperty;
+import static org.ops4j.pax.exam.CoreOptions.waitForFrameworkStartup;
+import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.vmOption;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.lang.reflect.Constructor;
+import java.net.URL;
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.apache.aries.application.ApplicationMetadataFactory;
+import org.apache.aries.application.DeploymentContent;
+import org.apache.aries.application.DeploymentMetadata;
+import org.apache.aries.application.management.ApplicationContext;
+import org.apache.aries.application.management.AriesApplication;
+import org.apache.aries.application.management.AriesApplicationManager;
+import org.apache.aries.application.management.BundleInfo;
+import org.apache.aries.application.resolver.obr.generator.RepositoryDescriptorGenerator;
+import org.apache.aries.application.utils.filesystem.FileSystem;
+import org.apache.aries.application.utils.manifest.BundleManifest;
+import org.apache.aries.samples.blog.api.BloggingService;
+import org.apache.aries.unittest.fixture.ArchiveFixture;
+import org.apache.aries.unittest.fixture.ArchiveFixture.ZipFixture;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleEvent;
+import org.osgi.framework.BundleListener;
+import org.osgi.service.obr.Repository;
+import org.osgi.service.obr.RepositoryAdmin;
+import org.w3c.dom.Document;
+
+@RunWith(JUnit4TestRunner.class)
+public class BlogSampleInstalledUsingOBRResolverTest extends AbstractIntegrationTest 
+{
+  /* Use @Before not @BeforeClass so as to ensure that these resources
+   * are created in the paxweb temp directory, and not in the svn tree
+   */
+  @Before
+  public static void createApplications() throws Exception 
+  {
+    System.out.println("creating eba " + new Date());
+    ZipFixture testEba = ArchiveFixture.newZip()
+     .binary("META-INF/APPLICATION.MF",
+        BlogSampleInstalledUsingOBRResolverTest.class.getClassLoader().getResourceAsStream("blog/APPLICATION.MF"))
+        .end()
+      .binary("org.apache.aries.samples.blog.jar", new URL("mvn:org.apache.aries.samples/blog").openStream()).end()
+      .binary("org.apache.derby.jar", new URL("mvn:org.apache.derby/derby").openStream()).end();
+
+    FileOutputStream fout = new FileOutputStream("blog.eba");
+    testEba.writeOut(fout);
+    fout.close();
+    System.out.println("created eba  " + new Date());
+  }
+
+  @Test
+  public void testBlogApp() throws Exception 
+  {
+    generateOBRRepoXML();
+    
+    RepositoryAdmin repositoryAdmin = getOsgiService(RepositoryAdmin.class);
+    
+    Repository[] repos = repositoryAdmin.listRepositories();
+    for (Repository repo : repos) {
+      repositoryAdmin.removeRepository(repo.getURL());
+    }
+    
+    repositoryAdmin.addRepository(new File("repository.xml").toURI().toURL());
+
+    AriesApplicationManager manager = getOsgiService(AriesApplicationManager.class);
+    AriesApplication app = manager.createApplication(FileSystem.getFSRoot(new File("blog.eba")));
+    //installing requires a valid url for the bundle in repository.xml.
+    
+    app = manager.resolve(app);
+    
+    DeploymentMetadata depMeta = app.getDeploymentMetadata();
+    
+    List<DeploymentContent> provision = depMeta.getApplicationProvisionBundles();
+    
+    assertEquals(provision.toString(), 2, provision.size());
+    
+    List<String> bundleSymbolicNames = new ArrayList<String>();
+    
+    for (DeploymentContent dep : provision) {
+      bundleSymbolicNames.add(dep.getContentName());
+    }
+    
+    assertTrue("Bundle org.apache.aries.samples.blog-api not found.", bundleSymbolicNames.contains("org.apache.aries.samples.blog-api"));
+    
+    bundleContext.addBundleListener(new BundleListener() {
+      public void bundleChanged(BundleEvent event)
+      {
+        System.out.println("Bundle " + event.getBundle().getSymbolicName() + " " + event.getType() + " at " + new Date());
+      }
+    });
+    
+    System.out.println("about to install " + new Date());
+    ApplicationContext ctx = manager.install(app);
+    System.out.println("about to start   " + new Date());
+    ctx.start();
+    System.out.println("started          " + new Date());
+
+    Set<Bundle> bundles = ctx.getApplicationContent();
+    
+    assertEquals("Number of bundles provisioned in the app", 4, bundles.size());
+    
+    BloggingService service = getOsgiService(BloggingService.class);
+    
+    assertNotNull("Found the blogging service", service);
+    
+    ctx.stop();
+    manager.uninstall(ctx);
+  }
+
+
+  private void generateOBRRepoXML() throws Exception
+  {
+    Set<BundleInfo> bundles = new HashSet<BundleInfo>();
+    
+    bundles.add(createBundleInfo("mvn:org.apache.aries.samples/blog-persistence"));
+    bundles.add(createBundleInfo("mvn:org.apache.aries.samples/blog-api"));
+//    bundles.add(createBundleInfo("mvn:org.apache.derby/derby"));
+    
+    Document doc = RepositoryDescriptorGenerator.generateRepositoryDescriptor("Blog sample description", bundles);
+    
+    FileOutputStream fout = new FileOutputStream("repository.xml");
+    
+    TransformerFactory.newInstance().newTransformer().transform(new DOMSource(doc), new StreamResult(fout));
+    
+    fout.close();
+    
+    TransformerFactory.newInstance().newTransformer().transform(new DOMSource(doc), new StreamResult(System.out));
+  }
+
+  private BundleInfo createBundleInfo(String urlToBundle) throws Exception
+  {
+    ApplicationMetadataFactory factory = getOsgiService(ApplicationMetadataFactory.class);
+    
+    Bundle b = getBundle("org.apache.aries.application.management");
+    @SuppressWarnings("unchecked")
+    Class<BundleInfo> clazz = b.loadClass("org.apache.aries.application.management.impl.BundleInfoImpl");
+    Constructor<BundleInfo> c = clazz.getConstructor(ApplicationMetadataFactory.class, BundleManifest.class, String.class);
+    
+    return c.newInstance(factory, BundleManifest.fromBundle(new URL(urlToBundle).openStream()), urlToBundle);
+  }
+
+  @org.ops4j.pax.exam.junit.Configuration
+  public static Option[] configuration() {
+    Option[] options = options(
+        // Log
+        mavenBundle("org.ops4j.pax.logging", "pax-logging-api"),
+        mavenBundle("org.ops4j.pax.logging", "pax-logging-service"),
+        // Felix Config Admin
+        mavenBundle("org.apache.felix", "org.apache.felix.configadmin"),
+        // Felix mvn url handler
+        mavenBundle("org.ops4j.pax.url", "pax-url-mvn"),
+
+        // this is how you set the default log level when using pax
+        // logging (logProfile)
+        systemProperty("org.ops4j.pax.logging.DefaultServiceLog.level").value("DEBUG"),
+
+        // Bundles
+        mavenBundle("org.apache.aries.application", "org.apache.aries.application.api"),
+        mavenBundle("org.apache.aries.application", "org.apache.aries.application.utils"),
+        mavenBundle("org.apache.aries.application", "org.apache.aries.application.management"),
+        mavenBundle("org.apache.aries.application", "org.apache.aries.application.runtime"),
+        mavenBundle("org.apache.aries.application", "org.apache.aries.application.resolver.obr"),
+        mavenBundle("org.apache.felix", "org.apache.felix.bundlerepository"),
+        mavenBundle("org.apache.aries.application", "org.apache.aries.application.runtime.itest.interfaces"),
+        mavenBundle("org.apache.aries", "org.apache.aries.util"),
+        mavenBundle("org.apache.aries.blueprint", "org.apache.aries.blueprint"),
+        mavenBundle("org.osgi", "org.osgi.compendium"),
+        mavenBundle("org.apache.aries.testsupport", "org.apache.aries.testsupport.unit"),
+
+        /* For debugging, uncomment the next two lines */
+//        vmOption ("-Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=7777"),
+//        waitForFrameworkStartup(),
+
+        /* For debugging, uncomment the next two lines
+        and add these imports:
+        import static org.ops4j.pax.exam.CoreOptions.waitForFrameworkStartup;
+        import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.vmOption;
+        */
+
+        equinox().version("3.5.0"));
+    options = updateOptions(options);
+    return options;
+  }
+}
