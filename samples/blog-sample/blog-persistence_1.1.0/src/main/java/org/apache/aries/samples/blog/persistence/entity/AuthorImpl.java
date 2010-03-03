5eeafa4249c9c9d98520cf39b014400e05293f86
Zoe Slattery <zoe@apache.org>
2010-03-03 12:52:03 +0000

    ARIES 199 - Not complete yet as the blog-assembly pom needs updating. This is just the persistence code which relies on JPA.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk@918451 13f79535-47bb-0310-9956-ffa450edef68

---
+++ samples/blog-sample/blog-persistence_1.1.0/src/main/java/org/apache/aries/samples/blog/persistence/entity/AuthorImpl.java
@@ -0,0 +1,143 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.aries.samples.blog.persistence.entity;
+
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+import javax.persistence.Table;
+
+import org.apache.aries.samples.blog.persistence.api.Author;
+
+/**
+ * This class represents a blog post Author
+ */
+
+@Entity(name = "AUTHOR")
+@Table(name = "AUTHOR")
+public class AuthorImpl implements Author
+{
+  /** The author's email address */
+  @Id
+  @Column(nullable = false, unique = true)
+  private String email;
+  
+  /** The author's full name */
+  private String name;
+  /** The display name for this author */
+  private String displayName;
+  /** A short bio for this author */
+  private String bio;  
+  /** The Author's date of birth */
+  private Date dob;
+
+  /** The blog entries posted by this user */
+  @OneToMany(cascade = {CascadeType.REMOVE}, fetch = FetchType.EAGER)
+  @OrderBy("publishDate DESC")
+  private List<EntryImpl> posts;
+
+  /** Get the author's email address */
+  public String getEmail()
+  {
+    return email;
+  }
+
+  /** Get the author's full name */
+  public String getName()
+  {
+    return name;
+  } 
+  
+  /** Get the author's displayed name */
+  public String getDisplayName()
+  {
+    return displayName;
+  }
+
+  /** Get the author's biographical information */
+  public String getBio()
+  {
+    return bio;
+  }
+
+  /** Get the author's date of birth */
+  public Date getDob()
+  {
+    return dob;
+  } 
+
+  /** Get the author's blog posts */
+  public List<EntryImpl> getEntries()
+  {
+    return posts;
+  }
+  
+  // Set methods are not defined in the interface
+  
+  /** Set the author's email address */
+  public void setEmail(String email)
+  {
+    this.email = email;
+  }
+  
+  /** Set the author's full name */
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+  
+  /** Set the author's displayed name */
+  public void setDisplayName(String displayName)
+  {
+    this.displayName = displayName;
+  }
+  
+  /** Set the author's biographical information */
+  public void setBio(String bio)
+  {
+    this.bio = bio;
+  }
+  
+  /** Set the author's date of birth */
+  public void setDob(Date dob)
+  {
+    this.dob = dob;
+  }
+
+  /** Update  the author's blog posts */
+  public void updateEntries(EntryImpl b)
+  {
+    this.posts.add(b);
+  }
+  
+  /** set  the author's blog posts */
+  public void setEntries(List<EntryImpl> lb)
+  {
+    this.posts = lb;
+  }
+  
+}
