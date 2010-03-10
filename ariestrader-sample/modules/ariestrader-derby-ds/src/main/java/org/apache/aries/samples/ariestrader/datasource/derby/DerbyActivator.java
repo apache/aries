98519faff0f9ac0222c4cab9a50805aac876a2e8
Joseph Alan Bohn <jbohn@apache.org>
2010-03-10 23:13:04 +0000

    ARIES-240 first pass at changing group and artifact names for AriesTrader
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk/samples@921619 13f79535-47bb-0310-9956-ffa450edef68

--- ariestrader-sample/modules/ariestrader-derby-ds/src/main/java/org/apache/aries/samples/ariestrader/datasource/derby/DerbyActivator.java
+++ ariestrader-sample/modules/ariestrader-derby-ds/src/main/java/org/apache/aries/samples/ariestrader/datasource/derby/DerbyActivator.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.aries.ariestrader.datasource.derby;
+package org.apache.aries.samples.ariestrader.datasource.derby;
 
 import java.sql.Driver;
 import java.sql.DriverManager;
