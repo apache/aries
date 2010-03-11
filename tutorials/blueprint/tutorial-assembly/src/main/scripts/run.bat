13def9a62aefde22f92e94c781b2af68efb5d553
Zoe Slattery <zoe@apache.org>
2010-03-11 11:17:15 +0000

    ARIES-226 Update the run scripts to pick up changed diretory names
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk@921802 13f79535-47bb-0310-9956-ffa450edef68

--- tutorials/blueprint/tutorial-assembly/src/main/scripts/run.bat
+++ tutorials/blueprint/tutorial-assembly/src/main/scripts/run.bat
@@ -1,7 +1,7 @@
 setlocal 
 cd %~dp0
-copy ..\source\greeter-api\target\*.jar ..\dropins
+copy ..\source\org.apache.aries.tutorials.blueprint.greeter.api\target\*.jar ..\dropins
 
-copy ..\source\greeter-server-osgi\target\*.jar ..\dropins
+copy ..\source\org.apache.aries.tutorials.blueprint.greeter.server.osgi\target\*.jar ..\dropins
 
-copy ..\source\greeter-client-osgi\target\*.jar ..\dropins
+copy ..\source\org.apache.aries.tutorials.blueprint.greeter.client.osgi\target\*.jar ..\dropins
--- tutorials/blueprint/tutorial-assembly/src/main/scripts/run.sh
+++ tutorials/blueprint/tutorial-assembly/src/main/scripts/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 scriptDir=`dirname $0`
-cp "${scriptDir}/../source/greeter-api/target/"*.jar "${scriptDir}/../dropins"
-cp "${scriptDir}/../source/greeter-server-osgi/target/"*.jar "${scriptDir}/../dropins"
-cp "${scriptDir}/../source/greeter-client-osgi/target/"*.jar "${scriptDir}/../dropins"
+cp "${scriptDir}/../source/org.apache.aries.tutorials.blueprint.greeter.api/target/"*.jar "${scriptDir}/../dropins"
+cp "${scriptDir}/../source/org.apache.aries.tutorials.blueprint.greeter.server.osgi/target/"*.jar "${scriptDir}/../dropins"
+cp "${scriptDir}/../source/org.apache.aries.tutorials.blueprint.greeter.client.osgi/target/"*.jar "${scriptDir}/../dropins"
