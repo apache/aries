2d1cb0c17efc8c7fdcd53ef22469f9ccdbb884bd
Jarek Gawor <gawor@apache.org>
2010-03-12 18:30:33 +0000

    ARIES-238: move NoOpResolver and DefaultLocalPlatform to application-utils
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk@922381 13f79535-47bb-0310-9956-ffa450edef68

--- application/application-utils/src/main/java/org/apache/aries/application/utils/management/DefaultLocalPlatform.java
+++ application/application-utils/src/main/java/org/apache/aries/application/utils/management/DefaultLocalPlatform.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.aries.application.runtime.defaults;
+package org.apache.aries.application.utils.management;
 
 import java.io.File;
 import java.io.IOException;
