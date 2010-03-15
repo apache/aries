87a8fd1f2d4a74a019ae45befe66938ec1dc2995
Alasdair Nottingham <not@apache.org>
2010-03-15 13:40:54 +0000

    ARIES-238 Reverting the move of the DefaultLocalPlatform and NoOpResolver.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk@923221 13f79535-47bb-0310-9956-ffa450edef68

--- application/application-runtime/src/main/java/org/apache/aries/application/runtime/defaults/DefaultLocalPlatform.java
+++ application/application-runtime/src/main/java/org/apache/aries/application/runtime/defaults/DefaultLocalPlatform.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.aries.application.utils.management;
+package org.apache.aries.application.runtime.defaults;
 
 import java.io.File;
 import java.io.IOException;
