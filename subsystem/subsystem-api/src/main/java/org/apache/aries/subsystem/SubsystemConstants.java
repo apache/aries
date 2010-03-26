f56ac94ee4526af95011bfc4d300e8da17c6f66e
Guillaume Nodet <gnodet@apache.org>
2010-03-26 14:23:50 +0000

    [subsystem] initial commit of an api for discussion
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk@927864 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subsystem/subsystem-api/src/main/java/org/apache/aries/subsystem/SubsystemConstants.java
@@ -0,0 +1,41 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.aries.subsystem;
+
+public interface SubsystemConstants {
+
+    String SUBSYSTEM_MANIFESTVERSION = "Subsystem-ManifestVersion";
+    String SUBSYSTEM_SYMBOLICNAME = "Subsystem-SymbolicName";
+    String SUBSYSTEM_VERSION = "Subsystem-Version";
+    String SUBSYSTEM_NAME = "Subsystem-Name";
+    String SUBSYSTEM_DESCRIPTION = "Subsystem-Description";
+    String SUBSYSTEM_CONTENT = "Subsystem-Content";
+    String SUBSYSTEM_IMPORTPACKAGE = "Subsystem-ImportPackage";
+    String SUBSYSTEM_EXPORTPACKAGE = "Subsystem-ExportPackage";
+    String SUBSYSTEM_IMPORTSERVICE = "Subsystem-ImportService";
+    String SUBSYSTEM_EXPORTSERVICE = "Subsystem-ExportService";
+    String SUBSYSTEM_REQUIREBUNDLE = "Subsystem-RequireBundle";
+    String SUBSYSTEM_LOCALIZATION = "Subsystem-Localization";
+
+    /*
+    String APPLICATION_SYMBOLICNAME = "Application-SymbolicName";
+    String APPLICATION_VERSION = "Application-Version";
+    String APPLICATION_NAME = "Application-Name";
+
+    String LIBRARY_SYMBOLICNAME = "Library-SymbolicName";
+    String LIBRARY_VERSION = "Library-Version";
+    String LIBRARY_NAME = "Library-Name";
+    */
+
+}
