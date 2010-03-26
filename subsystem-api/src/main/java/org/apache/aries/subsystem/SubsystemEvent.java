a611ec851de3a51ce418ed56f5832898dce6a6c3
Guillaume Nodet <gnodet@apache.org>
2010-03-26 14:23:50 +0000

    [subsystem] initial commit of an api for discussion
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk/subsystem@927864 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subsystem-api/src/main/java/org/apache/aries/subsystem/SubsystemEvent.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.aries.subsystem;
+
+import java.util.EventObject;
+
+public class SubsystemEvent extends EventObject {
+
+    public enum Type {
+        Installed,
+        Started,
+        Stopped,
+        Updated,
+        Uninstalled,
+        Resolved,
+        Starting,
+        Stopping
+    }
+    private final Type type;
+
+    private final long timestamp;
+
+    private final Subsystem subsystem;
+
+    public SubsystemEvent(Type type, long timestamp, Subsystem subsystem) {
+        super(subsystem);
+        this.type = type;
+        this.timestamp = timestamp;
+        this.subsystem = subsystem;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public Subsystem getSubsystem() {
+        return subsystem;
+    }
+
+}
