7b1a22bdef6e6ce646a53de2a0dbf2b514cc7b02
Valentin Mahrwald <mahrwald@apache.org>
2010-03-27 10:02:17 +0000

    ARIES-249 More javadoc: NamespaceHandler and ParserContext
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk/blueprint/blueprint-core@928179 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/aries/blueprint/ParserContext.java
+++ src/main/java/org/apache/aries/blueprint/ParserContext.java
@@ -22,15 +22,48 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
 public interface ParserContext  {
-    
+    /**
+     * Returns the DOM Node that was passed to the NamespaceHandler call for which
+     * this ParserContext instance was created.
+     */
     Node getSourceNode();
 
     ComponentDefinitionRegistry getComponentDefinitionRegistry();
     
+    /**
+     * Retrieve the <code>ComponentMetadata</code> of the component that
+     * encloses the current <code>Node</code> that is to be parsed by a 
+     * namespace handler.
+     * 
+     * In case of top-level components this method will return <code>null</code>.
+     * @returns the enclosing component's metadata or null if there is no enclosing component
+     */
     ComponentMetadata getEnclosingComponent();
     
+    /**
+     * Create a new metadata instance of the given type. The returned
+     * object will also implement the appropriate <code>MutableComponentMetadata</code>
+     * interface, so as to allow the caller to set the properties of the 
+     * metadata.
+     *
+     * Note that the returned object may not be initialised, so callers
+     * should take care to assure every property needed by the blueprint
+     * extender is set.
+     *
+     * @param type the class of the Metadata object to create
+     * @param <T> The expected Metadata type to be created
+     * @return a new instance
+     */
     <T extends Metadata> T createMetadata(Class<T> type);
 
+    /**
+     * Invoke the blueprint parser to parse a DOM element.
+     * @param type the class of the Metadata type to be parsed
+     * @param enclosingComponent The component metadata that contains the Element
+     * to be parsed
+     * @param element The DOM element that is to be parsed
+     * @param <T> The expected metadata type to be parsed
+     */
     <T> T parseElement(Class<T> type, ComponentMetadata enclosingComponent, Element element);
 
     /** 
@@ -39,10 +72,19 @@ public interface ParserContext  {
      */
     String generateId();
     
+    /**
+     * Get the default activation setting for the current blueprint file
+     */
     String getDefaultActivation();
     
+    /**
+     * Get the default availability setting for the current blueprint file
+     */
     String getDefaultAvailability();
     
+    /**
+     * Get the default timeout setting for the current blueprint file
+     */
     String getDefaultTimeout();
 }
 
