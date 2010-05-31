2b4c46b99817618438a4c02587b2443fc61c2386
Zoe Slattery <zoe@apache.org>
2010-05-31 14:53:44 +0000

    ARIES-313 Clean up JDBCwithEBA test, remove plain JDBC and JPA tests, add JPAWithEBA test
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk@949759 13f79535-47bb-0310-9956-ffa450edef68

--- samples/blog/blog-itests/src/test/java/org/apache/aries/samples/blog/itests/JdbcBlogSampleWithEbaTest.java
+++ samples/blog/blog-itests/src/test/java/org/apache/aries/samples/blog/itests/JdbcBlogSampleWithEbaTest.java
@@ -51,49 +51,45 @@ public class JdbcBlogSampleWithEbaTest extends AbstractIntegrationTest {
         AriesApplicationContext ctx = manager.install(app);
         ctx.start();
 
+		/* Check that the Blog Sample bundles are present an started */
         Bundle bapi = getInstalledBundle("org.apache.aries.samples.blog.api");
         assertNotNull(bapi);
-        bapi.start();
+		assertEquals(bapi.ACTIVE, bapi.getState());
 
         Bundle bweb = getInstalledBundle("org.apache.aries.samples.blog.web");
         assertNotNull(bweb);
-        bweb.start();
 
         Bundle bbiz = getInstalledBundle("org.apache.aries.samples.blog.biz");
         assertNotNull(bbiz);
-        bbiz.start();
+		assertEquals(bbiz.ACTIVE, bbiz.getState());
 
         Bundle bper = getInstalledBundle("org.apache.aries.samples.blog.persistence.jdbc");
         assertNotNull(bper);
-        bper.start();
+		assertEquals(bper.ACTIVE, bper.getState());
 
-        Bundle txs = getInstalledBundle("org.apache.aries.transaction.manager");
+        /* Require services from biz and persistence - wait for them to register */
+        waitForServices(bbiz, "org.apache.aries.samples.blog.api.BloggingService");
+        waitForServices(bper, "org.apache.aries.samples.blog.api.persistence.BlogPersistenceService");
 
-        waitForServices(bbiz, "org.apache.aries.samples.api.BloggingService");
-        waitForServices(bper, "org.apache.aries.samples.api.BloggingService");
-        waitForServices(txs, "javax.transaction.TransactionManager");
+       /*Check that they haven't timed out trying to register*/
+    	assertTrue("No services reistered for " + bbiz.getSymbolicName(), isServiceRegistered(bbiz));
+    	assertTrue("No services reistered for " + bper.getSymbolicName(), isServiceRegistered(bper));
 
-        if (!isServiceRegistered(bbiz)) {
-            System.out.println("ZZZZZ No services registered for "
-                    + bbiz.getSymbolicName());
-        }
+		/*Check what services are registered - uncomment for additional debug */
+		/*
+    	listBundleServices(bbiz);
+    	listBundleServices(bper);
 
-        if (!isServiceRegistered(bper)) {
-            System.out.println("ZZZZZ No services registered for "
-                    + bper.getSymbolicName());
-        }
-
-        if (!isServiceRegistered(txs)) {
-            System.out.println("ZZZZZ No services registered for "
-                    + txs.getSymbolicName());
-        }
-
-        System.out.println("ZZZZZ in test and trying to get connection....");
+    	System.out.println("In test and trying to get connection....");
+		*/
         HttpURLConnection conn = makeConnection("http://localhost:8080/org.apache.aries.samples.blog.web/ViewBlog");
         String response = getHTTPResponse(conn);
+		/* Uncomment for additional debug */
+		/*
         System.out.println("ZZZZZ " + response);
         System.out.println("ZZZZZ " + conn.getResponseCode());
         System.out.println("ZZZZZ " + HttpURLConnection.HTTP_OK);
+		*/
 
         assertEquals(HttpURLConnection.HTTP_OK, conn.getResponseCode());
 
@@ -106,7 +102,7 @@ public class JdbcBlogSampleWithEbaTest extends AbstractIntegrationTest {
 
     @org.ops4j.pax.exam.junit.Configuration
     public static Option[] configuration() {
-        Option[] options = options(
+    	Option[] options = options(
                 bootDelegationPackages("javax.transaction",
                         "javax.transaction.*"),
                 vmOption("-Dorg.osgi.framework.system.packages=javax.accessibility,javax.activation,javax.activity,javax.annotation,javax.annotation.processing,javax.crypto,javax.crypto.interfaces,javax.crypto.spec,javax.imageio,javax.imageio.event,javax.imageio.metadata,javax.imageio.plugins.bmp,javax.imageio.plugins.jpeg,javax.imageio.spi,javax.imageio.stream,javax.jws,javax.jws.soap,javax.lang.model,javax.lang.model.element,javax.lang.model.type,javax.lang.model.util,javax.management,javax.management.loading,javax.management.modelmbean,javax.management.monitor,javax.management.openmbean,javax.management.relation,javax.management.remote,javax.management.remote.rmi,javax.management.timer,javax.naming,javax.naming.directory,javax.naming.event,javax.naming.ldap,javax.naming.spi,javax.net,javax.net.ssl,javax.print,javax.print.attribute,javax.print.attribute.standard,javax.print.event,javax.rmi,javax.rmi.CORBA,javax.rmi.ssl,javax.script,javax.security.auth,javax.security.auth.callback,javax.security.auth.kerberos,javax.security.auth.login,javax.security.auth.spi,javax.security.auth.x500,javax.security.cert,javax.security.sasl,javax.sound.midi,javax.sound.midi.spi,javax.sound.sampled,javax.sound.sampled.spi,javax.sql,javax.sql.rowset,javax.sql.rowset.serial,javax.sql.rowset.spi,javax.swing,javax.swing.border,javax.swing.colorchooser,javax.swing.event,javax.swing.filechooser,javax.swing.plaf,javax.swing.plaf.basic,javax.swing.plaf.metal,javax.swing.plaf.multi,javax.swing.plaf.synth,javax.swing.table,javax.swing.text,javax.swing.text.html,javax.swing.text.html.parser,javax.swing.text.rtf,javax.swing.tree,javax.swing.undo,javax.tools,javax.xml,javax.xml.bind,javax.xml.bind.annotation,javax.xml.bind.annotation.adapters,javax.xml.bind.attachment,javax.xml.bind.helpers,javax.xml.bind.util,javax.xml.crypto,javax.xml.crypto.dom,javax.xml.crypto.dsig,javax.xml.crypto.dsig.dom,javax.xml.crypto.dsig.keyinfo,javax.xml.crypto.dsig.spec,javax.xml.datatype,javax.xml.namespace,javax.xml.parsers,javax.xml.soap,javax.xml.stream,javax.xml.stream.events,javax.xml.stream.util,javax.xml.transform,javax.xml.transform.dom,javax.xml.transform.sax,javax.xml.transform.stax,javax.xml.transform.stream,javax.xml.validation,javax.xml.ws,javax.xml.ws.handler,javax.xml.ws.handler.soap,javax.xml.ws.http,javax.xml.ws.soap,javax.xml.ws.spi,javax.xml.xpath,org.ietf.jgss,org.omg.CORBA,org.omg.CORBA.DynAnyPackage,org.omg.CORBA.ORBPackage,org.omg.CORBA.TypeCodePackage,org.omg.CORBA.portable,org.omg.CORBA_2_3,org.omg.CORBA_2_3.portable,org.omg.CosNaming,org.omg.CosNaming.NamingContextExtPackage,org.omg.CosNaming.NamingContextPackage,org.omg.Dynamic,org.omg.DynamicAny,org.omg.DynamicAny.DynAnyFactoryPackage,org.omg.DynamicAny.DynAnyPackage,org.omg.IOP,org.omg.IOP.CodecFactoryPackage,org.omg.IOP.CodecPackage,org.omg.Messaging,org.omg.PortableInterceptor,org.omg.PortableInterceptor.ORBInitInfoPackage,org.omg.PortableServer,org.omg.PortableServer.CurrentPackage,org.omg.PortableServer.POAManagerPackage,org.omg.PortableServer.POAPackage,org.omg.PortableServer.ServantLocatorPackage,org.omg.PortableServer.portable,org.omg.SendingContext,org.omg.stub.java.rmi,org.w3c.dom,org.w3c.dom.bootstrap,org.w3c.dom.css,org.w3c.dom.events,org.w3c.dom.html,org.w3c.dom.ls,org.w3c.dom.ranges,org.w3c.dom.stylesheets,org.w3c.dom.traversal,org.w3c.dom.views,org.xml.sax,org.xml.sax.ext,org.xml.sax.helpers,javax.transaction;partial=true;mandatory:=partial,javax.transaction.xa;partial=true;mandatory:=partial"),
@@ -147,6 +143,7 @@ public class JdbcBlogSampleWithEbaTest extends AbstractIntegrationTest {
                 mavenBundle("org.apache.aries.application", "org.apache.aries.application.management" ),
                 mavenBundle("org.apache.aries.application", "org.apache.aries.application.runtime" ),
                 mavenBundle("org.apache.aries.application", "org.apache.aries.application.utils" ),
+                mavenBundle("org.apache.felix", "org.apache.felix.fileinstall" ),
                 mavenBundle("org.apache.aries.jpa", "org.apache.aries.jpa.api" ),
                 mavenBundle("org.apache.aries.jpa", "org.apache.aries.jpa.container" ),
                 mavenBundle("org.apache.aries.jpa", "org.apache.aries.jpa.blueprint.aries" ),
