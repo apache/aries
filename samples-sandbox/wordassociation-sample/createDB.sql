83a59ecca3048c0fdcba7fd56d6460372f15f6eb
Zoe Slattery <zoe@apache.org>
2010-06-10 09:34:30 +0000

    ARIES-334 Moving directories around
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk@953262 13f79535-47bb-0310-9956-ffa450edef68

--- samples-sandbox/wordassociation-sample/createDB.sql
+++
@@ -1,4 +0,0 @@
-connect 'jdbc:derby:wordassociationDB/;create=true';
-CREATE TABLE ASSOCIATION (word VARCHAR(255) NOT NULL, associated VARCHAR(255), PRIMARY KEY (word));
-DELETE FROM ASSOCIATION;
-exit;
