d8995fd31a64532240f523cfa5979a59aa221996
Valentin Mahrwald <mahrwald@apache.org>
2010-06-14 12:32:07 +0000

    ARIES-336: Add do-priv around appropriate Bundle.loadClass
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk/jpa@954428 13f79535-47bb-0310-9956-ffa450edef68

--- jpa-container/src/main/java/org/apache/aries/jpa/container/unit/impl/BundleDelegatingClassLoader.java
+++ jpa-container/src/main/java/org/apache/aries/jpa/container/unit/impl/BundleDelegatingClassLoader.java
@@ -20,6 +20,12 @@ package org.apache.aries.jpa.container.unit.impl;
 
 import java.io.IOException;
 import java.net.URL;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+import java.security.PrivilegedActionException;
+import java.security.PrivilegedExceptionAction;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Enumeration;
 
 import org.osgi.framework.Bundle;
@@ -36,19 +42,57 @@ public class BundleDelegatingClassLoader extends ClassLoader {
   }
   
   @Override
-  protected Class<?> findClass(String className) throws ClassNotFoundException {
-    return bundle.loadClass(className);
+  protected Class<?> findClass(final String name) throws ClassNotFoundException {
+    try {
+      return AccessController.doPrivileged(new PrivilegedExceptionAction<Class<?>>() {
+        public Class<?> run() throws ClassNotFoundException 
+        {
+          return bundle.loadClass(name);
+        }
+      });
+    } catch (PrivilegedActionException e) {
+      Exception cause = e.getException();
+        
+      if (cause instanceof ClassNotFoundException) throw (ClassNotFoundException)cause;
+      else throw (RuntimeException)cause;
+    }
   }
 
   @Override
-  protected URL findResource(String resName) {
-    return bundle.getResource(resName);
+  protected URL findResource(final String name) {
+    return AccessController.doPrivileged(new PrivilegedAction<URL>() {
+      public URL run()
+      {
+        return bundle.getResource(name);
+      }
+    });
   }
 
   @SuppressWarnings("unchecked")
   @Override
-  protected Enumeration<URL> findResources(String resName) throws IOException {
-    return bundle.getResources(resName);
+  protected Enumeration<URL> findResources(final String name) throws IOException {
+    Enumeration<URL> urls;
+    try {
+      urls =  AccessController.doPrivileged(new PrivilegedExceptionAction<Enumeration<URL>>() {
+        @SuppressWarnings("unchecked")
+        public Enumeration<URL> run() throws IOException
+        {
+          return (Enumeration<URL>)bundle.getResources(name);
+        }
+        
+      });
+    } catch (PrivilegedActionException e) {
+      Exception cause = e.getException();
+      
+      if (cause instanceof IOException) throw (IOException)cause;
+      else throw (RuntimeException)cause;
+    }
+    
+    if (urls == null) {
+      urls = Collections.enumeration(new ArrayList<URL>());
+    }
+    
+    return urls;
   }
 
 }
