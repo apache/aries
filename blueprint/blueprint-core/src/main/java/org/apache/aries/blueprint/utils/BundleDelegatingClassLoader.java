1a56ba3852e9dc7587f753174c4444351d52cdcd
Valentin Mahrwald <mahrwald@apache.org>
2010-06-14 12:32:07 +0000

    ARIES-336: Add do-priv around appropriate Bundle.loadClass
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk@954428 13f79535-47bb-0310-9956-ffa450edef68

--- blueprint/blueprint-core/src/main/java/org/apache/aries/blueprint/utils/BundleDelegatingClassLoader.java
+++ blueprint/blueprint-core/src/main/java/org/apache/aries/blueprint/utils/BundleDelegatingClassLoader.java
@@ -20,6 +20,12 @@ package org.apache.aries.blueprint.utils;
 
 import java.io.IOException;
 import java.net.URL;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+import java.security.PrivilegedActionException;
+import java.security.PrivilegedExceptionAction;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Enumeration;
 
 import org.osgi.framework.Bundle;
@@ -43,20 +49,59 @@ public class BundleDelegatingClassLoader extends ClassLoader {
         this.classLoader = classLoader;
     }
 
-    protected Class findClass(String name) throws ClassNotFoundException {
-        return bundle.loadClass(name);
+    protected Class findClass(final String name) throws ClassNotFoundException {
+        try {
+            return AccessController.doPrivileged(new PrivilegedExceptionAction<Class<?>>() {
+                public Class<?> run() throws ClassNotFoundException 
+                {
+                    return bundle.loadClass(name);
+                }
+            
+            });
+        } catch (PrivilegedActionException e) {
+            Exception cause = e.getException();
+          
+            if (cause instanceof ClassNotFoundException) throw (ClassNotFoundException)cause;
+            else throw (RuntimeException)cause;
+        }    
     }
 
-    protected URL findResource(String name) {
-        URL resource = bundle.getResource(name);
+    protected URL findResource(final String name) {
+        URL resource = AccessController.doPrivileged(new PrivilegedAction<URL>() {
+            public URL run()
+            {
+                return bundle.getResource(name);
+            }
+        });        
         if (classLoader != null && resource == null) {
             resource = classLoader.getResource(name);
         }
         return resource;
     }
 
-    protected Enumeration findResources(String name) throws IOException {
-        return bundle.getResources(name);
+    protected Enumeration findResources(final String name) throws IOException {
+        Enumeration<URL> urls;
+        try {
+            urls =  AccessController.doPrivileged(new PrivilegedExceptionAction<Enumeration<URL>>() {
+                @SuppressWarnings("unchecked")
+                public Enumeration<URL> run() throws IOException
+                {
+                    return (Enumeration<URL>)bundle.getResources(name);
+                }
+          
+            });
+        } catch (PrivilegedActionException e) {
+            Exception cause = e.getException();
+        
+            if (cause instanceof IOException) throw (IOException)cause;
+            else throw (RuntimeException)cause;
+        }
+      
+        if (urls == null) {
+            urls = Collections.enumeration(new ArrayList<URL>());
+        }
+      
+        return urls;    
     }
 
     protected Class loadClass(String name, boolean resolve) throws ClassNotFoundException {
