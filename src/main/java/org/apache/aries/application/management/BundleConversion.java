bc2cba164eff3739a3831c7ed1676570b6c79336
Mark Nuttall <mnuttall@apache.org>
2010-07-01 15:55:55 +0000

    ARIES-344: BundleConverter interface returns one-use-only inputstream
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk/application/application-api@959704 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/aries/application/management/BundleConversion.java
@@ -0,0 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIESOR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.aries.application.management;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.aries.application.ApplicationMetadataFactory;
+
+/**
+ * A BundleConversion represents a .JAR file which has been converted in to 
+ * an well-formed OSGi bundle, or a .WAR file which has been converted into a .WAB 
+ * file
+ */
+public interface BundleConversion {
+
+	/**
+	 * @return The InputStream to the converted bundle.
+	 */
+	public InputStream getInputStream() throws IOException;
+	
+	/** 
+	 * @param The ApplicationMetadataFactory used to parse the content of 
+	 * the converted bundle into a BundleInfo object
+	 * @return The bundle information for the converted bundle.
+	 */
+	public BundleInfo getBundleInfo(ApplicationMetadataFactory amf) throws IOException;
+}
