6d1627e0302f7d0650b11a152e29eaccf2501fc5
Graham Charters <gcc@apache.org>
2010-07-05 12:23:14 +0000

    ARIES-349 - Added/updated Javadoc for subsystem.api package.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk/subsystem@960581 13f79535-47bb-0310-9956-ffa450edef68

--- subsystem-api/src/main/java/org/apache/aries/subsystem/Subsystem.java
+++ subsystem-api/src/main/java/org/apache/aries/subsystem/Subsystem.java
@@ -20,86 +20,178 @@ import org.osgi.framework.Bundle;
 import org.osgi.framework.Version;
 
 /**
- * Interface representing an installed subsystem
+ * A representation of a subsystem in the framework. A subsystem is a
+ * collection of bundles and/or other resource. A subsystem has isolation
+ * semantics. Subsystem types are defined that have different default isolation
+ * semantics. For example, an Application subsystem does not export any of the
+ * packages or services provided by its content bundles, and imports any
+ * packages or services that are required to satisfy unresolved package or
+ * service dependencies of the content bundles.
+ * 
+ * A subsystem is defined using a manifest format.
  */
 public interface Subsystem {
 
+    /**
+     * The states of a subsystem in the framework. These states match those of a
+     * Bundle and are derived using the same rules as CompositeBundles. As such,
+     * they are more a reflection of what content bundles are permitted to do
+     * rather than an aggregation of the content bundle states.
+     */
     public enum State {
 
+        /**
+         * A subsystem is in the INSTALLED state when it is initially created.
+         * It may or may not contain content bundles. No content bundles are
+         * permitted to be resolved while the subsystem is in the INSTALLED
+         * state.
+         */
         INSTALLED,
+
+        /**
+         * A subsystem in the RESOLVED is allowed to have its content bundles
+         * resolved. The content bundles are not allowed to activate while the
+         * subsystem is in the RESOLVED state.
+         */
         RESOLVED,
+
+        /**
+         * A subsystem is in the STARTING state when all its content bundles are
+         * enabled for activation. The content bundles are started according to
+         * the start-level specification.
+         */
         STARTING,
+
+        /**
+         * A subsystem in the STOPPING state is in the process of taking its its active start level to
+         * zero, stopping all the content bundles.
+         */
         STOPPING,
+
+        /**
+         * A subsystem is in the ACTIVE state when it has reached the beginning
+         * start-level (for starting it's contents), and all its persistently
+         * started content bundles that are resolved and have had their
+         * start-levels met have completed, or failed, their activator start
+         * method.
+         */
         ACTIVE,
+
+        /**
+         * A subsystem is in the UNINSTALLED state when all its content bundles
+         * and uninstalled and its system bundle context is invalidated.
+         */
         UNINSTALLED
 
     }
 
     /**
-     * Retrieve the state of the subsystem.
+     * Gets the state of the subsystem.
      *
-     * @return
+     * @return the state of the subsystem.
      */
     State getState();
 
     /**
-     * Start the subsystem (i.e. start all its constituent bundles according to their start level).
+     * Starts the subsystem. The subsystem is started according to the rules
+     * defined for Bundles and the content bundles are enabled for activation.
+     * 
+     * @throws SubsystemException
+     *             If this subsystem could not be started.
+     * @throws IllegalStateException
+     *             If this subsystem has been uninstalled.
+     * @throws SecurityException
+     *             If the caller does not have the appropriate
+     *             AdminPermission[this,EXECUTE] and the runtime supports
+     *             permissions.
      */
     void start() throws SubsystemException;
 
     /**
-     * Stop the subsystem (i.e. stop all its constituent bundles).
+     * Stops the subsystem. The subsystem is stopped according to the rules
+     * defined for Bundles and the content bundles are disabled for activation
+     * and stopped.
+     * 
+     * @throws SubsystemException
+     *             TODO: does not fit with BundleException from Bundle.stop.
+     *             What are the circumstances that cause this to be throw? When
+     *             an exception is thrown from a content bundle?
+     * @throws IllegalStateException
+     *             If this subsystem has been uninstalled.
+     * @throws SecurityException
+     *             If the caller does not have the appropriate
+     *             AdminPermission[this,EXECUTE] and the runtime supports
+     *             permissions.
      */
     void stop() throws SubsystemException;
 
     /**
-     * The identifier of the subsystem.  Must be unique in the framework.
-     *
-     * @return
+     * Gets the identifier of the subsystem. Subsystem identifiers are assigned
+     * when the subsystem is installed and are unique within the framework.
+     * 
+     * @return the identifier for the subsystem.
      */
     long getSubsystemId();
 
     /**
-     * The location of the subsystem.
-     * The location will be used when updating a subsystem to load the new
-     * content and/or identify subsystems to update.
-     *
-     * @return
+     * The location identifier used to install this subsystem through
+     * SubsystemAdmin.install. This identifier does not change while this
+     * subsystem remains installed, even after SubsystemAdmin.update. This
+     * location identifier is used in SubsystemAdmin.update if no other update
+     * source is specified.
+     * 
+     * @return The string representation of the subsystem's location identifier.
      */
     String getLocation();
 
     /**
-     * Retrieve the symbolic name of this subsystem.
+     * Gets the symbolic name of this subsystem.
      *
-     * @return
+     * @return the symbolic name of this subsystem.
      */
     String getSymbolicName();
 
     /**
-     * Retrieve the version of this subsystem.
+     * Gets the version of this subsystem.
      *
-     * @return
+     * @return the version of this subsystem.
      */
     Version getVersion();
 
     /**
-     * Return the subsystem headers
-     *
-     * @return
+     * Gets the headers used to define this subsystem. The headers will be
+     * localized using the locale returned by java.util.Locale.getDefault. This
+     * is equivalent to calling <code>getHeaders(null)</code>.
+     * 
+     * @return the headers used to define this subsystem.
+     * @throws SecurityException
+     *             If the caller does not have the appropriate
+     *             AdminPermission[this,METADATA] and the runtime supports
+     *             permissions.
      */
     Map<String, String> getHeaders();
 
     /**
-     * Return the subsystem headers
-     *
-     * @return
+     * Gets the headers used to define this subsystem.
+     * 
+     * @param locale
+     *            The locale name to be used to localize the headers. If the
+     *            locale is <code>null</code> then the locale returned by
+     *            java.util.Locale.getDefault is used. If the value is the empty
+     *            string then the returned headers are returned unlocalized.
+     * @return the headers used to define this subsystem, localized to the
+     *         specified locale.
+     * @throws SecurityException
+     *             If the caller does not have the appropriate
+     *             AdminPermission[this,METADATA] and the runtime supports
+     *             permissions.
      */
     Map<String, String> getHeaders(String locale);
 
     /**
-     * Retrieve the constituent bundles of this subsystem.
+     * Gets the content bundles of this subsystem.
      *
-     * @return
+     * @return the content of this subsystem.
      */
     Collection<Bundle> getConstituents();
 
--- subsystem-api/src/main/java/org/apache/aries/subsystem/SubsystemEvent.java
+++ subsystem-api/src/main/java/org/apache/aries/subsystem/SubsystemEvent.java
@@ -13,19 +13,58 @@
  */
 package org.apache.aries.subsystem;
 
+import java.io.InputStream;
+
 /**
  * Event sent to listeners when an operation has been performed on a subsystem.
  */
 public class SubsystemEvent {
 
+    /**
+     * The subsystem lifecycle event types that can be produced by a subsystem.
+     * See {@link Subsystem} and {@link SubsystemAdmin} for details on the
+     * circumstances under which these events are fired.
+     */
     public enum Type {
+        
+        /**
+         * Event type used to indicate a subsystem has been installed.
+         */
         INSTALLED,
+
+        /**
+         * Event type used to indicate a subsystem has been started.
+         */
         STARTED,
+
+        /**
+         * Event type used to indicate a subsystem has been stopped.
+         */
         STOPPED,
+        
+        /**
+         * Event type used to indicate a subsystem has been updated.
+         */
         UPDATED,
+        
+        /**
+         * Event type used to indicate a subsystem has been uninstalled.
+         */
         UNINSTALLED,
+        
+        /**
+         * Event type used to indicate a subsystem has been resolved.
+         */
         RESOLVED,
+        
+        /**
+         * Event type used to indicate a subsystem is starting.
+         */
         STARTING,
+        
+        /**
+         * Event type used to indicate a subsystem is stopping.
+         */
         STOPPING
     }
     private final Type type;
@@ -34,20 +73,41 @@ public class SubsystemEvent {
 
     private final Subsystem subsystem;
 
+    /**
+     * Constructs a new subsystem event.
+     * 
+     * @param type The type of the event.  For example, INSTALLED.  See {@link SubsystemEvent.Type} for the list of event types.
+     * @param timestamp The timestamp for the event.
+     * @param subsystem The subsystem for which the event is being created.
+     */
     public SubsystemEvent(Type type, long timestamp, Subsystem subsystem) {
         this.type = type;
         this.timestamp = timestamp;
         this.subsystem = subsystem;
     }
 
+    /**
+     * Gets the type of the subsystem event.  See {@link SubsystemEvent.Type} for the list of event types.
+     * @return the type of the subsystem event.
+     */
     public Type getType() {
         return type;
     }
 
+    /**
+     * Gets the timestamp for the subsystem event.
+     * 
+     * @return the timestamp for the subsystem event.
+     */
     public long getTimestamp() {
         return timestamp;
     }
 
+    /**
+     * Gets the subsystem for which the event was created.
+     * 
+     * @return the subsystem for which the event was created.
+     */
     public Subsystem getSubsystem() {
         return subsystem;
     }
