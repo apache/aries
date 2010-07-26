cda47908551fcaa31c394ad74e97156af4c5b0d0
Jarek Gawor <gawor@apache.org>
2010-07-26 16:04:24 +0000

    ARIES-356: Make sure classloader for cglib proxies can load cglib classes. Also refector the jdk/cglib proxy code into separate classes.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk/jndi/jndi-url@979336 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/aries/jndi/services/CgLibProxyFactory.java
@@ -0,0 +1,97 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.aries.jndi.services;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.Callable;
+
+import net.sf.cglib.proxy.Dispatcher;
+import net.sf.cglib.proxy.Enhancer;
+
+import org.apache.aries.util.BundleToClassLoaderAdapter;
+import org.osgi.framework.Bundle;
+
+public class CgLibProxyFactory implements ProxyFactory {
+
+    public Object createProxy(final Bundle bundle,
+                              final Class[] classes,
+                              final Callable<Object> dispatcher) {
+        Enhancer e = new Enhancer();
+        e.setClassLoader(new CgLibClassLoader(bundle));
+        e.setSuperclass(getTargetClass(classes));
+        e.setInterfaces(getInterfaces(classes));
+        e.setInterceptDuringConstruction(false);
+        e.setCallback(new Dispatcher() {
+            public Object loadObject() throws Exception {
+                return dispatcher.call();
+            }
+        });
+        e.setUseFactory(false);
+        return e.create();
+    }
+
+    private static Class<?>[] getInterfaces(Class<?>[] classes) {
+        Set<Class<?>> interfaces = new HashSet<Class<?>>();
+        for (Class<?> clazz : classes) {
+            if (clazz.isInterface()) {
+                interfaces.add(clazz);
+            }
+        }
+        return interfaces.toArray(new Class[interfaces.size()]);
+    }
+
+    protected Class<?> getTargetClass(Class<?>[] interfaceNames) {
+        // Only allow class proxying if specifically asked to
+        Class<?> root = Object.class;
+        for (Class<?> clazz : interfaceNames) {
+            if (!clazz.isInterface()) {
+                if (root.isAssignableFrom(clazz)) {
+                    root = clazz;
+                } else if (clazz.isAssignableFrom(root)) {
+                    // nothing to do, root is correct
+                } else {
+                    throw new IllegalArgumentException("Classes " + root.getClass().getName()
+                                                       + " and " + clazz.getName()
+                                                       + " are not in the same hierarchy");
+                }
+            }
+        }
+        return root;
+    }
+
+    private static class CgLibClassLoader extends BundleToClassLoaderAdapter {
+
+        private ClassLoader cgLibClassLoader;
+
+        public CgLibClassLoader(Bundle bundle) {
+            super(bundle);
+            this.cgLibClassLoader = Enhancer.class.getClassLoader();
+        }
+
+        @Override
+        public Class<?> loadClass(final String name) throws ClassNotFoundException {
+            if (name.startsWith("net.sf.cglib")) {
+                return cgLibClassLoader.loadClass(name);
+            } else {
+                return super.loadClass(name);
+            }
+        }
+    }
+}
