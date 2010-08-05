2b1e2ed6a99da61007f304cda1d874f3add03972
Lin Sun <linsun@apache.org>
2010-08-05 21:20:36 +0000

    move test in NameSpaceHandlerOptionMethodTest to NameSpaceHandlerTest
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk@982790 13f79535-47bb-0310-9956-ffa450edef68

--- transaction/transaction-blueprint/src/test/java/org/apache/aries/transaction/NameSpaceHandlerTest.java
+++ transaction/transaction-blueprint/src/test/java/org/apache/aries/transaction/NameSpaceHandlerTest.java
@@ -38,6 +38,8 @@ import org.apache.aries.blueprint.container.Parser;
 import org.apache.aries.blueprint.container.NamespaceHandlerRegistry.NamespaceHandlerSet;
 import org.apache.aries.blueprint.namespace.ComponentDefinitionRegistryImpl;
 import org.apache.aries.blueprint.namespace.NamespaceHandlerRegistryImpl;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
@@ -51,32 +53,73 @@ import org.apache.aries.unittest.mocks.Skeleton;
 
 public class NameSpaceHandlerTest {
     
+	Bundle b;
+	private NamespaceHandlerRegistry nhri;
+	private TxComponentMetaDataHelperImpl txenhancer;
+	
+    @Before
+    public void setUp() {
+        b = Skeleton.newMock(new BundleMock("org.apache.aries.tx", new Properties()), Bundle.class);
+        BundleContext ctx = b.getBundleContext();
+        nhri = new NamespaceHandlerRegistryImpl(ctx);
+        
+        TransactionManager tm = Skeleton.newMock(TransactionManager.class);
+        
+        txenhancer = new TxComponentMetaDataHelperImpl();
+        
+        TxInterceptorImpl txinterceptor = new TxInterceptorImpl();
+        txinterceptor.setTransactionManager(tm);
+        txinterceptor.setTxMetaDataHelper(txenhancer);
+        
+        TxElementHandler namespaceHandler = new TxElementHandler();
+        namespaceHandler.setTransactionInterceptor(txinterceptor);
+        namespaceHandler.setTxMetaDataHelper(txenhancer);
+            
+        Properties props = new Properties();
+        props.put("osgi.service.blueprint.namespace", new String[]{"http://aries.apache.org/xmlns/transactions/v1.0.0", "http://aries.apache.org/xmlns/transactions/v1.1.0"});
+        ctx.registerService(NamespaceHandler.class.getName(), namespaceHandler, props);
+    }
+    
+    @After
+    public void tearDown() throws Exception{
+    	b = null;
+        nhri = null;
+        txenhancer = null;
+    }
     
     @Test
     public void testMultipleElements() throws Exception
     {
-      Bundle b = Skeleton.newMock(new BundleMock("org.apache.aries.tx", new Properties()), Bundle.class);
-      BundleContext ctx = b.getBundleContext();
-      NamespaceHandlerRegistry nhri = new NamespaceHandlerRegistryImpl(ctx);
-      
-      TransactionManager tm = Skeleton.newMock(TransactionManager.class);
-      
-      TxComponentMetaDataHelperImpl txenhancer = new TxComponentMetaDataHelperImpl();
-      
-      TxInterceptorImpl txinterceptor = new TxInterceptorImpl();
-      txinterceptor.setTransactionManager(tm);
-      txinterceptor.setTxMetaDataHelper(txenhancer);
-      
-      TxElementHandler namespaceHandler = new TxElementHandler();
-      namespaceHandler.setTransactionInterceptor(txinterceptor);
-      namespaceHandler.setTxMetaDataHelper(txenhancer);
-          
-      Properties props = new Properties();
-      props.put("osgi.service.blueprint.namespace", new String[]{"http://aries.apache.org/xmlns/transactions/v1.0.0", "http://aries.apache.org/xmlns/transactions/v1.1.0"});
-      ctx.registerService(NamespaceHandler.class.getName(), namespaceHandler, props);
       Parser p = new Parser();
       
       URL bpxml = this.getClass().getResource("aries.xml");
+      List<URL> bpxmlList = new LinkedList<URL>();
+      bpxmlList.add(bpxml); 
+      
+      p.parse(bpxmlList);
+      Set<URI> nsuris = p.getNamespaces();
+      NamespaceHandlerSet nshandlers = nhri.getNamespaceHandlers(nsuris, b);
+      p.validate(nshandlers.getSchema());
+      
+      ComponentDefinitionRegistry cdr = new ComponentDefinitionRegistryImpl();
+      p.populate(nshandlers, cdr);
+      
+      BeanMetadata comp = (BeanMetadata) cdr.getComponentDefinition("top");
+      
+      BeanMetadata anon = (BeanMetadata) ((BeanProperty) comp.getProperties().get(0)).getValue();
+      BeanMetadata anonToo = (BeanMetadata) ((BeanProperty) comp.getProperties().get(1)).getValue();
+
+      assertEquals("Required", txenhancer.getComponentMethodTxAttribute(anon, "doSomething"));
+      assertEquals("Never", txenhancer.getComponentMethodTxAttribute(anonToo, "doSomething"));
+        
+    }
+    
+    @Test
+    public void testOptionalMethodAttribute() throws Exception
+    {
+      Parser p = new Parser();
+      
+      URL bpxml = this.getClass().getResource("aries2.xml");
       List<URL> bpxmlList = new LinkedList<URL>();
       bpxmlList.add(bpxml); 
       
