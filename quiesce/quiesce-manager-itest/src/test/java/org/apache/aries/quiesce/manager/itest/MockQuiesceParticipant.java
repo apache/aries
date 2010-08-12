8f2ab43f3c201a9957ce900857a9e2c45ffae381
Timothy James Ward <timothyjward@apache.org>
2010-08-12 15:06:30 +0000

    ARIES-371 - Quiesce Manager implementation (from Hannah Ramlee)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk@984819 13f79535-47bb-0310-9956-ffa450edef68

---
+++ quiesce/quiesce-manager-itest/src/test/java/org/apache/aries/quiesce/manager/itest/MockQuiesceParticipant.java
@@ -0,0 +1,77 @@
+package org.apache.aries.quiesce.manager.itest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.apache.aries.quiesce.manager.QuiesceCallback;
+import org.apache.aries.quiesce.participant.QuiesceParticipant;
+import org.osgi.framework.Bundle;
+
+public class MockQuiesceParticipant implements QuiesceParticipant {
+
+	public static final int RETURNIMMEDIATELY = 0;
+	public static final int NEVERRETURN = 1;
+	public static final int WAIT = 2;
+	private int behaviour;
+	private List<QuiesceCallback> callbacks = new ArrayList<QuiesceCallback>();
+	private ExecutorService executor = Executors.newCachedThreadPool();
+	private int started = 0;
+	private int finished = 0;
+	
+	public MockQuiesceParticipant( int i ) {
+		behaviour = i;
+	}
+
+	public void quiesce(final QuiesceCallback callback, final List<Bundle> bundlesToQuiesce) {
+		Runnable command = new Runnable() {
+			public void run() {
+				started += 1;
+				callbacks.add(callback);
+				switch (behaviour) {
+				case 0:
+					//return immediately
+					System.out.println("MockParticipant: return immediately");
+					callback.bundleQuiesced(bundlesToQuiesce.toArray(new Bundle[bundlesToQuiesce.size()]));
+					callbacks.remove(callback);
+					finished += 1;
+					break;
+				case 1:
+					//just don't do anything
+					System.out.println("MockParticipant: just don't do anything");
+					break;
+				case 2:
+					//Wait for 5s then quiesce
+					System.out.println("MockParticipant: Wait for 5s then quiesce");
+					try {
+						Thread.sleep(5000);
+					} catch (InterruptedException e) {
+					}
+					callback.bundleQuiesced(bundlesToQuiesce.toArray(new Bundle[bundlesToQuiesce.size()]));
+					callbacks.remove(callback);
+					finished += 1;
+					break;
+				default: 
+					//Unknown behaviour, don't do anything
+				}
+			}
+		};
+		executor.execute(command);
+	}
+
+	public int getStartedCount() {
+		return started;
+	}
+	
+	public int getFinishedCount() {
+		return finished;
+	}
+	
+	public synchronized void reset() {
+		started = 0;
+		finished = 0;
+	}
+	
+	
+}
