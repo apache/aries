c7d726df6bc0bc88ff38f2019fc17bf81629811d
Mark Nuttall <mnuttall@apache.org>
2010-08-17 13:02:17 +0000

    ARIES-380: IOUtils.getOutputStream bug; add unpackZip method
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk@986298 13f79535-47bb-0310-9956-ffa450edef68

--- application/application-utils/src/test/java/org/apache/aries/application/utils/filesystem/IOUtilsTest.java
+++ application/application-utils/src/test/java/org/apache/aries/application/utils/filesystem/IOUtilsTest.java
@@ -20,17 +20,22 @@
 package org.apache.aries.application.utils.filesystem;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.StringBufferInputStream;
 import java.util.zip.ZipFile;
 
+import org.apache.aries.application.filesystem.IFile;
+import org.apache.aries.application.utils.filesystem.impl.FileImpl;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -65,7 +70,8 @@ public class IOUtilsTest
   public void testWriteOut() throws IOException
   {
     File tmpDir = new File("ioUtilsTest/tmp");
-    tmpDir.mkdirs();
+    // Do not call mkdirs() on tmpDir. writeOut() should create it. 
+    // tmpDir.mkdirs(); 
     
     IOUtils.writeOut(tmpDir, "simple.txt", new StringBufferInputStream("abc"));
     IOUtils.writeOut(tmpDir, "some/relative/directory/complex.txt", new StringBufferInputStream("def"));
@@ -94,6 +100,80 @@ public class IOUtilsTest
     assertEquals("ghi", r.readLine());
     assertNull(r.readLine());
     r.close();
+  }
+  
+  @Test 
+  public void testWriteOutAndDoNotCloseInputStream() throws IOException{
+    InputStream is = new InputStream(){
+      int idx=0;
+      int data[]=new int[]{1,2,3,4,5,-1};
+      @Override
+      public int read() throws IOException
+      {
+        if(idx<data.length)
+          return data[idx++];
+        else
+          return -1;
+      }
+      @Override
+      public void close() throws IOException
+      {
+        fail("Close was invoked");
+      }
+    };
+    File f = new File("unittest/outtest1");
+    f.mkdirs();
+    IOUtils.writeOutAndDontCloseInputStream(f, "/fred", is);
+    File fred = new File(f,"/fred");
+    assertTrue(fred.exists());
+    File outtest = fred.getParentFile();
+    fred.delete();
+    outtest.delete();
+    
+  }
+  
+  @Test
+  public void testEmptyDirectory() throws IOException {
+    /* Create a .zip with a single entry, an empty directory inside. 
+     * Confirm that the directory is recreated on unzip. 
+     */
+    File testDir = new File ("unittest/emptyDirTest/");
+    File parent = new File (testDir, "emptyTestRootDir/");
+    File emptyDir = new File (parent, "foo/");
+    emptyDir.mkdirs();
+    assertTrue (emptyDir.exists());
+    
+    File zipWithEmptyDir = new File (testDir, "empty.zip");
+    IOUtils.zipUp(parent, zipWithEmptyDir);
+    
+    emptyDir.delete();
+    assertFalse (emptyDir.exists());
+    
+    IFile zip = new FileImpl(zipWithEmptyDir, testDir);
+    IOUtils.unpackZip(zip, parent);
+    assertTrue (emptyDir.exists());
+  }
+  
+  @Test
+  public void testSingleRootLevelEntry() throws IOException { 
+    /* Create a .zip with a single entry, a root-level file. 
+     * Confirm that the file is recreated on unzip. 
+     */
+    File testDir = new File ("unittest/singleFileInZipTest/");
+    File parent = new File (testDir, "singleFileRootDir/");
+    File entry = new File (parent, "foo.txt");
+    entry.mkdirs();
+    assertTrue (entry.exists());
+    
+    File zipWithSingleFileInRootdir = new File (testDir, "singleFile.zip");
+    IOUtils.zipUp(parent, zipWithSingleFileInRootdir);
+    
+    entry.delete();
+    assertFalse (entry.exists());
+    
+    IFile zip = new FileImpl(zipWithSingleFileInRootdir, testDir);
+    IOUtils.unpackZip(zip, parent);
+    assertTrue (entry.exists());
   }
 }
 
