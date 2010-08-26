4c50d7b7e185c0a0c09efe658a71bf4ee2408e1b
Mark Nuttall <mnuttall@apache.org>
2010-08-26 11:51:09 +0000

    ARIES-361: Provisioning changes required to support application isolation. From a patch submitted by Emily Jiang.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk@989612 13f79535-47bb-0310-9956-ffa450edef68

--- application/application-itests/src/test/java/org/apache/aries/application/runtime/itests/MinimumImportsTest.java
+++ application/application-itests/src/test/java/org/apache/aries/application/runtime/itests/MinimumImportsTest.java
@@ -18,14 +18,18 @@
  */
 package org.apache.aries.application.runtime.itests;
 
-import java.io.File;
-import java.io.FileOutputStream;
-
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.ops4j.pax.exam.CoreOptions.equinox;
 import static org.ops4j.pax.exam.CoreOptions.options;
 import static org.ops4j.pax.exam.CoreOptions.systemProperty;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 
 import org.apache.aries.application.management.AriesApplication;
 import org.apache.aries.application.management.AriesApplicationContext;
@@ -33,52 +37,86 @@ import org.apache.aries.application.management.AriesApplicationManager;
 import org.apache.aries.application.utils.filesystem.FileSystem;
 import org.apache.aries.unittest.fixture.ArchiveFixture;
 import org.apache.aries.unittest.fixture.ArchiveFixture.ZipFixture;
+import org.apache.felix.bundlerepository.Repository;
+import org.apache.felix.bundlerepository.RepositoryAdmin;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.osgi.framework.Constants;
 import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.blueprint.container.BlueprintEvent;
 import org.osgi.service.blueprint.container.BlueprintListener;
 
-
 @RunWith(JUnit4TestRunner.class)
+
 public class MinimumImportsTest extends AbstractIntegrationTest {
-  
+
   /* Use @Before not @BeforeClass so as to ensure that these resources
    * are created in the paxweb temp directory, and not in the svn tree 
    */
   static boolean createdApplications = false;
+  static String fake_app_management = "application.management.fake";
   @Before
   public static void createApplications() throws Exception {
 
     if (createdApplications) { 
       return;
     }
+
+    // need to fake a application manager to export the service in order to pass the resolving for the client
+    // In the real situation, we don't allow customers' bundles to explicitly import the runtime services.
+    ZipFixture bundle = ArchiveFixture.newJar().manifest()
+    .attribute(Constants.BUNDLE_SYMBOLICNAME, fake_app_management)
+    .attribute(Constants.BUNDLE_MANIFESTVERSION, "2")
+    .attribute(Constants.BUNDLE_VERSION, "1.0.0").end();
+
+    OutputStream out = new FileOutputStream(fake_app_management + ".jar");
+    bundle.writeOut(out);
+    out.close();
+
+
     ZipFixture testEba = ArchiveFixture.newZip()
-      .jar("org.apache.aries.application.itests.minimports.jar")
-        .manifest().symbolicName("org.apache.aries.application.itests.minimports")
-          .attribute("Bundle-Version", "1.0.0")
-          .attribute("Import-Package", "org.apache.aries.application.management")
-          .end()
-        .binary("org/apache/aries/application/sample/appmgrclient/AppMgrClient.class", 
-            MinimumImportsTest.class.getClassLoader().getResourceAsStream("org/apache/aries/application/sample/appmgrclient/AppMgrClient.class"))
+    .jar("org.apache.aries.application.itests.minimports.jar")
+    .manifest().symbolicName("org.apache.aries.application.itests.minimports")
+    .attribute("Bundle-Version", "1.0.0")
+    .attribute("Import-Package", "org.apache.aries.application.management")
+    .end()
+    .binary("org/apache/aries/application/sample/appmgrclient/AppMgrClient.class", 
+        MinimumImportsTest.class.getClassLoader().getResourceAsStream("org/apache/aries/application/sample/appmgrclient/AppMgrClient.class"))
         .binary("OSGI-INF/blueprint/app-mgr-client.xml", 
             MinimumImportsTest.class.getClassLoader().getResourceAsStream("app-mgr-client.xml"))
-        .end();
-      
+            .end();
+
     FileOutputStream fout = new FileOutputStream("appmgrclienttest.eba");
     testEba.writeOut(fout);
     fout.close();
-    
+
+    StringBuilder repositoryXML = new StringBuilder();
+
+    BufferedReader reader = new BufferedReader(new InputStreamReader(MinimumImportsTest.class.getResourceAsStream("/basic/fakeAppMgrServiceRepo.xml")));
+    String line;
+
+    while ((line = reader.readLine()) != null) {
+      repositoryXML.append(line);
+      repositoryXML.append("\r\n");
+    }
+
+    String repo = repositoryXML.toString().replaceAll("bundle_location", new File(fake_app_management + ".jar").toURI().toString());
+
+    System.out.println(repo);
+
+    FileWriter writer = new FileWriter("repository.xml");
+    writer.write(repo);
+    writer.close();
     createdApplications = true;
   }
-  
+
   public static class AppMgrClientBlueprintListener implements BlueprintListener {
-    
+
     Boolean success = null;
-    
+
     public void blueprintEvent(BlueprintEvent event) {
       if (event.getBundle().getSymbolicName().equals(
           "org.apache.aries.application.itests.minimports")) {
@@ -91,20 +129,29 @@ public class MinimumImportsTest extends AbstractIntegrationTest {
       }
     }
   }
-  
+
   @Test
   public void testAppUsingAriesApplicationManager() throws Exception {
-    
+
     // Register a BlueprintListener to listen for the events from the BlueprintContainer for the bundle in the appmgrclienttest.eba
-    
+
     AppMgrClientBlueprintListener acbl = new AppMgrClientBlueprintListener();
     ServiceRegistration sr = bundleContext.registerService("org.osgi.service.blueprint.container.BlueprintListener", acbl, null);
-    
+
     AriesApplicationManager manager = getOsgiService(AriesApplicationManager.class);
     AriesApplication app = manager.createApplication(FileSystem.getFSRoot(new File("appmgrclienttest.eba")));
+    RepositoryAdmin repositoryAdmin = getOsgiService(RepositoryAdmin.class);
+
+    Repository[] repos = repositoryAdmin.listRepositories();
+    for (Repository repo : repos) {
+      repositoryAdmin.removeRepository(repo.getURI());
+    }
+
+    repositoryAdmin.addRepository(new File("repository.xml").toURI().toURL());
+
     AriesApplicationContext ctx = manager.install(app);
     ctx.start();
-    
+
     int sleepfor = 3000;
     while ((acbl.success == null || acbl.success == false) && sleepfor > 0) {
       Thread.sleep(100);
@@ -112,12 +159,12 @@ public class MinimumImportsTest extends AbstractIntegrationTest {
     }
     assertNotNull("Timed out - didn't receive Blueprint CREATED or FAILURE event", acbl.success);
     assertTrue("Received Blueprint FAILURE event", acbl.success);
-    
+
     ctx.stop();
     manager.uninstall(ctx);
     sr.unregister();
   }
-  
+
   @org.ops4j.pax.exam.junit.Configuration
   public static Option[] configuration() {
     Option[] options = options(
@@ -140,18 +187,22 @@ public class MinimumImportsTest extends AbstractIntegrationTest {
         mavenBundle("org.apache.aries.application", "org.apache.aries.application.runtime"),
         mavenBundle("org.apache.aries.application", "org.apache.aries.application.runtime.itest.interfaces"),
         mavenBundle("org.apache.aries", "org.apache.aries.util"),
+        mavenBundle("org.apache.felix", "org.apache.felix.bundlerepository"),
+        mavenBundle("org.apache.aries.application", "org.apache.aries.application.resolver.obr"),
+        mavenBundle("org.apache.aries.application", "org.apache.aries.application.modeller"),
+        mavenBundle("org.apache.aries.application", "org.apache.aries.application.deployment.management"),
         mavenBundle("org.apache.aries.blueprint", "org.apache.aries.blueprint"), 
         mavenBundle("org.osgi", "org.osgi.compendium"),
         mavenBundle("org.apache.aries.testsupport", "org.apache.aries.testsupport.unit"),
-        
-        /* For debugging, uncomment the next two lines
-        vmOption ("-Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5006"),
-        waitForFrameworkStartup(),
-        
-        and add these imports:
-        import static org.ops4j.pax.exam.CoreOptions.waitForFrameworkStartup;
-        import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.vmOption;
-        */
+
+        /* For debugging, uncomment the next two lines*/
+        /*vmOption ("-Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5007"),
+          waitForFrameworkStartup(),*/
+
+        /*and add these imports:
+          import static org.ops4j.pax.exam.CoreOptions.waitForFrameworkStartup;
+          import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.vmOption;*/
+
 
         equinox().version("3.5.0"));
     options = updateOptions(options);
