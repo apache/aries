6cff8dd095fba137a6357989c2f84dfc5a0a60cd
Mark Nuttall <mnuttall@apache.org>
2010-09-08 15:31:57 +0000

    ARIES-361: Plug point to allow BundleResource objects to be augmented externally during provisioning. Fix a bad license header. Add a factory for ServiceDeclaration.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk@995096 13f79535-47bb-0310-9956-ffa450edef68

--- application/application-utils/src/main/java/org/apache/aries/application/utils/FilterUtils.java
+++ application/application-utils/src/main/java/org/apache/aries/application/utils/FilterUtils.java
@@ -1,20 +1,20 @@
 /*
- * @start_prolog@
- * ============================================================================
- * IBM Confidential OCO Source Materials
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * 5724-J08, 5724-I63, 5724-H88, 5724-H89, 5655-N02, 5733-W70 Copyright IBM Corp. 2010
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * The source code for this program is not published or otherwise divested
- * of its trade secrets, irrespective of what has been deposited with the
- * U.S. Copyright Office.
- * ============================================================================
- * @end_prolog@
- * 
- * Change activity:
- * 
- * Issue       Date        Name        Description
- * ----------- ----------- --------    ------------------------------------
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIESOR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package org.apache.aries.application.utils;
 
