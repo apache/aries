3039d99eb2035b743aa06e31b4e431c1166f6368
Guillaume Nodet <gnodet@apache.org>
2010-09-26 21:38:51 +0000

    [ARIES-424] Under certain circumstances, the blueprint container try to register a service while the bundle is stopping
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk/blueprint@1001527 13f79535-47bb-0310-9956-ffa450edef68

--- blueprint-core/src/main/java/org/apache/aries/blueprint/container/ServiceRecipe.java
+++ blueprint-core/src/main/java/org/apache/aries/blueprint/container/ServiceRecipe.java
@@ -143,6 +143,10 @@ public class ServiceRecipe extends AbstractRecipe {
     }
 
     public void register() {
+        int state = blueprintContainer.getBundleContext().getBundle().getState();
+        if (state != Bundle.ACTIVE && state != Bundle.STARTING) {
+            return;
+        }
         if (registered.compareAndSet(false, true)) {
             createExplicitDependencies();
             
