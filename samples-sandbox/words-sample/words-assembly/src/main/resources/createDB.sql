ec63fbaa8f7a6407dfa8b8bc90ee1c1e2d62dbd1
Holly Cummins <cumminsh@apache.org>
2010-09-27 16:12:13 +0000

    ARIES-422: Further improvements to the wordassociation instructions to simplify setup and running.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk@1001798 13f79535-47bb-0310-9956-ffa450edef68

--- samples-sandbox/words-sample/words-assembly/src/main/resources/createDB.sql
+++ samples-sandbox/words-sample/words-assembly/src/main/resources/createDB.sql
@@ -1,4 +1,4 @@
-connect 'jdbc:derby:wordsDB/;create=true';
-CREATE TABLE ASSOCIATION (word VARCHAR(255) NOT NULL, associated VARCHAR(255), PRIMARY KEY (word));
-DELETE FROM ASSOCIATION;
+connect 'jdbc:derby:target/wordsDB/;create=true';
+CREATE TABLE Association (word VARCHAR(255) NOT NULL, associated VARCHAR(255), PRIMARY KEY (word));
+DELETE FROM Association;
 exit;
