4b48331488efa7e1df13595060070d5a9640cc0b
Alasdair Nottingham <not@apache.org>
2010-10-06 15:42:36 +0000

    ARIES-440 Fixes for the following:
    1. The core does the following: 
       i) Find an ICF service 
       ii) Get ICF 
       iii) Create a Context 
       iv) release ICF service 
       v) return Context 
    
       This means that the ICF cannot manage the lifecycle because it doesn't know when the context is finished with. We should only release the service when the context has been closed. 
    2. We do a lot of queries, which is kind of expensive. If we used a service tracker we could remove all the queries and it would be more efficient and perform some amount better. 
    3. Got some things wrong on ARIES-417. e.g. 
        ii) The URLObjectFactoryFinder doesn't get the environment, so it can't do things based on the JNDI environment, this is bad. 
        
    I would have liked to separate these things out more, but I managed to get my changes tangled. Need to take more care next time.
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk@1005089 13f79535-47bb-0310-9956-ffa450edef68

--- jndi/jndi-api/src/main/java/org/apache/aries/jndi/urls/URLObjectFactoryFinder.java
+++ jndi/jndi-api/src/main/java/org/apache/aries/jndi/urls/URLObjectFactoryFinder.java
@@ -1,8 +1,11 @@
 package org.apache.aries.jndi.urls;
 
+import java.util.Hashtable;
+
+import javax.naming.NamingException;
 import javax.naming.spi.ObjectFactory;
 
 public interface URLObjectFactoryFinder 
 {
-  public ObjectFactory findFactory(String url);
+  public ObjectFactory findFactory(String url, Hashtable<?, ?> env) throws NamingException;
 }
--- jndi/jndi-core/src/main/java/org/apache/aries/jndi/startup/Activator.java
+++ jndi/jndi-core/src/main/java/org/apache/aries/jndi/startup/Activator.java
@@ -19,10 +19,13 @@
 package org.apache.aries.jndi.startup;
 
 import java.lang.reflect.Field;
+import java.util.Arrays;
 
 import javax.naming.NamingException;
+import javax.naming.spi.InitialContextFactory;
 import javax.naming.spi.InitialContextFactoryBuilder;
 import javax.naming.spi.NamingManager;
+import javax.naming.spi.ObjectFactory;
 import javax.naming.spi.ObjectFactoryBuilder;
 
 import org.apache.aries.jndi.ContextManagerServiceFactory;
@@ -30,10 +33,16 @@ import org.apache.aries.jndi.JREInitialContextFactoryBuilder;
 import org.apache.aries.jndi.OSGiInitialContextFactoryBuilder;
 import org.apache.aries.jndi.OSGiObjectFactoryBuilder;
 import org.apache.aries.jndi.ProviderAdminServiceFactory;
+import org.apache.aries.jndi.Utils;
+import org.apache.aries.jndi.tracker.ServiceTrackerCustomizers;
+import org.apache.aries.jndi.urls.URLObjectFactoryFinder;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
 import org.osgi.service.jndi.JNDIContextManager;
 import org.osgi.service.jndi.JNDIProviderAdmin;
+import org.osgi.util.tracker.ServiceTracker;
+import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
 /**
  * The activator for this bundle makes sure the static classes in it are
@@ -43,9 +52,18 @@ public class Activator implements BundleActivator {
     
     private OSGiInitialContextFactoryBuilder icfBuilder;
     private OSGiObjectFactoryBuilder ofBuilder;
+    private static ServiceTracker icfBuilders;
+    private static ServiceTracker urlObjectFactoryFinders;
+    private static ServiceTracker initialContextFactories;
+    private static ServiceTracker objectFactories;
     
     public void start(BundleContext context) {
-          
+        
+        initialContextFactories = initServiceTracker(context, InitialContextFactory.class, ServiceTrackerCustomizers.ICF_CACHE);
+        objectFactories = initServiceTracker(context, ObjectFactory.class, ServiceTrackerCustomizers.URL_FACTORY_CACHE);
+        icfBuilders = initServiceTracker(context, InitialContextFactoryBuilder.class, ServiceTrackerCustomizers.LAZY);
+        urlObjectFactoryFinders = initServiceTracker(context, URLObjectFactoryFinder.class, ServiceTrackerCustomizers.LAZY);
+        
         try {
             OSGiInitialContextFactoryBuilder builder = new OSGiInitialContextFactoryBuilder();
             NamingManager.setInitialContextFactoryBuilder(builder);
@@ -79,6 +97,14 @@ public class Activator implements BundleActivator {
                                 null);
     }
 
+    private ServiceTracker initServiceTracker(BundleContext context,
+        Class<?> type, ServiceTrackerCustomizer custom) 
+    {
+      ServiceTracker t = new ServiceTracker(context, type.getName(), custom);
+      t.open();
+      return t;
+    }
+
     public void stop(BundleContext context) {
         /*
          * Try to reset the InitialContextFactoryBuilder and ObjectFactoryBuilder
@@ -90,6 +116,12 @@ public class Activator implements BundleActivator {
         if (ofBuilder != null) {
             unsetField(ObjectFactoryBuilder.class);
         }
+        
+        icfBuilders.close();
+        urlObjectFactoryFinders.close();
+        objectFactories.close();
+        initialContextFactories.close();
+        
     }
     
     /*
@@ -108,5 +140,32 @@ public class Activator implements BundleActivator {
             // Ignore
           t.printStackTrace();
         }
+    }
+
+    public static ServiceReference[] getInitialContextFactoryBuilderServices()
+    {
+      ServiceReference[] refs = icfBuilders.getServiceReferences();
+      
+      if (refs != null) Arrays.sort(refs, Utils.SERVICE_REFERENCE_COMPARATOR);
+      
+      return refs;
+    }
+    
+    public static ServiceReference[] getInitialContextFactoryServices() 
+    {
+      ServiceReference[] refs = initialContextFactories.getServiceReferences();
+      
+      if (refs != null) Arrays.sort(refs, Utils.SERVICE_REFERENCE_COMPARATOR);
+      
+      return refs;
+    }
+
+    public static ServiceReference[] getURLObectFactoryFinderServices() 
+    {
+      ServiceReference[] refs = urlObjectFactoryFinders.getServiceReferences();
+      
+      if (refs != null) Arrays.sort(refs, Utils.SERVICE_REFERENCE_COMPARATOR);
+      
+      return refs;
     }
 }
