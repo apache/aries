ed92957f1d2d4bc5d12c5b18c805744feab97e83
Zoe Slattery <zoe@apache.org>
2010-10-30 15:42:36 +0000

    ARIES-476 Committing test - client fails to register service, but server publishes.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk@1029102 13f79535-47bb-0310-9956-ffa450edef68

---
+++ samples-sandbox/dgoat/dgoat-web/src/main/resources/web/goat/ConfigManager.js
@@ -0,0 +1,20 @@
+//dojo.provide allows pages use all types declared in this resource
+dojo.provide("goat.ConfigManager");
+
+//not even started this yet.. the idea is to have a config panel for the global config, allowing 
+//all sorts of magic stuff to be done ;-) .. yeah.. right..
+
+dojo.declare("goat.ConfigManager", [], {
+	
+    dialog:null,
+
+	constructor : function() {
+		this.dialog = new dijit.Dialog({title:"Configuration", id:"configDialog"});
+
+		dojo.body().appendChild(this.dialog.domNode);
+		this.dialog.hide();
+}
+
+
+
+});
