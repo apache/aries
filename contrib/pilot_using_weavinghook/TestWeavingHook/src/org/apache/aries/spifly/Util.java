4b50cd0acf0612b63ec5b905a622846691e359d1
A. J. David Bosschaert <davidb@apache.org>
2010-12-04 11:06:58 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/aries/trunk/spi-fly@1042142 13f79535-47bb-0310-9956-ffa450edef68

---
+++ contrib/pilot_using_weavinghook/TestWeavingHook/src/org/apache/aries/spifly/Util.java
@@ -0,0 +1,58 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.aries.spifly;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleReference;
+import org.osgi.framework.wiring.BundleWiring;
+
+public class Util {
+    static ThreadLocal<ClassLoader> classLoaders = new ThreadLocal<ClassLoader>();
+    
+    public static void storeContextClassloader() {
+        classLoaders.set(Thread.currentThread().getContextClassLoader());
+    }
+    
+    public static void restoreContextClassloader() {
+        Thread.currentThread().setContextClassLoader(classLoaders.get());
+        classLoaders.set(null);
+    }
+    
+    public static void fixContextClassloader() {
+        Thread.currentThread().setContextClassLoader(findClassLoader());
+    }
+    
+    private static ClassLoader findClassLoader() {
+        ClassLoader cl = Activator.class.getClassLoader();
+        if (!(cl instanceof BundleReference)) {
+            return null;
+        }
+        
+        BundleReference br = (BundleReference) cl;      
+        for (Bundle b : br.getBundle().getBundleContext().getBundles()) {
+            // TODO find the appropriate bundle
+            if ("MyServiceImpl".equals(b.getSymbolicName())) {
+                BundleWiring bw = b.adapt(BundleWiring.class);
+                if (bw != null)
+                    return bw.getClassLoader();
+            }           
+        }
+        return null;
+    }
+}
