4c54e2e85598fee1800ba91a2ad79d9e359efe85
Alasdair Nottingham <not@apache.org>
2011-02-15 18:44:55 +0000

    ARIES-576 Fix the grace period support that I broke by accidentally leaving in my workaround so I didn't have to wait 5 minutes to see if my fix worked.
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk/blueprint/blueprint-core@1071011 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/aries/blueprint/container/BlueprintContainerImpl.java
+++ src/main/java/org/apache/aries/blueprint/container/BlueprintContainerImpl.java
@@ -296,7 +296,7 @@ public class BlueprintContainerImpl implements ExtendedBlueprintContainer, Names
                                 }
                             }
                         };
-                        timeoutFuture = executors.schedule(r, 10, TimeUnit.SECONDS);
+                        timeoutFuture = executors.schedule(r, timeout, TimeUnit.MILLISECONDS);
                         state = State.WaitForInitialReferences;
                         break;
                     case WaitForInitialReferences:
