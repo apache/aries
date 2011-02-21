9e0dbfdc88b0f7378ab86022506b178209feaa9e
Alasdair Nottingham <not@apache.org>
2011-02-21 14:18:51 +0000

    ARIES-580 Move SingleServiceTracker back to the org.apache.aries.util package so we don't break clients with [0.3,1) in their import range, since we are yet ready to do a util 1.0 release.
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk/util@1072975 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/aries/util/SingleServiceTracker.java
+++ src/main/java/org/apache/aries/util/SingleServiceTracker.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.aries.util.tracker;
+package org.apache.aries.util;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
