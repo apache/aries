1158101a060611bb1182fde6ebd805cc05867e9b
A. J. David Bosschaert <davidb@apache.org>
2011-03-16 15:16:50 +0000

    Work in progress for Spi Fly
    The beginnings of a static weaver.
    
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk/spi-fly@1082164 13f79535-47bb-0310-9956-ffa450edef68

--- spi-fly-static/src/main/java/org/apache/aries/spifly/statictool/Main.java
+++ spi-fly-static/src/main/java/org/apache/aries/spifly/statictool/Main.java
@@ -20,15 +20,23 @@ package org.apache.aries.spifly.statictool;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.jar.JarEntry;
 import java.util.jar.JarInputStream;
+import java.util.jar.JarOutputStream;
 import java.util.jar.Manifest;
 
+import org.apache.aries.spifly.ConsumerHeaderProcessor;
 import org.apache.aries.spifly.Streams;
+import org.apache.aries.spifly.TCCLSetterVisitor;
+import org.apache.aries.spifly.WeavingData;
+import org.apache.aries.spifly.api.SpiFlyConstants;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.ClassWriter;
 
 public class Main {
     public static void usage() {
@@ -45,11 +53,45 @@ public class Main {
 
     private static void weaveJar(String jarPath) throws IOException {
         File jarFile = new File(jarPath);
-        File tempdir = new File(System.getProperty("java.io.tmpdir") + File.separator + jarFile.getName() + "_" + System.currentTimeMillis());        
-        unJar(jarFile, tempdir);
+        File tempDir = new File(System.getProperty("java.io.tmpdir") + File.separator + jarFile.getName() + "_" + System.currentTimeMillis());        
+        Manifest manifest = unJar(jarFile, tempDir);
+        String consumerHeader = manifest.getMainAttributes().getValue(SpiFlyConstants.SPI_CONSUMER_HEADER);
+        if (consumerHeader != null) {
+            weaveDir(tempDir, consumerHeader);
+            // jar(tempDir, newJar);
+        }
+        // finally - clean up
     }
 
-    static void unJar(File jarFile, File tempDir) throws IOException {
+    private static void weaveDir(File dir, String consumerHeader) throws IOException {
+        DirTree dt = new DirTree(dir);
+        for (File f : dt.getFiles()) {
+            if (!f.getName().endsWith(".class"))
+                continue;
+            
+            WeavingData[] wd = ConsumerHeaderProcessor.processHeader(consumerHeader);
+            InputStream is = new FileInputStream(f);
+            byte[] b;
+            try {
+                ClassReader cr = new ClassReader(is);
+                ClassWriter cw = new ClassWriter(0);
+                ClassVisitor cv = new TCCLSetterVisitor(cw, null, wd); 
+                cr.accept(cv, 0);
+                b = cw.toByteArray();
+            } finally {
+                is.close();
+            }
+            
+            OutputStream os = new FileOutputStream(f);
+            try {
+                os.write(b);                
+            } finally {
+                os.close();
+            }
+        }
+    }
+
+    static Manifest unJar(File jarFile, File tempDir) throws IOException {
         ensureDirectory(tempDir);
 
         JarInputStream jis = new JarInputStream(new FileInputStream(jarFile));
@@ -93,6 +135,55 @@ public class Main {
         }
         
         jis.close();
+        return manifest;
+    }
+    
+    static void jar(File jarFile, File rootFile, Manifest manifest) throws IOException {
+        JarOutputStream jos = new JarOutputStream(new FileOutputStream(jarFile), manifest);
+        try {
+            addToJarRecursively(jos, rootFile.getAbsoluteFile(), rootFile.getAbsolutePath());
+        } finally {
+            jos.close();
+        }
+    }
+
+    static void addToJarRecursively(JarOutputStream jar, File source, String rootDirectory) throws IOException {                
+        String sourceName = source.getAbsolutePath().replace("\\", "/");
+        sourceName = sourceName.substring(rootDirectory.length());
+        
+        if (sourceName.startsWith("/")) {
+            sourceName = sourceName.substring(1);
+        }
+        
+        if ("META-INF/MANIFEST.MF".equals(sourceName))
+            return;
+        
+        if (source.isDirectory()) {
+            /* Is there any point in adding a directory beyond just taking up space? 
+            if (!sourceName.isEmpty()) {
+                if (!sourceName.endsWith("/")) {
+                    sourceName += "/";                        
+                }
+                JarEntry entry = new JarEntry(sourceName);
+                jar.putNextEntry(entry);
+                jar.closeEntry();
+            }
+            */
+            for (File nested : source.listFiles()) {
+                addToJarRecursively(jar, nested, rootDirectory);
+            }
+            return;
+        }
+        
+        JarEntry entry = new JarEntry(sourceName);
+        jar.putNextEntry(entry);
+        InputStream is = new FileInputStream(source);
+        try {
+            Streams.pump(is, jar);
+        } finally {
+            jar.closeEntry();
+            is.close();
+        }
     }
 
     private static void ensureDirectory(File outDir) throws IOException {
