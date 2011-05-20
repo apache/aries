9f23bd1ee2c9791e7541f283f1901ca8d4c7ceda
A. J. David Bosschaert <davidb@apache.org>
2011-05-20 15:43:33 +0000

    Increase test coverage on ProviderBundleTrackerCustomizer.
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk/spi-fly@1125437 13f79535-47bb-0310-9956-ffa450edef68

--- spi-fly-core/src/test/java/org/apache/aries/spifly/ProviderBundleTrackerCustomizerTest.java
+++ spi-fly-core/src/test/java/org/apache/aries/spifly/ProviderBundleTrackerCustomizerTest.java
@@ -20,9 +20,11 @@ package org.apache.aries.spifly;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
 
 import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Dictionary;
@@ -31,45 +33,161 @@ import java.util.List;
 
 import org.apache.aries.spifly.api.SpiFlyConstants;
 import org.apache.aries.spifly.impl1.MySPIImpl1;
+import org.apache.aries.spifly.impl2.MySPIImpl2a;
+import org.apache.aries.spifly.impl2.MySPIImpl2b;
+import org.apache.aries.spifly.impl3.MySPIImpl3;
 import org.easymock.EasyMock;
 import org.junit.Test;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
 import org.osgi.framework.ServiceRegistration;
 
 public class ProviderBundleTrackerCustomizerTest {
     @Test
-    @SuppressWarnings("unchecked")
     public void testAddingRemovedBundle() throws Exception {
         Bundle spiBundle = EasyMock.createMock(Bundle.class);
         EasyMock.replay(spiBundle);
-        BaseActivator a = new BaseActivator() {
+        BaseActivator activator = new BaseActivator() {
             @Override
             public void start(BundleContext context) throws Exception {}
         };
 
-        ProviderBundleTrackerCustomizer customizer = new ProviderBundleTrackerCustomizer(a, spiBundle);
+        ProviderBundleTrackerCustomizer customizer = new ProviderBundleTrackerCustomizer(activator, spiBundle);
 
         ServiceRegistration sreg = EasyMock.createMock(ServiceRegistration.class);
         sreg.unregister();
         EasyMock.expectLastCall();
         EasyMock.replay(sreg);
 
-        // The bundle context for the test SPI bundle
+        BundleContext implBC = mockSPIBundleContext(sreg);
+        Bundle implBundle = mockSPIBundle(implBC);
+
+        assertEquals("Precondition", 0, activator.findProviderBundles("org.apache.aries.mytest.MySPI").size());
+        // Call addingBundle();
+        List<ServiceRegistration> registrations = customizer.addingBundle(implBundle, null);
+        Collection<Bundle> bundles = activator.findProviderBundles("org.apache.aries.mytest.MySPI");
+        assertEquals(1, bundles.size());
+        assertSame(implBundle, bundles.iterator().next());
+
+        // The bc.registerService() call should now have been made
+        EasyMock.verify(implBC);
+
+        // Call removedBundle();
+        customizer.removedBundle(implBundle, null, registrations);
+        // sreg.unregister() should have been called.
+        EasyMock.verify(sreg);
+    }
+
+    @Test
+    public void testAddingBundleSPIBundle() throws Exception {
+        BundleContext implBC = mockSPIBundleContext(EasyMock.createNiceMock(ServiceRegistration.class));
+        Bundle spiBundle = mockSPIBundle(implBC);
+
+        ProviderBundleTrackerCustomizer customizer = new ProviderBundleTrackerCustomizer(EasyMock.createNiceMock(BaseActivator.class), spiBundle);
+        assertNull("The SpiFly bundle itself should be ignored", customizer.addingBundle(spiBundle, null));
+    }
+
+    @Test
+    public void testAddingNonOptInBundle() throws Exception {
+        BundleContext implBC = mockSPIBundleContext(EasyMock.createNiceMock(ServiceRegistration.class));
+        Bundle implBundle = mockSPIBundle(implBC, null);
+
+        ProviderBundleTrackerCustomizer customizer = new ProviderBundleTrackerCustomizer(EasyMock.createNiceMock(BaseActivator.class), null);
+        assertNull("Bundle doesn't opt-in so should be ignored", customizer.addingBundle(implBundle, null));
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void testAddingBundleWithBundleClassPath() throws Exception {
+        Bundle spiBundle = EasyMock.createMock(Bundle.class);
+        EasyMock.replay(spiBundle);
+        BaseActivator activator = new BaseActivator() {
+            @Override
+            public void start(BundleContext context) throws Exception {}
+        };
+
+        ProviderBundleTrackerCustomizer customizer = new ProviderBundleTrackerCustomizer(activator, spiBundle);
+
+        BundleContext implBC = EasyMock.createMock(BundleContext.class);
+        EasyMock.<Object>expect(implBC.registerService(
+                EasyMock.eq("org.apache.aries.mytest.MySPI"),
+                EasyMock.isA(MySPIImpl2a.class),
+                (Dictionary<String,?>) EasyMock.anyObject())).andReturn(EasyMock.createNiceMock(ServiceRegistration.class));
+        EasyMock.<Object>expect(implBC.registerService(
+                EasyMock.eq("org.apache.aries.mytest.MySPI"),
+                EasyMock.isA(MySPIImpl2b.class),
+                (Dictionary<String,?>) EasyMock.anyObject())).andReturn(EasyMock.createNiceMock(ServiceRegistration.class));
+        EasyMock.<Object>expect(implBC.registerService(
+                EasyMock.eq("org.apache.aries.mytest.MySPI"),
+                EasyMock.isA(MySPIImpl3.class),
+                (Dictionary<String,?>) EasyMock.anyObject())).andReturn(EasyMock.createNiceMock(ServiceRegistration.class));
+        EasyMock.replay(implBC);
+
+
+        Bundle implBundle = EasyMock.createNiceMock(Bundle.class);
+        EasyMock.expect(implBundle.getBundleContext()).andReturn(implBC).anyTimes();
+
+        Dictionary<String, String> headers = new Hashtable<String, String>();
+        headers.put(SpiFlyConstants.SPI_PROVIDER_HEADER, "*");
+        headers.put(Constants.BUNDLE_CLASSPATH, ".,non-jar.jar,embedded.jar,embedded2.jar");
+        EasyMock.expect(implBundle.getHeaders()).andReturn(headers).anyTimes();
+
+        URL embeddedJar = getClass().getResource("/embedded.jar");
+        assertNotNull("precondition", embeddedJar);
+        EasyMock.expect(implBundle.getResource("embedded.jar")).andReturn(embeddedJar).anyTimes();
+        URL embedded2Jar = getClass().getResource("/embedded2.jar");
+        assertNotNull("precondition", embedded2Jar);
+        EasyMock.expect(implBundle.getResource("embedded2.jar")).andReturn(embedded2Jar).anyTimes();
+        URL dir = new URL("jar:" + embeddedJar + "!/META-INF/services");
+        assertNotNull("precondition", dir);
+        EasyMock.expect(implBundle.getResource("/META-INF/services")).andReturn(dir).anyTimes();
+        EasyMock.expect(implBundle.findEntries((String) EasyMock.anyObject(), (String) EasyMock.anyObject(), EasyMock.anyBoolean())).
+            andReturn(null).anyTimes();
+
+        ClassLoader cl = new URLClassLoader(new URL [] {embeddedJar}, getClass().getClassLoader());
+        Class<?> clsA = cl.loadClass("org.apache.aries.spifly.impl2.MySPIImpl2a");
+        EasyMock.<Object>expect(implBundle.loadClass("org.apache.aries.spifly.impl2.MySPIImpl2a")).andReturn(clsA).anyTimes();
+        Class<?> clsB = cl.loadClass("org.apache.aries.spifly.impl2.MySPIImpl2b");
+        EasyMock.<Object>expect(implBundle.loadClass("org.apache.aries.spifly.impl2.MySPIImpl2b")).andReturn(clsB).anyTimes();
+        ClassLoader cl2 = new URLClassLoader(new URL [] {embedded2Jar}, getClass().getClassLoader());
+        Class<?> clsC = cl2.loadClass("org.apache.aries.spifly.impl3.MySPIImpl3");
+        EasyMock.<Object>expect(implBundle.loadClass("org.apache.aries.spifly.impl3.MySPIImpl3")).andReturn(clsC).anyTimes();
+        EasyMock.replay(implBundle);
+
+        assertEquals("Precondition", 0, activator.findProviderBundles("org.apache.aries.mytest.MySPI").size());
+        // Call addingBundle();
+        List<ServiceRegistration> registrations = customizer.addingBundle(implBundle, null);
+        Collection<Bundle> bundles = activator.findProviderBundles("org.apache.aries.mytest.MySPI");
+        assertEquals(1, bundles.size());
+        assertSame(implBundle, bundles.iterator().next());
+
+        // The bc.registerService() call should now have been made
+        EasyMock.verify(implBC);
+    }
+
+    @SuppressWarnings("unchecked")
+    private BundleContext mockSPIBundleContext(ServiceRegistration sreg) {
         BundleContext implBC = EasyMock.createMock(BundleContext.class);
         EasyMock.<Object>expect(implBC.registerService(
                 EasyMock.eq("org.apache.aries.mytest.MySPI"),
                 EasyMock.isA(MySPIImpl1.class),
                 (Dictionary<String,?>) EasyMock.anyObject())).andReturn(sreg);
         EasyMock.replay(implBC);
+        return implBC;
+    }
 
-        // The test impl bundle
+    private Bundle mockSPIBundle(BundleContext implBC) throws ClassNotFoundException {
+        return mockSPIBundle(implBC, "*");
+    }
+
+    private Bundle mockSPIBundle(BundleContext implBC, String spiProviderHeader) throws ClassNotFoundException {
         Bundle implBundle = EasyMock.createNiceMock(Bundle.class);
-        EasyMock.expect(implBundle.getBundleContext()).andReturn(implBC);
+        EasyMock.expect(implBundle.getBundleContext()).andReturn(implBC).anyTimes();
 
         Dictionary<String, String> headers = new Hashtable<String, String>();
-        // Specify the headers for the test bundle
-        headers.put(SpiFlyConstants.SPI_PROVIDER_HEADER, "*");
+        if (spiProviderHeader != null)
+            headers.put(SpiFlyConstants.SPI_PROVIDER_HEADER, spiProviderHeader);
         EasyMock.expect(implBundle.getHeaders()).andReturn(headers).anyTimes();
 
         // List the resources found at META-INF/services in the test bundle
@@ -81,22 +199,8 @@ public class ProviderBundleTrackerCustomizerTest {
         EasyMock.expect(implBundle.findEntries("META-INF/services", "*", false)).andReturn(
                 Collections.enumeration(Collections.singleton(res))).anyTimes();
         Class<?> cls = getClass().getClassLoader().loadClass("org.apache.aries.spifly.impl1.MySPIImpl1");
-        EasyMock.<Object> expect(implBundle.loadClass("org.apache.aries.spifly.impl1.MySPIImpl1")).andReturn(cls).anyTimes();
+        EasyMock.<Object>expect(implBundle.loadClass("org.apache.aries.spifly.impl1.MySPIImpl1")).andReturn(cls).anyTimes();
         EasyMock.replay(implBundle);
-
-        assertEquals("Precondition", 0, a.findProviderBundles("org.apache.aries.mytest.MySPI").size());
-        // Call addingBundle();
-        List<ServiceRegistration> registrations = customizer.addingBundle(implBundle, null);
-        Collection<Bundle> bundles = a.findProviderBundles("org.apache.aries.mytest.MySPI");
-        assertEquals(1, bundles.size());
-        assertSame(implBundle, bundles.iterator().next());
-
-        // The bc.registerService() call should now have been made
-        EasyMock.verify(implBC);
-
-        // Call removedBundle();
-        customizer.removedBundle(implBundle, null, registrations);
-        // sreg.unregister() should have been called.
-        EasyMock.verify(sreg);
+        return implBundle;
     }
 }
