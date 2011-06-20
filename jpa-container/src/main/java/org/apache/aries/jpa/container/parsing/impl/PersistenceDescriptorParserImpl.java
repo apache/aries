e3ee6b101a6f295bfdde537d049345e290458ef0
Valentin Mahrwald <mahrwald@apache.org>
2011-06-20 15:54:15 +0000

    ARIES-582: Put IOUtils and RememberingInputStream
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk/jpa@1137671 13f79535-47bb-0310-9956-ffa450edef68

--- jpa-container/src/main/java/org/apache/aries/jpa/container/parsing/impl/PersistenceDescriptorParserImpl.java
+++ jpa-container/src/main/java/org/apache/aries/jpa/container/parsing/impl/PersistenceDescriptorParserImpl.java
@@ -20,7 +20,6 @@
 package org.apache.aries.jpa.container.parsing.impl;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -32,6 +31,7 @@ import org.apache.aries.jpa.container.parsing.ParsedPersistenceUnit;
 import org.apache.aries.jpa.container.parsing.PersistenceDescriptor;
 import org.apache.aries.jpa.container.parsing.PersistenceDescriptorParser;
 import org.apache.aries.jpa.container.parsing.PersistenceDescriptorParserException;
+import org.apache.aries.util.io.RememberingInputStream;
 import org.osgi.framework.Bundle;
 
 /**
@@ -40,152 +40,13 @@ import org.osgi.framework.Bundle;
  */
 public class PersistenceDescriptorParserImpl implements PersistenceDescriptorParser {
 
-  /**
-   * This class is used internally to prevent the first pass parse from
-   * closing the InputStream when it exits.
-   */
-  private static class RememberingInputStream extends InputStream {
-
-    /** The size by which to grow our array */
-    private static final int bufferGrowthSize = 0x4000;
-    /** The bytes that have been read so far */
-    private byte[] bytes = new byte[bufferGrowthSize];
-    /** Index of the next empty entry in the array */
-    private int pos = 0;
-    /** The input stream that actually holds the data */
-    private final InputStream stream;
-    /** Index of the last valid byte in the byte array */
-    private int maxRead = -1;
-    /** The point to reset to */
-    private int markPoint = -1;
-    
-    
-    public RememberingInputStream(InputStream in) throws IOException{
-      stream = in;
-      // Pre fill with data that we know we're going to need - it's 
-      // more efficient than the single byte reads are - hopefully
-      // someone reading a lot of data will do reads in bulk
-      
-      maxRead = stream.read(bytes) - 1;
-    }
-
-    @Override
-    public int read() throws IOException {
-      
-      if(pos <= maxRead)
-      {
-        //We can't return the byte directly, because it is signed
-        //We can pretend this is an unsigned byte by using boolean
-        //& to set the low end byte of an int.
-        return bytes[pos++] & 0xFF;
-      } else {
-        int i = stream.read();
-        if(i<0)
-          return i;
-      
-        ensureCapacity(0);
-        bytes[pos++] = (byte) i;
-        return i;
-      }
-    }
-
-    /**
-     * Ensure our internal byte array can hold enough data
-     * @param i one less than the number of bytes that need
-     *          to be held.
-     */
-    private void ensureCapacity(int i) {
-      if((pos + i) >= bytes.length) {
-        byte[] tmp = bytes;
-        int newLength = bytes.length + bufferGrowthSize;
-        while(newLength < pos + i) {
-          newLength += bufferGrowthSize;
-        }
-        bytes = new byte[newLength];
-        System.arraycopy(tmp, 0, bytes, 0, (maxRead >= pos) ? maxRead + 1 : pos);
-      }
-    }
-
-    @Override
-    public int read(byte[] b) throws IOException {
-      return read(b, 0, b.length);
-    }
-
-    @Override
-    public int read(byte[] b, int off, int len) throws IOException {
-      if(pos <= maxRead) {
-        if(pos + len <= maxRead)
-        {
-          System.arraycopy(bytes, pos, b, off, len);
-          pos += len;
-          return len;
-        } else {
-          int lengthLeftOfBuffer = (maxRead - pos) + 1;
-          System.arraycopy(bytes, pos, b, off, lengthLeftOfBuffer);
-          int read = stream.read(b, off + lengthLeftOfBuffer, len - lengthLeftOfBuffer);
-          if(read < 0) {
-            pos += lengthLeftOfBuffer;
-            return lengthLeftOfBuffer;
-          }
-          ensureCapacity(lengthLeftOfBuffer + read - 1);
-          System.arraycopy(b, off + lengthLeftOfBuffer, bytes, maxRead + 1, read);
-          pos +=  (lengthLeftOfBuffer + read);
-          return lengthLeftOfBuffer + read;
-        }
-      } else {
-        int i = stream.read(b, off, len);
-        if(i<0)
-          return i;
-        ensureCapacity(i - 1);
-        System.arraycopy(b, off, bytes, pos, i);
-        pos += i;
-        return i;
-      }
-    }
-
-    @Override
-    public long skip(long n) throws IOException {
-      throw new IOException("Skip is unsupported");
-    }
-
-    @Override
-    public int available() throws IOException {
-      if(pos <= maxRead) 
-        return (maxRead - pos) + 1;
-      else 
-        return stream.available(); 
-    }
-
-    @Override
-    public synchronized void mark(int readlimit) {
-      markPoint = pos;
-    }
-
-    @Override
-    public synchronized void reset() throws IOException {
-      if(maxRead < pos)
-        maxRead = pos - 1;
-      pos = markPoint;
-    }
-
-    @Override
-    public boolean markSupported() {
-      return true;
-    }
-
-    @Override
-    public void close() throws IOException {
-      //No op, don't close the parent.
-    }
-  }
-  
   /* (non-Javadoc)
    * @see org.apache.aries.jpa.container.parsing.impl.PersistenceDescriptorParser#parse(org.osgi.framework.Bundle, org.apache.aries.jpa.container.parsing.PersistenceDescriptor)
    */
   public Collection<? extends ParsedPersistenceUnit> parse(Bundle b, PersistenceDescriptor descriptor) throws PersistenceDescriptorParserException {
     Collection<ParsedPersistenceUnit> persistenceUnits = new ArrayList<ParsedPersistenceUnit>();
     SAXParserFactory parserFactory = SAXParserFactory.newInstance();
-    InputStream is = null;
+    RememberingInputStream is = null;
     boolean schemaFound = false;
     try {
       //Buffer the InputStream so we can mark it, though we'll be in 
@@ -220,7 +81,7 @@ public class PersistenceDescriptorParserImpl implements PersistenceDescriptorPar
           + " in bundle " + b.getSymbolicName() + "_" + b.getVersion(), e);
     } finally {
       if(is != null) try {
-        is.close();
+        is.closeUnderlying();
       } catch (IOException e) {
         //No logging necessary, just consume
       }
