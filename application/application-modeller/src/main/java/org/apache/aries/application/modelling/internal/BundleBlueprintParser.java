04290f8418e3d8b3f969ddaa6669dfeae9082773
Valentin Mahrwald <mahrwald@apache.org>
2011-06-21 15:41:11 +0000

    ARIES-682: Sanitize names coming out of file-based IFiles (plus a bit more trace).
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk@1138057 13f79535-47bb-0310-9956-ffa450edef68

--- application/application-modeller/src/main/java/org/apache/aries/application/modelling/internal/BundleBlueprintParser.java
+++ application/application-modeller/src/main/java/org/apache/aries/application/modelling/internal/BundleBlueprintParser.java
@@ -23,6 +23,10 @@ import java.util.regex.Pattern;
 
 import org.apache.aries.util.manifest.BundleManifest;
 import org.apache.aries.util.manifest.ManifestHeaderProcessor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.apache.aries.application.utils.AppConstants.*;
 
 /**
  * A bundle may contain a Bundle-Blueprint: header as per p649 of the v4 spec. If present, 
@@ -36,6 +40,7 @@ import org.apache.aries.util.manifest.ManifestHeaderProcessor;
 public class BundleBlueprintParser {
   
   public static final String DEFAULT_HEADER = "OSGI-INF/blueprint/*.xml";
+  private static final Logger logger = LoggerFactory.getLogger(BundleBlueprintParser.class);
   
   String _mfHeader = null;
   List<Path> _paths;
@@ -81,7 +86,7 @@ public class BundleBlueprintParser {
    * @param bundleMf BundleManifest to construct the parser from
    */
   public BundleBlueprintParser (BundleManifest bundleMf) {
-    String bundleBPHeader = (String) bundleMf.getRawAttributes().getValue("Bundle-Blueprint");
+    String bundleBPHeader = bundleMf.getRawAttributes().getValue("Bundle-Blueprint");
     setup (bundleBPHeader);
   }
 
@@ -107,6 +112,8 @@ public class BundleBlueprintParser {
     } else { 
       _mfHeader = bundleBPHeader;
     }
+    
+    logger.debug("Bundle-Blueprint header: {}", _mfHeader);
     
     // Break this comma separated list up
     List<String> files = ManifestHeaderProcessor.split(_mfHeader, ",");
@@ -150,12 +157,18 @@ public class BundleBlueprintParser {
    *  @return true if this is a blueprint file according to the Bundle-Blueprint header
    */
   public boolean isBPFile (String directory, String filename) { 
+    logger.debug(LOG_ENTRY, "isBPFile", new Object[] {directory, filename});
+      
+    boolean result=false;
     for (Path path: _paths) { 
       if (path.matches(directory, filename)) { 
-        return true;
+        result = true;
+        break;
       }
     }
-    return false;
+    
+    logger.debug(LOG_EXIT, "isBPFile", result);
+    return result;
   }
        
 }
