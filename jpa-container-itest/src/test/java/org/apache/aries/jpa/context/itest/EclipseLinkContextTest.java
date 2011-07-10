c0bb146251812496b45665a2db660b5f93be5f59
Valentin Mahrwald <mahrwald@apache.org>
2011-07-10 20:05:08 +0000

    ARIES-707: Remove some duplicated code from itests
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk/jpa@1144949 13f79535-47bb-0310-9956-ffa450edef68

--- jpa-container-itest/src/test/java/org/apache/aries/jpa/context/itest/EclipseLinkContextTest.java
+++ jpa-container-itest/src/test/java/org/apache/aries/jpa/context/itest/EclipseLinkContextTest.java
@@ -1,3 +1,18 @@
+/*  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.apache.aries.jpa.context.itest;
 
 import org.junit.runner.RunWith;
@@ -6,6 +21,7 @@ import org.ops4j.pax.exam.junit.Configuration;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
 
 import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.apache.aries.itest.ExtraOptions.*;
 
 @RunWith(JUnit4TestRunner.class)
 public class EclipseLinkContextTest extends JPAContextTest {
