fab006f5e4ab7c8a3f479fc9fe651d2706b4de8d
Timothy James Ward <timothyjward@apache.org>
2011-07-26 19:35:49 +0000

    ARIES-691 : Allow EclipseLink adapter to work with multiple EclipseLink bundles concurrently
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk/jpa@1151218 13f79535-47bb-0310-9956-ffa450edef68

--- jpa-container-eclipselink-adapter/src/main/java/org/apache/aries/jpa/eclipselink/adapter/Activator.java
+++ jpa-container-eclipselink-adapter/src/main/java/org/apache/aries/jpa/eclipselink/adapter/Activator.java
@@ -18,30 +18,19 @@
  */
 package org.apache.aries.jpa.eclipselink.adapter;
 
-import java.lang.reflect.Constructor;
-import java.net.URL;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Hashtable;
 import java.util.List;
-import java.util.Map;
-import java.util.Properties;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.SharedCacheMode;
-import javax.persistence.ValidationMode;
-import javax.persistence.spi.ClassTransformer;
 import javax.persistence.spi.PersistenceProvider;
-import javax.persistence.spi.PersistenceUnitInfo;
-import javax.persistence.spi.PersistenceUnitTransactionType;
-import javax.persistence.spi.ProviderUtil;
-import javax.sql.DataSource;
 
-import org.apache.aries.jpa.eclipselink.adapter.platform.OSGiTSServer;
+import org.apache.aries.jpa.eclipselink.adapter.EclipseLinkProviderService;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
@@ -67,152 +56,14 @@ import org.slf4j.LoggerFactory;
  * </ul>
  */
 public class Activator implements BundleActivator, BundleListener {
-    private static final String ECLIPSELINK_JPA_PROVIDER_BUNDLE_SYMBOLIC_NAME = "org.eclipse.persistence.jpa";
-    private static final String ECLIPSELINK_JPA_PROVIDER_CLASS_NAME = "org.eclipse.persistence.jpa.PersistenceProvider";
+    public static final String ECLIPSELINK_JPA_PROVIDER_BUNDLE_SYMBOLIC_NAME = "org.eclipse.persistence.jpa";
+    public static final String ECLIPSELINK_JPA_PROVIDER_CLASS_NAME = "org.eclipse.persistence.jpa.PersistenceProvider";
     private final ConcurrentMap<Bundle, ServiceRegistration> registeredProviders = new ConcurrentHashMap<Bundle, ServiceRegistration>();
     
     private static final Logger logger = LoggerFactory.getLogger(Activator.class);
     
     private ServiceTracker tracker;
     private BundleContext context;
-    
-    /**
-     * Wrapper {@link PersistenceUnitInfo} object that adds the eclipselink.target-server setting (if not present)
-     * and makes sure we can load {@link OSGiTSServer} from the unit's classloader.
-     */
-    private static class PersistenceUnitProxyWithTargetServer implements PersistenceUnitInfo {
-        private final PersistenceUnitInfo delegate;
-        
-        public PersistenceUnitProxyWithTargetServer(PersistenceUnitInfo info) {
-            delegate = info;
-        }
-
-        public void addTransformer(ClassTransformer arg0) {
-            delegate.addTransformer(arg0);
-        }
-
-        public boolean excludeUnlistedClasses() {
-            return delegate.excludeUnlistedClasses();
-        }
-
-        public ClassLoader getClassLoader() {
-            ClassLoader unionClassLoader = new ClassLoader(delegate.getClassLoader()) {
-                protected Class<?> findClass(String name) throws ClassNotFoundException {
-                    return Activator.class.getClassLoader().loadClass(name);
-                }
-            };
-            
-            return unionClassLoader;
-        }
-
-        public List<URL> getJarFileUrls() {
-            return delegate.getJarFileUrls();
-        }
-
-        public DataSource getJtaDataSource() {
-            return delegate.getJtaDataSource();
-        }
-
-        public List<String> getManagedClassNames() {
-            return delegate.getManagedClassNames();
-        }
-
-        public List<String> getMappingFileNames() {
-            return delegate.getMappingFileNames();
-        }
-
-        public ClassLoader getNewTempClassLoader() {
-            return delegate.getNewTempClassLoader();
-        }
-
-        public DataSource getNonJtaDataSource() {
-            return delegate.getNonJtaDataSource();
-        }
-
-        public String getPersistenceProviderClassName() {
-            return delegate.getPersistenceProviderClassName();
-        }
-
-        public String getPersistenceUnitName() {
-            return delegate.getPersistenceUnitName();
-        }
-
-        public URL getPersistenceUnitRootUrl() {
-            return delegate.getPersistenceUnitRootUrl();
-        }
-
-        public String getPersistenceXMLSchemaVersion() {
-            return delegate.getPersistenceXMLSchemaVersion();
-        }
-
-        public Properties getProperties() {
-            Properties props = delegate.getProperties();
-            
-            if (props == null) {
-                props = new Properties();
-            }
-            
-            if (!!!props.containsKey("eclipselink.target-server")) {
-                props.put("eclipselink.target-server", OSGiTSServer.class.getName());
-            }
-            
-            return props;
-        }
-
-        public SharedCacheMode getSharedCacheMode() {
-            return delegate.getSharedCacheMode();
-        }
-
-        public PersistenceUnitTransactionType getTransactionType() {
-            return delegate.getTransactionType();
-        }
-
-        public ValidationMode getValidationMode() {
-            return delegate.getValidationMode();
-        }
-    }
-  
-    /**
-     * Service factory for generating the Eclipselink OSGi compatible provider. It proxies the provider so that
-     * we can go in at entity manager creation time and set the eclipselink target-server to be {@link OSGiTSServer}.
-     */
-    private static class EclipseLinkProviderService implements ServiceFactory {
-        private final Bundle eclipseLinkJpaBundle;
-        
-        public EclipseLinkProviderService(Bundle b) {
-            eclipseLinkJpaBundle = b;
-        }
-        
-        public Object getService(Bundle bundle, ServiceRegistration registration) {
-            logger.debug("Requested EclipseLink Provider service");
-            
-            try {
-                Class<? extends PersistenceProvider> providerClass = eclipseLinkJpaBundle.loadClass(ECLIPSELINK_JPA_PROVIDER_CLASS_NAME);
-                Constructor<? extends PersistenceProvider> con = providerClass.getConstructor();
-                final PersistenceProvider provider = con.newInstance();
-                
-                return new PersistenceProvider() {
-                    public ProviderUtil getProviderUtil() {
-                        return provider.getProviderUtil();
-                    }
-                    
-                    public EntityManagerFactory createEntityManagerFactory(String arg0, Map arg1) {
-                        return provider.createEntityManagerFactory(arg0, arg1);
-                    }
-                    
-                    public EntityManagerFactory createContainerEntityManagerFactory(PersistenceUnitInfo punit, Map props) {
-                        return provider.createContainerEntityManagerFactory(new PersistenceUnitProxyWithTargetServer(punit), props);
-                    }
-                };
-                
-            } catch (Exception e) {
-                logger.error("Got exception trying to instantiate the EclipseLink provider", e);
-                return null;                
-            }
-        }
-
-        public void ungetService(Bundle bundle, ServiceRegistration registration, Object service) {}
-    }
     
     public void start(BundleContext ctx) {
         logger.debug("Starting EclipseLink adapter");
