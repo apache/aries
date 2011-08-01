9020b4f1a59bc155f070fb6662c0524825593a3e
Timothy James Ward <timothyjward@apache.org>
2011-08-01 11:08:00 +0000

    ARIES-718: Add EJB modelling support
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk/ejb/ejb-modeller@1152738 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/aries/ejb/modelling/impl/EJBModeller.java
@@ -0,0 +1,74 @@
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.aries.ejb.modelling.impl;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.apache.aries.application.modelling.ModellerException;
+import org.apache.aries.application.modelling.ParsedServiceElements;
+import org.apache.aries.application.modelling.ServiceModeller;
+import org.apache.aries.ejb.modelling.EJBLocator;
+import org.apache.aries.util.filesystem.IDirectory;
+import org.apache.aries.util.manifest.BundleManifest;
+import org.apache.aries.util.manifest.ManifestHeaderProcessor;
+import org.apache.aries.util.manifest.ManifestHeaderProcessor.NameValuePair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EJBModeller implements ServiceModeller {
+
+  private static final Logger logger = LoggerFactory.getLogger(EJBModeller.class);
+  
+  private EJBLocator locator;
+  
+  public void setLocator(EJBLocator locator) {
+    this.locator = locator;
+  }
+  
+  public ParsedServiceElements modelServices(BundleManifest manifest, IDirectory bundle) 
+    throws ModellerException {
+    logger.trace("modelServices", new Object[] {manifest, bundle});
+    ParsedEJBServices ejbServices = new ParsedEJBServices();
+    
+    String header = manifest.getRawAttributes().getValue("Export-EJB");
+    logger.debug("Export-EJB header is " + header);
+    
+    if(header == null || "".equals(header))
+      return ejbServices;
+    
+    Collection<String> allowedNames = getNames(header);
+    
+    if(allowedNames.contains("NONE"))
+      return ejbServices;
+
+    ejbServices.setAllowedNames(allowedNames);
+    locator.findEJBs(manifest, bundle, ejbServices);
+    
+    logger.trace("modelServices", ejbServices);
+    return ejbServices;
+  }
+
+  private Collection<String> getNames(String header) {
+    Collection<String> names = new ArrayList<String>();
+    for(NameValuePair nvp: ManifestHeaderProcessor.parseExportString(header)){
+      names.add(nvp.getName().trim());
+    }
+    return names;
+  }
+
+}
---
+++ src/main/java/org/apache/aries/ejb/modelling/impl/ParsedEJBServices.java
@@ -0,0 +1,71 @@
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.aries.ejb.modelling.impl;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.aries.application.modelling.ExportedService;
+import org.apache.aries.application.modelling.ImportedService;
+import org.apache.aries.application.modelling.ParsedServiceElements;
+import org.apache.aries.ejb.modelling.EJBRegistry;
+
+public class ParsedEJBServices implements ParsedServiceElements, EJBRegistry {
+
+  private final Collection<ImportedService> references;
+  private final Collection<ExportedService> services;
+  
+  private boolean all;
+  private Set<String> allowedNames; 
+  
+  public ParsedEJBServices() {
+    this.references = Collections.emptyList();
+    this.services = new ArrayList<ExportedService>();
+    allowedNames = new HashSet<String>();
+    all = false;
+  }
+
+  public Collection<ImportedService> getReferences() {
+    return references;
+  }
+
+  public Collection<ExportedService> getServices() {
+    return Collections.unmodifiableCollection(services);
+  }
+  
+  public void setAllowedNames(Collection<String> names) {
+    if(names.contains("NONE")) {
+      all= false;
+      allowedNames.clear();
+      return;
+    }
+    if(names.contains("ALL")) {
+      all = true;
+      return;
+    }
+    allowedNames.addAll(names);
+  }
+  
+  public void addEJBView(String ejbName, String ejbType, String interfaceName,
+      boolean remote) {
+    if(all || allowedNames.contains(ejbName))
+      services.add(new EJBServiceExport(ejbName, ejbType, interfaceName, remote));
+  }
+}
