b1d758718556e4bf09d5246fef1df5a28501c410
John Ross <jwross@apache.org>
2011-08-15 13:39:06 +0000

    ARIES-728: Check in initial baseline for subsystems implementation based on latest RFCs.
    org.apache.aries.subsystem.api
    
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk@1157841 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subsystem/subsystem-api/src/main/java/org/osgi/service/repository/ContentNamespace.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) OSGi Alliance (2011). All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.osgi.service.repository;
+
+/**
+ * Constants for use in the "osgi.content" namespace. This namespace is used to
+ * locate content via the
+ * {@link Repository#findProviders(org.osgi.framework.wiring.Requirement...)}
+ * method.
+ */
+public interface ContentNamespace {
+  /**
+   * Namespace of the content capability
+   */
+  final String CAPABILITY = "osgi.content";
+
+  /**
+   * Checksum attribute of a resource
+   */
+  final String CHECKSUM_ATTRIBUTE = "checksum";
+
+  /**
+   * The checksum algorithm used to calculate the {@link #CHECKSUM_ATTRIBUTE} if
+   * not specified this is assumed to be SHA-256 - TODO need default?
+   */
+  final String CHECKSUM_ALGO_ATTRIBUTE = "checksumAlgo";
+
+  /**
+   * A copyright statement for the resource
+   */
+  final String COPYRIGHT_ATTRIBUTE = "copyright";
+
+  /**
+   * A human readable description of this resource
+   */
+  final String DESCRIPTION_ATTRIBUTE = "description";
+
+  /**
+   * A URL where documentation for this resource can be accessed
+   */
+  final String DOCUMENTATION_URL_ATTRIBUTE = "documentation";
+
+  /**
+   * Provides an optional machine readable form of license information. See
+   * section 3.2.1.10 of the OSGi Core Specification for information on it's
+   * usage.
+   */
+  final String LICENSE_ATTRIBUTE = "license";
+
+  /**
+   * A URL where source control management for this resource is located
+   */
+  final String SCM_URL_ATTRIBUTE = "scm";
+
+  /**
+   * The size of this resource in bytes.
+   */
+  final String SIZE_ATTRIBUTE = "size";
+
+  /**
+   * A URL where source code for this resource is located
+   */
+  final String SOURCE_URL_ATTRIBUTE = "source";
+
+  /**
+   * All attributes defined in this interface
+   */
+  final String[] ATTRIBUTES = { CHECKSUM_ATTRIBUTE, CHECKSUM_ALGO_ATTRIBUTE,
+      COPYRIGHT_ATTRIBUTE, DESCRIPTION_ATTRIBUTE, DOCUMENTATION_URL_ATTRIBUTE,
+      LICENSE_ATTRIBUTE, SCM_URL_ATTRIBUTE, SIZE_ATTRIBUTE,
+      SOURCE_URL_ATTRIBUTE };
+}
--- subsystem/subsystem-api/src/main/java/org/osgi/service/subsystem/SubsystemConstants.java
+++ subsystem/subsystem-api/src/main/java/org/osgi/service/subsystem/SubsystemConstants.java
@@ -1,4 +1,6 @@
 /*
+ * Copyright (c) OSGi Alliance (2011). All Rights Reserved.
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -11,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.aries.subsystem;
+package org.osgi.service.subsystem;
 
 /*
  * TODO
@@ -29,73 +31,11 @@ package org.apache.aries.subsystem;
  * 		<li>Attributes used in the above headers</li>
  * </ol>
  */
-public interface SubsystemConstants {
-	/**
-	 * The subsystem lifecycle event types that can be produced by a subsystem. 
-	 * See ? and SubsystemAdmin for details on the circumstances under which 
-	 * these events are fired.
-	 */
-	public static enum EventType {
-		/**
-		 * Event type used to indicate that the operations was cancelled (e.g. 
-		 * an install was cancelled).
-		 */
-		CANCELED,
-		/**
-		 * Event type used to indicate that a subsystem operation is being 
-		 * cancelled.
-		 */
-		CANCELING,
-		/**
-		 * Event type used to indicate that the operation failed (e.g. an 
-		 * exception was thrown during installation).
-		 */
-		FAILED,
-		/**
-		 * Event type used to indicate a subsystem has been installed.
-		 */
-		INSTALLED,
-		/**
-		 * Event type used to indicate a subsystem is installing.
-		 */
-		INSTALLING,
-		/**
-		 * Event type used to indicate a subsystem has been resolved.
-		 */
-		RESOLVED,
-		/**
-		 * Event type used to indicate a subsystem is resolving.
-		 */
-		RESOLVING,
-		/**
-		 * Event type used to indicate a subsystem has been started.
-		 */
-		STARTED,
-		/**
-		 * Event type used to indicate a subsystem is starting.
-		 */
-		STARTING,
-		/**
-		 * Event type used to indicate a subsystem has been stopped.
-		 */
-		STOPPED,
-		/**
-		 * Event type used to indicate a subsystem is stopping.
-		 */
-		STOPPING,
-		/**
-		 * Event type used to indicate a subsystem has been uninstalled.
-		 */
-		UNINSTALLED,
-		/**
-		 * Event type used to indicate a subsystem has been updated.
-		 */
-		UPDATED,
-		/**
-		 * Event type used to indicate a subsystem is updating.
-		 */
-		UPDATING
+public class SubsystemConstants {
+	private SubsystemConstants() {
+		throw new AssertionError("This class is not designed to be instantiated");
 	}
+	
 	/**
 	 * The list of application contents identified by a symbolic name and 
 	 * version.
@@ -286,11 +226,11 @@ public interface SubsystemConstants {
 	/**
 	 * Identify resources that are bundles (this is the default type).
 	 */
-	public static final String RESOURCE_NAMESPACE_BUNDLE = "osgi.bundle";
+	public static final String RESOURCE_NAMESPACE_BUNDLE = "";
 	/**
 	 * Identify resources that are subsystems.
 	 */
-	public static final String RESOURCE_NAMESPACE_SUBSYSTEM = "osgi.subsystem";
+	public static final String RESOURCE_NAMESPACE_SUBSYSTEM = "";
 	/**
 	 * Attribute to indicate a bundle needs to be started, defaults to true.
 	 */
@@ -329,7 +269,77 @@ public interface SubsystemConstants {
 	public static final String SUBSYSTEM_DIRECTIVE = "";
 	
 	// Event related constants.
-	
+
+	/**
+	 * The subsystem lifecycle event types that can be produced by a subsystem. 
+	 * See ? and Subsystem for details on the circumstances under which these 
+	 * events are fired.
+	 */
+	public static enum EventType {
+		/**
+		 * Event type used to indicate a subsystem is installing.
+		 */
+		INSTALLING,
+		/**
+		 * Event type used to indicate a subsystem has been installed.
+		 */
+		INSTALLED,
+		/**
+		 * Event type used to indicate a subsystem is resolving.
+		 */
+		RESOLVING,
+		/**
+		 * Event type used to indicate a subsystem has been resolved.
+		 */
+		RESOLVED,
+		/**
+		 * Event type used to indicate a subsystem is starting.
+		 */
+		STARTING,
+		/**
+		 * Event type used to indicate a subsystem has been started.
+		 */
+		STARTED,
+		/**
+		 * Event type used to indicate a subsystem is stopping.
+		 */
+		STOPPING,
+		/**
+		 * Event type used to indicate a subsystem has been stopped.
+		 */
+		STOPPED,
+		/**
+		 * Event type used to indicate a subsystem is updating.
+		 */
+		UPDATING,
+		/**
+		 * Event type used to indicate a subsystem has been updated.
+		 */
+		UPDATED,
+		/**
+		 * Event type used to indicate a subsystem is uninstalling.
+		 */
+		UNINSTALLING,
+		/**
+		 * Event type used to indicate a subsystem has been uninstalled.
+		 */
+		UNINSTALLED,
+		/**
+		 * Event type used to indicate that a subsystem operation is being 
+		 * cancelled.
+		 */
+		CANCELING,
+		/**
+		 * Event type used to indicate that the operations was cancelled (e.g. 
+		 * an install was cancelled).
+		 */
+		CANCELED,
+		/**
+		 * Event type used to indicate that the operation failed (e.g. an 
+		 * exception was thrown during installation).
+		 */
+		FAILED
+	}
 	/**
 	 * Key for the event property that holds the subsystem id.
 	 */
@@ -339,55 +349,23 @@ public interface SubsystemConstants {
 	 */
 	public static final String SUBSYSTEM_LOCATION = "subsystem.location";
 	/**
+	 * Key for the event property that holds the subsystem state.
+	 */
+	public static final String SUBYSTEM_STATE = "subsystem.state";
+	/**
 	 * Key for the event property that holds the subsystem symbolic name.
 	 */
-	public static final String SUBSYSTEM_SYMBOLICNAME = "Subsystem-SymbolicName";
+	public static final String SUBSYSTEM_SYMBOLICNAME = "subsystem.symbolicname";
 	/**
 	 * Key for the event property that holds the subsystem version.
 	 */
-	public static final String SUBSYSTEM_VERSION = "Subsystem-Version";
+	public static final String SUBSYSTEM_VERSION = "subsystem.version";
 	/**
 	 * The topic for subsystem event admin events.
 	 */
-	public static final String TOPIC = "org/osgi/service/Subsystem";
+	public static final String TOPIC = "org/osgi/service/Subsystem/";
 	/**
 	 * The topic for subsystem internal event admin events.
 	 */
-	public static final String TOPIC_INTERNALS = "org/osgi/service/SubsystemInternals";
-	/**
-     * Location to use when updating the subsystem.
-     */
-    public static final String SUBSYSTEM_UPDATELOCATION = "Subsystem-UpdateLocation";
-    /**
-     * The list of subsystem contents identified by a symbolic name and version.
-     */
-    public static final String SUBSYSTEM_CONTENT             = "Subsystem-Content";
-    /**
-     * The packages to be imported into the subsystem for use by the subsystem contents.
-     */
-    public static final String SUBSYSTEM_IMPORTPACKAGE       = "Subsystem-ImportPackage";
-    /**
-     * The packages to be exported by the subsystem for use outside the
-     * subsystem. The packages declarations must match a package provide by a
-     * bundle listed in the subsystem content.
-     */
-    public static final String SUBSYSTEM_EXPORTPACKAGE       = "Subsystem-ExportPackage";
-    /**
-     * TODO: what is this for?
-     */
-    public static final String SUBSYSTEM_RESOURCES           = "Subsystem-Resources";
-    /**
-     * The subsystem manifest version header must be present and equals to 1.0
-     * for this version of subsystems.
-     */
-    public static final String SUBSYSTEM_MANIFESTVERSION     = "Subsystem-ManifestVersion";
-    /**
-     * A list of service filters used to identify services that this subsystem requires.
-     */
-    public static final String SUBSYSTEM_IMPORTSERVICE       = "Subsystem-ImportService";
-    /**
-     * A list of service filters used to identify the services provided by
-     * bundles inside the subsystem that can be exported outside the subsystem.
-     */
-    public static final String SUBSYSTEM_EXPORTSERVICE       = "Subsystem-ExportService";
+	public static final String TOPIC_INTERNALS = "org/osgi/service/SubsystemInternals/";
 }
