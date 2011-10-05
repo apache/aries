dea459a4fa9094016c8674ebdd9c9101c6008edc
Timothy James Ward <timothyjward@apache.org>
2011-10-05 16:11:40 +0000

    ARIES-754: Update JPA itests to use core blueprint bundles
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk/jpa@1179289 13f79535-47bb-0310-9956-ffa450edef68

--- jpa-container-itest/src/test/java/org/apache/aries/jpa/context/itest/EclipseLinkStartupTest.java
+++ jpa-container-itest/src/test/java/org/apache/aries/jpa/context/itest/EclipseLinkStartupTest.java
@@ -56,7 +56,8 @@ public class EclipseLinkStartupTest extends AbstractIntegrationTest {
                 mavenBundle("org.apache.aries", "org.apache.aries.util"),
                 // Adding blueprint to the runtime is a hack to placate the
                 // maven bundle plugin.
-                mavenBundle("org.apache.aries.blueprint", "org.apache.aries.blueprint"),
+                mavenBundle("org.apache.aries.blueprint", "org.apache.aries.blueprint.api"),
+                mavenBundle("org.apache.aries.blueprint", "org.apache.aries.blueprint.core"),
                 mavenBundle("asm", "asm-all"),
                 mavenBundle("org.apache.aries.proxy", "org.apache.aries.proxy.api"),
                 mavenBundle("org.apache.aries.proxy", "org.apache.aries.proxy.impl"),
