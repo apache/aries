1cd047eb0db5cbd9d68ea68eea1f582411803d08
A. J. David Bosschaert <davidb@apache.org>
2011-10-26 15:11:41 +0000

    Add configuration to Pax Exam to allow it to run under Equinox 3.7 (OSGi 4.3). Changed BundleStateMBeanTest to use it.
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk@1189260 13f79535-47bb-0310-9956-ffa450edef68

--- jmx/jmx-itests/src/test/java/org/apache/aries/jmx/framework/BundleStateMBeanTest.java
+++ jmx/jmx-itests/src/test/java/org/apache/aries/jmx/framework/BundleStateMBeanTest.java
@@ -16,6 +16,9 @@
  */
 package org.apache.aries.jmx.framework;
 
+import static org.apache.aries.itest.ExtraOptions.mavenBundle;
+import static org.apache.aries.itest.ExtraOptions.paxLogging;
+import static org.apache.aries.itest.ExtraOptions.testOptions;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
@@ -24,8 +27,6 @@ import static org.ops4j.pax.exam.CoreOptions.provision;
 import static org.ops4j.pax.swissbox.tinybundles.core.TinyBundles.newBundle;
 import static org.ops4j.pax.swissbox.tinybundles.core.TinyBundles.withBnd;
 import static org.osgi.jmx.framework.BundleStateMBean.OBJECTNAME;
-
-import static org.apache.aries.itest.ExtraOptions.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -41,10 +42,15 @@ import org.apache.aries.jmx.codec.BundleData.Header;
 import org.junit.Test;
 import org.ops4j.pax.exam.CoreOptions;
 import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.container.def.PaxRunnerOptions;
 import org.ops4j.pax.exam.junit.Configuration;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.Constants;
+import org.osgi.framework.ServiceReference;
+import org.osgi.framework.Version;
 import org.osgi.jmx.framework.BundleStateMBean;
+import org.osgi.service.packageadmin.ExportedPackage;
+import org.osgi.service.packageadmin.PackageAdmin;
 
 /**
  * @version $Rev$ $Date$
@@ -54,9 +60,13 @@ public class BundleStateMBeanTest extends AbstractIntegrationTest {
     @Configuration
     public static Option[] configuration() {
         return testOptions(
-                        CoreOptions.equinox(),
+                        // new VMOption( "-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8000" ),
+                        // new TimeoutOption( 0 ),
+
+                        PaxRunnerOptions.rawPaxRunnerOption("config", "classpath:ss-runner.properties"),
+                        CoreOptions.equinox().version("3.7.0.v20110613"),
                         paxLogging("INFO"),
-                        
+
                         mavenBundle("org.apache.felix", "org.apache.felix.configadmin"),
                         mavenBundle("org.osgi", "org.osgi.compendium"),
                         mavenBundle("org.apache.aries.jmx", "org.apache.aries.jmx"),
@@ -102,7 +112,7 @@ public class BundleStateMBeanTest extends AbstractIntegrationTest {
                                 .build(withBnd()))
                         );
     }
-    
+
     @Override
     public void doSetUp() throws Exception {
         waitForMBean(new ObjectName(BundleStateMBean.OBJECTNAME));
@@ -110,39 +120,39 @@ public class BundleStateMBeanTest extends AbstractIntegrationTest {
 
     @Test
     public void testMBeanInterface() throws Exception {
-        
+
         BundleStateMBean mbean = getMBean(OBJECTNAME, BundleStateMBean.class);
         assertNotNull(mbean);
-        
+
         //get bundles
-        
+
         Bundle a = context().getBundleByName("org.apache.aries.jmx.test.bundlea");
         assertNotNull(a);
-        
+
         Bundle b = context().getBundleByName("org.apache.aries.jmx.test.bundleb");
         assertNotNull(b);
-        
+
         Bundle frag = context().getBundleByName("org.apache.aries.jmx.test.fragc");
         assertNotNull(frag);
 
         Bundle d = context().getBundleByName("org.apache.aries.jmx.test.bundled");
         assertNotNull(d);
-        
+
         // exportedPackages
-        
+
         String[] exports = mbean.getExportedPackages(a.getBundleId());
         assertEquals(2, exports.length);
-        
+
         List<String> packages = Arrays.asList(exports);
         assertTrue(packages.contains("org.apache.aries.jmx.test.bundlea.api;2.0.0"));
         assertTrue(packages.contains("org.apache.aries.jmx.test.fragmentc;0.0.0"));
-        
+
         //fragments
-        
+
         long[] fragments = mbean.getFragments(a.getBundleId());
         assertEquals(1, fragments.length);
         assertEquals(frag.getBundleId() , fragments[0]);
-       
+
         //headers
 
         TabularData headers = mbean.getHeaders(b.getBundleId());
@@ -150,109 +160,119 @@ public class BundleStateMBeanTest extends AbstractIntegrationTest {
         assertEquals(BundleStateMBean.HEADERS_TYPE, headers.getTabularType());
         assertTrue(headers.values().size() >= 4 );
         assertEquals("org.apache.aries.jmx.test.bundleb", Header.from(headers.get(new Object[] {Constants.BUNDLE_SYMBOLICNAME})).getValue());
-        
-        //hosts 
-        
+
+        //hosts
+
         long[] hosts = mbean.getHosts(frag.getBundleId());
         assertEquals(1, hosts.length);
         assertEquals(a.getBundleId() , hosts[0]);
-        
+
         //imported packages
-           
+
         String[] imports = mbean.getImportedPackages(a.getBundleId());
         assertTrue(imports.length >= 3);
         List<String> importedPackages = Arrays.asList(imports);
-        assertTrue(importedPackages.contains("org.osgi.framework;1.5.0"));
+
+        Version version = getPackageVersion("org.osgi.framework");
+        assertTrue(importedPackages.contains("org.osgi.framework;" + version.toString()));
         assertTrue(importedPackages.contains("org.apache.aries.jmx.test.bundleb.api;1.1.0"));
-        
+
         //last modified
-        
+
         assertTrue(mbean.getLastModified(b.getBundleId()) > 0);
-        
+
         //location
-        
+
         assertEquals(b.getLocation(), mbean.getLocation(b.getBundleId()));
-        
+
         //registered services
-        
+
         long[] serviceIds = mbean.getRegisteredServices(a.getBundleId());
         assertEquals(1, serviceIds.length);
-        
+
         //required bundles
-        
+
         long[] required = mbean.getRequiredBundles(d.getBundleId());
         assertEquals(1, required.length);
         assertEquals(a.getBundleId(), required[0]);
-        
+
         //requiring bundles
-        
+
         long[] requiring = mbean.getRequiringBundles(a.getBundleId());
         assertEquals(3, requiring.length);
         assertTrue(b.getSymbolicName(), arrayContains(b.getBundleId(), requiring));
         assertTrue(frag.getSymbolicName(), arrayContains(frag.getBundleId(), requiring));
         assertTrue(d.getSymbolicName(), arrayContains(d.getBundleId(), requiring));
-        
+
         //services in use
-        
+
         long[] servicesInUse = mbean.getServicesInUse(a.getBundleId());
         assertEquals(1, servicesInUse.length);
-        
+
         //start level
-        
+
         long startLevel = mbean.getStartLevel(b.getBundleId());
         assertTrue(startLevel >= 0);
-        
-        //state 
-        
+
+        //state
+
         assertEquals("ACTIVE", mbean.getState(b.getBundleId()));
-        
+
         //isFragment
-        
+
         assertFalse(mbean.isFragment(b.getBundleId()));
         assertTrue(mbean.isFragment(frag.getBundleId()));
-        
+
         //isRemovalPending
         assertFalse(mbean.isRemovalPending(b.getBundleId()));
-        
+
         // isRequired
-       
+
         assertTrue(mbean.isRequired(a.getBundleId()));
-        assertTrue(mbean.isRequired(b.getBundleId())); 
-        
+        assertTrue(mbean.isRequired(b.getBundleId()));
+
         // listBundles
-        
+
         TabularData bundlesTable = mbean.listBundles();
         assertNotNull(bundlesTable);
         assertEquals(BundleStateMBean.BUNDLES_TYPE, bundlesTable.getTabularType());
         assertEquals(bundleContext.getBundles().length, bundlesTable.values().size());
-        
-        
+
+
         // notifications
-        
+
         final List<Notification> received = new ArrayList<Notification>();
-      
+
         mbeanServer.addNotificationListener(new ObjectName(BundleStateMBean.OBJECTNAME), new NotificationListener() {
             public void handleNotification(Notification notification, Object handback) {
                received.add(notification);
             }
         }, null, null);
-        
+
         assertEquals(Bundle.ACTIVE, b.getState());
         b.stop();
         assertEquals(Bundle.RESOLVED, b.getState());
         b.start();
         assertEquals(Bundle.ACTIVE, b.getState());
-        
+
         int i = 0;
         while (received.size() < 2 && i < 3) {
             Thread.sleep(1000);
             i++;
         }
-        
+
         assertEquals(2, received.size());
-        
+
     }
-    
+
+    private Version getPackageVersion(String packageName) {
+        ServiceReference paRef = context().getServiceReference(PackageAdmin.class.getName());
+        PackageAdmin pa = (PackageAdmin) context().getService(paRef);
+        ExportedPackage pkg = pa.getExportedPackage(packageName);
+        Version version = pkg.getVersion();
+        return version;
+    }
+
     private static boolean arrayContains(long value, long[] values) {
         for (long i : values) {
             if (i == value) {
