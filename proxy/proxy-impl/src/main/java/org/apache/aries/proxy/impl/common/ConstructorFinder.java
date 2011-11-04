14c07d5d51a9c09fe3ee4be8184cf3c99e53bb19
Timothy James Ward <timothyjward@apache.org>
2011-11-04 19:12:30 +0000

    ARIES-775: Fix ClassCircularityError seen on Mac VMs when locating no-args constructors.
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk@1197720 13f79535-47bb-0310-9956-ffa450edef68

---
+++ proxy/proxy-impl/src/main/java/org/apache/aries/proxy/impl/common/ConstructorFinder.java
@@ -0,0 +1,53 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.aries.proxy.impl.common;
+
+import org.objectweb.asm.ClassAdapter;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Type;
+import org.objectweb.asm.commons.EmptyVisitor;
+
+import static org.objectweb.asm.Opcodes.ACC_PRIVATE;
+
+public class ConstructorFinder extends ClassAdapter
+{
+
+  private boolean hasNoArgsConstructor = false;
+  
+  public boolean hasNoArgsConstructor()
+  {
+    return hasNoArgsConstructor;
+  }
+
+  public ConstructorFinder()
+  {
+    super(new EmptyVisitor());
+  }
+
+  @Override
+  public MethodVisitor visitMethod(int access, String name, String desc, String signature,
+      String[] exceptions)
+  {
+    if("<init>".equals(name)) {
+      if(Type.getArgumentTypes(desc).length == 0 && (access & ACC_PRIVATE) == 0)
+        hasNoArgsConstructor = true;
+    }
+    return null;
+  }
+}
