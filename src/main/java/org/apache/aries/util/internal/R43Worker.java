8d114bc5cece41f5d6798d9f49ff3ef78e75e218
David Jencks <djencks@apache.org>
2011-11-10 01:57:21 +0000

    ARIES-777 Introduce a R43Worker to get classloader from bundle in r 4.3 frameworks. Also avoid an NPE I ran into
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk/util@1200093 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/aries/util/internal/R43Worker.java
@@ -0,0 +1,42 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+
+package org.apache.aries.util.internal;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.wiring.BundleWiring;
+
+/**
+ * @version $Rev:$ $Date:$
+ */
+public class R43Worker implements FrameworkUtilWorker {
+
+    static {
+        BundleWiring.class.getClassLoader();
+    }
+
+    public ClassLoader getClassLoader(Bundle b) {
+        return b.adapt(BundleWiring.class).getClassLoader();
+    }
+
+    public boolean isValid() {
+        return true;
+    }
+}
