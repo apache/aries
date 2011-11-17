385769180fa9d5a20f39918f6d69dfaeb3ce6b27
Daniel Kulp <dkulp@apache.org>
2011-11-17 16:00:21 +0000

    Get blueprint completely building error free in eclipse Reconfigure how pax-exam is setup in blueprint-itests so some of the tests will run from within eclipse
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk/blueprint/blueprint-jexl-evaluator@1203229 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/aries/blueprint/jexl/evaluator/JexlPropertyEvaluator.java
+++ src/main/java/org/apache/aries/blueprint/jexl/evaluator/JexlPropertyEvaluator.java
@@ -32,7 +32,6 @@ public class JexlPropertyEvaluator implements PropertyEvaluator {
     private JexlExpressionParser jexlParser;
     private Dictionary<String, String> properties;
     
-    @Override
     public String evaluate(String expression, Dictionary<String, String> properties) {
         JexlExpressionParser parser = getJexlParser();
         this.properties = properties;
@@ -60,63 +59,51 @@ public class JexlPropertyEvaluator implements PropertyEvaluator {
 
     private Map<String, Object> toMap() {
         return new Map<String, Object>() {
-            @Override
             public boolean containsKey(Object o) {
                 return properties.get(o) != null;
             }
             
-            @Override
             public Object get(Object o) {
                 return properties.get(o);
             }
             
             // following are not important
-            @Override
             public Object put(String s, Object o) {
                 throw new UnsupportedOperationException();
             }
             
-            @Override
             public int size() {
                 throw new UnsupportedOperationException();
             }
 
-            @Override
             public boolean isEmpty() {
                 throw new UnsupportedOperationException();
             }
 
-            @Override
             public boolean containsValue(Object o) {
                 throw new UnsupportedOperationException();
             }
 
-            @Override
             public Object remove(Object o) {
                 throw new UnsupportedOperationException();
             }
 
-            @Override
             public void putAll(Map<? extends String, ? extends Object> map) {
                 throw new UnsupportedOperationException();
             }
 
-            @Override
             public void clear() {
                 throw new UnsupportedOperationException();
             }
 
-            @Override
             public Set<String> keySet() {
                 throw new UnsupportedOperationException();
             }
 
-            @Override
             public Collection<Object> values() {
                 throw new UnsupportedOperationException();
             }
 
-            @Override
             public Set<Entry<String, Object>> entrySet() {
                 throw new UnsupportedOperationException();
             }
