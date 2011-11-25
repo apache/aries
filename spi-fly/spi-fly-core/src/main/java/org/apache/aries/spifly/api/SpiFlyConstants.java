be5d5b15cd1d8b5ebea679ae49506f4f35c0524a
A. J. David Bosschaert <davidb@apache.org>
2011-11-25 14:44:19 +0000

    Support for capability filtering on bundle versions.
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk@1206197 13f79535-47bb-0310-9956-ffa450edef68

--- spi-fly/spi-fly-core/src/main/java/org/apache/aries/spifly/api/SpiFlyConstants.java
+++ spi-fly/spi-fly-core/src/main/java/org/apache/aries/spifly/api/SpiFlyConstants.java
@@ -19,6 +19,7 @@
 package org.apache.aries.spifly.api;
 
 public interface SpiFlyConstants {
+    // Not taken from OSGi Constants because this code needs to compile with the 4.2 OSGi classes.
     String PROVIDE_CAPABILITY = "Provide-Capability";
     String REQUIRE_CAPABILITY = "Require-Capability";
 
