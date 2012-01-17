0fbf73f18a27445edcf64179f04619d60770302e
A. J. David Bosschaert <davidb@apache.org>
2012-01-17 08:34:46 +0000

    Update copy script
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk@1232334 13f79535-47bb-0310-9956-ffa450edef68

--- sandbox/jmx-next/tck/apply_to_tck.sh
+++ sandbox/jmx-next/tck/apply_to_tck.sh
@@ -3,23 +3,22 @@
 ARIES_DIR=...the root of your apache aries checkout...
 # Fill in the variable below, e.g. OSGI_GIT_DIR=/Users/david/clones/osgi-build
 OSGI_GIT_DIR=...the root of your OSGi build clone...
-# TODO fix the target version number
-ARIES_TARGET_VERSION=0.999
 
 JMX_TARGET_DIR=$OSGI_GIT_DIR/licensed/repo
-LIB_BASE=$OSGI_GIT_DIR/cnf/repo/org.apache.aries.impl.jmx
-LIB_FILE=$LIB_BASE/org.apache.aries.impl.jmx-4.2.0.lib
+#LIB_BASE=$OSGI_GIT_DIR/cnf/repo/org.apache.aries.impl.jmx
+#LIB_FILE=$LIB_BASE/org.apache.aries.impl.jmx-4.2.0.lib
 
-mkdir -p -v $JMX_TARGET_DIR/org.apache.aries.jmx
-cp $ARIES_DIR/sandbox/jmx-next/jmx-bundle/target/org.apache.aries.jmx-0.3.1-SNAPSHOT.jar $JMX_TARGET_DIR/org.apache.aries.jmx/org.apache.aries.jmx-$ARIES_TARGET_VERSION.jar
+#mkdir -p -v $JMX_TARGET_DIR/org.apache.aries.jmx
+cp $ARIES_DIR/sandbox/jmx-next/jmx-bundle/target/org.apache.aries.jmx-0.3.1-SNAPSHOT.jar $JMX_TARGET_DIR/org.apache.aries.jmx/org.apache.aries.jmx-0.3.1.jar
 
-mkdir -p -v $JMX_TARGET_DIR/org.apache.aries.util
-cp $ARIES_DIR/util/target/org.apache.aries.util-0.4-SNAPSHOT.jar $JMX_TARGET_DIR/org.apache.aries.util/org.apache.aries.util-$ARIES_TARGET_VERSION.jar
+#mkdir -p -v $JMX_TARGET_DIR/org.apache.aries.util
+cp $ARIES_DIR/util/util/target/org.apache.aries.util-0.5-SNAPSHOT.jar $JMX_TARGET_DIR/org.apache.aries.util/org.apache.aries.util-0.5.0.jar
 
-mkdir -p -v $LIB_BASE
-echo "slf4j.api; version=1.5.10" > $LIB_FILE
-echo "slf4j.simple; version=1.5.10" >> $LIB_FILE
-echo "org.apache.aries.util; version=$ARIES_TARGET_VERSION" >> $LIB_FILE
-echo "org.apache.aries.jmx; version=$ARIES_TARGET_VERSION" >> $LIB_FILE
+# mkdir -p -v $LIB_BASE
+# echo "slf4j.api; version=1.5.10" > $LIB_FILE
+# echo "slf4j.simple; version=1.5.10" >> $LIB_FILE
+# echo "org.apache.aries.util; version=$ARIES_TARGET_VERSION" >> $LIB_FILE
+# echo "org.apache.aries.jmx; version=$ARIES_TARGET_VERSION" >> $LIB_FILE
+# 
+# cp $ARIES_DIR/sandbox/jmx-next/tck/org.osgi.test.cases.jmx.bnd.bnd $OSGI_GIT_DIR/org.osgi.test.cases.jmx/bnd.bnd
 
-cp $ARIES_DIR/sandbox/jmx-next/tck/org.osgi.test.cases.jmx.bnd.bnd $OSGI_GIT_DIR/org.osgi.test.cases.jmx/bnd.bnd
