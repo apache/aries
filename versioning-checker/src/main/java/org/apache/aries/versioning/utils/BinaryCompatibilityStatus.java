1047c0e1968ecc7eda9ff21357728efa79148eb3
David Jencks <djencks@apache.org>
2012-01-24 00:53:35 +0000

    ARIES-757 refactor ClassDeclaration to a simpler list of compatibility problems.  Avoids dependence on HashSet implementation order
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk/versioning@1235096 13f79535-47bb-0310-9956-ffa450edef68

--- versioning-checker/src/main/java/org/apache/aries/versioning/utils/BinaryCompatibilityStatus.java
+++ versioning-checker/src/main/java/org/apache/aries/versioning/utils/BinaryCompatibilityStatus.java
@@ -18,44 +18,16 @@
  */
 package org.apache.aries.versioning.utils;
 
-public class BinaryCompatibilityStatus {
-    private final boolean compatible;
-    private final String reason;
+import java.util.ArrayList;
 
-    public BinaryCompatibilityStatus(boolean compatible, String reason) {
-        this.compatible = compatible;
-        this.reason = reason;
+public class BinaryCompatibilityStatus extends ArrayList<String> {
+
+
+    public BinaryCompatibilityStatus() {
     }
 
     public boolean isCompatible() {
-        return compatible;
+        return isEmpty();
     }
-
-    public String getReason() {
-        return reason;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + (compatible ? 1231 : 1237);
-        result = prime * result + ((reason == null) ? 0 : reason.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null) return false;
-        if (getClass() != obj.getClass()) return false;
-        BinaryCompatibilityStatus other = (BinaryCompatibilityStatus) obj;
-        if (compatible != other.compatible) return false;
-        if (reason == null) {
-            if (other.reason != null) return false;
-        } else if (!reason.equals(other.reason)) return false;
-        return true;
-    }
-
 
 }
