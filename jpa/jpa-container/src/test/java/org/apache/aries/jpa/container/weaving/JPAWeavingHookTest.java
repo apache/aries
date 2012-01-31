2411714cd08253bbcf2c049306fba155f14dc485
Timothy James Ward <timothyjward@apache.org>
2012-01-31 16:46:18 +0000

    ARIES-808: Don't add bundle symbolic name attributes when weaving packages are specified by the PersistenceProvider service.
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk@1238691 13f79535-47bb-0310-9956-ffa450edef68

--- jpa/jpa-container/src/test/java/org/apache/aries/jpa/container/weaving/JPAWeavingHookTest.java
+++ jpa/jpa-container/src/test/java/org/apache/aries/jpa/container/weaving/JPAWeavingHookTest.java
@@ -35,6 +35,7 @@ import org.apache.aries.jpa.container.weaving.impl.TransformerRegistryFactory;
 import org.apache.aries.mocks.BundleMock;
 import org.apache.aries.unittest.mocks.MethodCall;
 import org.apache.aries.unittest.mocks.Skeleton;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.osgi.framework.Bundle;
@@ -58,6 +59,12 @@ public class JPAWeavingHookTest {
     ct1a = Skeleton.newMock(ClassTransformer.class);
   }
   
+  @After
+  public void teardown() {
+    Skeleton.getSkeleton(b1).clearMethodCalls();
+    Skeleton.getSkeleton(ct1a).clearMethodCalls();
+  }
+  
   @Test
   public void testFactory() {
     assertSame(JPAWeavingHook.class, 
@@ -67,148 +74,243 @@ public class JPAWeavingHookTest {
         TransformerRegistryFactory.getTransformerRegistry().getClass());
   }
   
-  @Test
-  public void testAddTransformer() {
-     
-     Bundle b2 = Skeleton.newMock(new BundleMock("bundle2", null), Bundle.class);
-     
-     ClassTransformer ct1b = Skeleton.newMock(ClassTransformer.class);
-     
-     ServiceReference provider1 = Skeleton.newMock(ServiceReference.class);
-     
-     Skeleton.getSkeleton(provider1).setReturnValue(new MethodCall(Bundle.class,
-         "adapt", BundleWiring.class), Skeleton.getSkeleton(provider1).createMock(BundleWiring.class));
-     
-     Skeleton.getSkeleton(provider1).setReturnValue(new MethodCall(Bundle.class,
-         "getSymbolicName"), b1.getSymbolicName());
-     
-     Skeleton.getSkeleton(provider1).setReturnValue(new MethodCall(Bundle.class,
-     "getVersion"), b1.getVersion());
-     
-     Skeleton.getSkeleton(provider1).setReturnValue(new MethodCall(BundleRevision.class, 
-         "getDeclaredCapabilities", BundleRevision.PACKAGE_NAMESPACE), 
-         Collections.singletonList(Skeleton.getSkeleton(provider1).createMock(BundleCapability.class)));
-     
-     Map m = new HashMap();
-     m.put(BundleRevision.PACKAGE_NAMESPACE, "foundPackage");
-     
-     Skeleton.getSkeleton(provider1).setReturnValue(new MethodCall(BundleCapability.class,
-         "getAttributes"), m);
-     
-     
-     JPAWeavingHook tr = (JPAWeavingHook) TransformerRegistryFactory.getTransformerRegistry();
-     
-     
-     //Add a transforme and check it gets called to weave
-     tr.addTransformer(b1, ct1a, provider1);
-     
-     ProtectionDomain pd = new ProtectionDomain(null, null);
-     byte[] bytes = new byte[] {(byte) 0xAB, (byte)0xCD};
-
-     WovenClass wc = getWovenClass(b1, "test1", bytes, this.getClass().getClassLoader(),
-         this.getClass(), pd);
-     
-     tr.weave(wc);
-     
-     Skeleton.getSkeleton(ct1a).assertCalled(new MethodCall(ClassTransformer.class, "transform",
-         this.getClass().getClassLoader(), "test1", this.getClass(), pd, bytes));
-     
-     assertNotCalled(wc);
-     
-     Skeleton.getSkeleton(ct1a).clearMethodCalls();
-     
-     //Check it isn't called for the wrong bundle
-     
-     wc = getWovenClass(b2, "test2", bytes, this.getClass().getClassLoader(),
-         this.getClass(), pd);
-     
-     tr.weave(wc);
-     
-     assertNotCalled(wc);
-     
-     Skeleton.getSkeleton(ct1a).assertNotCalled(new MethodCall(ClassTransformer.class, "transform",
-         ClassLoader.class, String.class, Class.class, ProtectionDomain.class, byte[].class));
-     
-     //Check a second weaver isn't called for a when the first gives a result
-     
-     tr.addTransformer(b1, ct1b, provider1);
-     
-     bytes = new byte[] {(byte) 0xBE, (byte)0xAD};
-     
-     byte[] returnedBytes = new byte[] {(byte) 0xBA, (byte)0xDE};
-     
-     Skeleton.getSkeleton(ct1a).setReturnValue(new MethodCall(ClassTransformer.class, "transform",
-         this.getClass().getClassLoader(), "test3", this.getClass(), pd, bytes), returnedBytes);
-     
-     wc = getWovenClass(b1, "test3", bytes, this.getClass().getClassLoader(),
-         this.getClass(), pd);
-     
-     tr.weave(wc);
-     
-     Skeleton.getSkeleton(ct1a).assertCalled(new MethodCall(ClassTransformer.class, "transform",
-         this.getClass().getClassLoader(), "test3", this.getClass(), pd, bytes));
-     
-     Skeleton.getSkeleton(ct1b).assertNotCalled(new MethodCall(ClassTransformer.class, "transform",
-         ClassLoader.class, String.class, Class.class, ProtectionDomain.class, byte[].class));
-     
-     assertCalled(wc, returnedBytes, "foundPackage", b1);
-     
-     Skeleton.getSkeleton(ct1a).clearMethodCalls();
-     
-     
-     //Check both weavers are called for a when the first gives no result
-     
-     bytes = new byte[] {(byte) 0xBE, (byte)0xAD};
-     
-     wc = getWovenClass(b1, "test4", bytes, this.getClass().getClassLoader(),
-         this.getClass(), pd);
-     
-     tr.weave(wc);
-     
-     Skeleton.getSkeleton(ct1a).assertCalled(new MethodCall(ClassTransformer.class, "transform",
-         this.getClass().getClassLoader(), "test4", this.getClass(), pd, bytes));
-     
-     Skeleton.getSkeleton(ct1b).assertCalled(new MethodCall(ClassTransformer.class, "transform",
-         this.getClass().getClassLoader(), "test4", this.getClass(), pd, bytes));
-     
-     assertNotCalled(wc);
-     
-     Skeleton.getSkeleton(ct1a).clearMethodCalls();
-     Skeleton.getSkeleton(ct1b).clearMethodCalls();
-     
-     //Check we can remove ct1a
-     
-     tr.removeTransformer(b1, ct1a);
-     
-     wc = getWovenClass(b1, "test5", bytes, this.getClass().getClassLoader(),
-         this.getClass(), pd);
-     
-     tr.weave(wc);
-     
-     Skeleton.getSkeleton(ct1a).assertNotCalled(new MethodCall(ClassTransformer.class, "transform",
-         ClassLoader.class, String.class, Class.class, ProtectionDomain.class, byte[].class));
-     
-     Skeleton.getSkeleton(ct1b).assertCalled(new MethodCall(ClassTransformer.class, "transform",
-         this.getClass().getClassLoader(), "test5", this.getClass(), pd, bytes));
-     
-     assertNotCalled(wc);
-     
-  }
-
   
-  private void assertCalled(WovenClass wc, byte[] returnedBytes, String string,
-      Bundle b) {
+  private ServiceReference basicProviderSetup(String[] weavingPackages) {
+    ServiceReference provider1 = Skeleton.newMock(ServiceReference.class);
+	     
+    Skeleton.getSkeleton(provider1).setReturnValue(new MethodCall(ServiceReference.class, 
+        "getProperty", "org.apache.aries.jpa.container.weaving.packages"), weavingPackages);
+    
+    Skeleton.getSkeleton(provider1).setReturnValue(new MethodCall(Bundle.class,
+        "adapt", BundleWiring.class), Skeleton.getSkeleton(provider1).createMock(BundleWiring.class));
+	     
+    Skeleton.getSkeleton(provider1).setReturnValue(new MethodCall(Bundle.class,
+        "getSymbolicName"), b1.getSymbolicName());
+	     
+    Skeleton.getSkeleton(provider1).setReturnValue(new MethodCall(Bundle.class,
+        "getVersion"), b1.getVersion());
+	     
+    Skeleton.getSkeleton(provider1).setReturnValue(new MethodCall(BundleRevision.class, 
+        "getDeclaredCapabilities", BundleRevision.PACKAGE_NAMESPACE), 
+        Collections.singletonList(Skeleton.getSkeleton(provider1).createMock(BundleCapability.class)));
+    Map m = new HashMap();
+    m.put(BundleRevision.PACKAGE_NAMESPACE, "foundPackage");
+    
+    Skeleton.getSkeleton(provider1).setReturnValue(new MethodCall(BundleCapability.class,
+        "getAttributes"), m);
+    return provider1;
+  }
+  
+  @Test
+  public void testNotWoven() {
+    ServiceReference provider1 = basicProviderSetup(null);
+    JPAWeavingHook tr = (JPAWeavingHook) TransformerRegistryFactory.getTransformerRegistry();
+     
+    //Add a transforme and check it gets called to weave
+    tr.addTransformer(b1, ct1a, provider1);
+     
+    ProtectionDomain pd = new ProtectionDomain(null, null);
+    byte[] bytes = new byte[] {(byte) 0xAB, (byte)0xCD};
+
+    WovenClass wc = getWovenClass(b1, "test1", bytes, this.getClass().getClassLoader(),
+        this.getClass(), pd);
+	     
+    tr.weave(wc);
+	     
+    Skeleton.getSkeleton(ct1a).assertCalled(new MethodCall(ClassTransformer.class, "transform",
+        this.getClass().getClassLoader(), "test1", this.getClass(), pd, bytes));
+	     
+     assertNotCalled(wc);
+  }
+  
+  
+  @Test
+  public void testWeavingWrongBundle() {
+    Bundle b2 = Skeleton.newMock(new BundleMock("bundle2", null), Bundle.class);
+    ServiceReference provider1 = basicProviderSetup(null);
+    JPAWeavingHook tr = (JPAWeavingHook) TransformerRegistryFactory.getTransformerRegistry();
+	     
+    //Add a transforme and check it gets called to weave
+    tr.addTransformer(b1, ct1a, provider1);
+	     
+    ProtectionDomain pd = new ProtectionDomain(null, null);
+    byte[] bytes = new byte[] {(byte) 0xAB, (byte)0xCD};
+
+    WovenClass wc = getWovenClass(b2, "test2", bytes, this.getClass().getClassLoader(),
+        this.getClass(), pd);
+	     
+    tr.weave(wc);
+	     
+    assertNotCalled(wc);
+	     
+    Skeleton.getSkeleton(ct1a).assertNotCalled(new MethodCall(ClassTransformer.class, "transform",
+        ClassLoader.class, String.class, Class.class, ProtectionDomain.class, byte[].class));
+  }
+  
+  @Test
+  public void testBasicWeaving() {
+    ServiceReference provider1 = basicProviderSetup(null);
+    JPAWeavingHook tr = (JPAWeavingHook) TransformerRegistryFactory.getTransformerRegistry();
+	     
+    //Add a transforme and check it gets called to weave
+    tr.addTransformer(b1, ct1a, provider1);
+
+    ProtectionDomain pd = new ProtectionDomain(null, null);
+    byte[] bytes = new byte[] {(byte) 0xBE, (byte)0xAD};
+	     
+    byte[] returnedBytes = new byte[] {(byte) 0xBA, (byte)0xDE};
+	     
+    Skeleton.getSkeleton(ct1a).setReturnValue(new MethodCall(ClassTransformer.class, "transform",
+        this.getClass().getClassLoader(), "test3", this.getClass(), pd, bytes), returnedBytes);
+	     
+    WovenClass wc = getWovenClass(b1, "test3", bytes, this.getClass().getClassLoader(),
+        this.getClass(), pd);
+	     
+    tr.weave(wc);
+	     
+    Skeleton.getSkeleton(ct1a).assertCalled(new MethodCall(ClassTransformer.class, "transform",
+        this.getClass().getClassLoader(), "test3", this.getClass(), pd, bytes));
+	     
+    assertCalled(wc, returnedBytes, b1, "foundPackage;" + Constants.BUNDLE_SYMBOLICNAME_ATTRIBUTE + "=bundle1;" + 
+        Constants.BUNDLE_VERSION_ATTRIBUTE + "=0.0.0");
+  }
+  
+  @Test
+  public void testMultipleTransformers() {
+    ClassTransformer ct1b = Skeleton.newMock(ClassTransformer.class);
+    ServiceReference provider1 = basicProviderSetup(null);
+    JPAWeavingHook tr = (JPAWeavingHook) TransformerRegistryFactory.getTransformerRegistry();
+	     
+    //Add a transforme and check it gets called to weave
+    tr.addTransformer(b1, ct1a, provider1);
+    //Check a second weaver isn't called for a when the first gives a result
+    tr.addTransformer(b1, ct1b, provider1);
+
+    ProtectionDomain pd = new ProtectionDomain(null, null);
+    byte[] bytes = new byte[] {(byte) 0xBE, (byte)0xAD};
+	     
+    byte[] returnedBytes = new byte[] {(byte) 0xBA, (byte)0xDE};
+	     
+    Skeleton.getSkeleton(ct1a).setReturnValue(new MethodCall(ClassTransformer.class, "transform",
+        this.getClass().getClassLoader(), "test3", this.getClass(), pd, bytes), returnedBytes);
+	     
+    WovenClass wc = getWovenClass(b1, "test3", bytes, this.getClass().getClassLoader(),
+        this.getClass(), pd);
+	     
+    tr.weave(wc);
+	     
+    Skeleton.getSkeleton(ct1a).assertCalled(new MethodCall(ClassTransformer.class, "transform",
+        this.getClass().getClassLoader(), "test3", this.getClass(), pd, bytes));
+	     
+    Skeleton.getSkeleton(ct1b).assertNotCalled(new MethodCall(ClassTransformer.class, "transform",
+        ClassLoader.class, String.class, Class.class, ProtectionDomain.class, byte[].class));
+	     
+    assertCalled(wc, returnedBytes, b1, "foundPackage;" + Constants.BUNDLE_SYMBOLICNAME_ATTRIBUTE + "=bundle1;" + 
+        Constants.BUNDLE_VERSION_ATTRIBUTE + "=0.0.0");
+  }
+  
+  @Test
+  public void testMultipleTransformers2() {
+    ClassTransformer ct1b = Skeleton.newMock(ClassTransformer.class);
+    ServiceReference provider1 = basicProviderSetup(null);
+    JPAWeavingHook tr = (JPAWeavingHook) TransformerRegistryFactory.getTransformerRegistry();
+    
+    //Add a two transformers that will return null
+    tr.addTransformer(b1, ct1a, provider1);
+    tr.addTransformer(b1, ct1b, provider1);
+    
+    ProtectionDomain pd = new ProtectionDomain(null, null);
+    byte[] bytes = new byte[] {(byte) 0xBE, (byte)0xAD};
+    
+    WovenClass wc = getWovenClass(b1, "test4", bytes, this.getClass().getClassLoader(),
+        this.getClass(), pd);
+    
+    tr.weave(wc);
+    
+    Skeleton.getSkeleton(ct1a).assertCalled(new MethodCall(ClassTransformer.class, "transform",
+        this.getClass().getClassLoader(), "test4", this.getClass(), pd, bytes));
+    
+    Skeleton.getSkeleton(ct1b).assertCalled(new MethodCall(ClassTransformer.class, "transform",
+        this.getClass().getClassLoader(), "test4", this.getClass(), pd, bytes));
+    
+    assertNotCalled(wc);
+  }
+  
+  
+  @Test
+  public void testMultipleTransformers3() {
+    ClassTransformer ct1b = Skeleton.newMock(ClassTransformer.class);
+    ServiceReference provider1 = basicProviderSetup(null);
+    JPAWeavingHook tr = (JPAWeavingHook) TransformerRegistryFactory.getTransformerRegistry();
+	    
+    //Add a two transformers that will return null
+    tr.addTransformer(b1, ct1a, provider1);
+    tr.addTransformer(b1, ct1b, provider1);
+	    
+    tr.removeTransformer(b1, ct1a);
+	    
+    ProtectionDomain pd = new ProtectionDomain(null, null);
+    byte[] bytes = new byte[] {(byte) 0xBE, (byte)0xAD};
+	    
+    WovenClass wc = getWovenClass(b1, "test5", bytes, this.getClass().getClassLoader(),
+           this.getClass(), pd);
+	    
+    tr.weave(wc);
+	    
+    Skeleton.getSkeleton(ct1a).assertNotCalled(new MethodCall(ClassTransformer.class, "transform",
+            ClassLoader.class, String.class, Class.class, ProtectionDomain.class, byte[].class));
+	         
+    Skeleton.getSkeleton(ct1b).assertCalled(new MethodCall(ClassTransformer.class, "transform",
+            this.getClass().getClassLoader(), "test5", this.getClass(), pd, bytes));
+	         
+    assertNotCalled(wc);
+	    
+  }
+  
+  @Test
+  public void testWeavingPackages() {
+    ClassTransformer ct1b = Skeleton.newMock(ClassTransformer.class);
+    ServiceReference provider1 = basicProviderSetup(new String[] {"specifiedPackage1", "specifiedPackage2;attribute=value"});
+    JPAWeavingHook tr = (JPAWeavingHook) TransformerRegistryFactory.getTransformerRegistry();
+	     
+    //Add a transforme and check it gets called to weave
+    tr.addTransformer(b1, ct1a, provider1);
+    //Check a second weaver isn't called for a when the first gives a result
+    tr.addTransformer(b1, ct1b, provider1);
+
+    ProtectionDomain pd = new ProtectionDomain(null, null);
+    byte[] bytes = new byte[] {(byte) 0xBE, (byte)0xAD};
+	     
+    byte[] returnedBytes = new byte[] {(byte) 0xBA, (byte)0xDE};
+	     
+    Skeleton.getSkeleton(ct1a).setReturnValue(new MethodCall(ClassTransformer.class, "transform",
+        this.getClass().getClassLoader(), "test3", this.getClass(), pd, bytes), returnedBytes);
+	     
+    WovenClass wc = getWovenClass(b1, "test3", bytes, this.getClass().getClassLoader(),
+        this.getClass(), pd);
+	     
+    tr.weave(wc);
+	     
+    Skeleton.getSkeleton(ct1a).assertCalled(new MethodCall(ClassTransformer.class, "transform",
+        this.getClass().getClassLoader(), "test3", this.getClass(), pd, bytes));
+	     
+    Skeleton.getSkeleton(ct1b).assertNotCalled(new MethodCall(ClassTransformer.class, "transform",
+        ClassLoader.class, String.class, Class.class, ProtectionDomain.class, byte[].class));
+	     
+    assertCalled(wc, returnedBytes, b1, "specifiedPackage1", "specifiedPackage2;attribute=value");
+  }
+  
+  private void assertCalled(WovenClass wc, byte[] returnedBytes, Bundle b,
+      String... string) {
     Skeleton.getSkeleton(wc).assertCalledExactNumberOfTimes(new MethodCall(WovenClass.class, 
         "setBytes", byte[].class), 1);
     Skeleton.getSkeleton(wc).assertCalled(new MethodCall(WovenClass.class, 
         "setBytes", returnedBytes));
        
-    String importString = string + ";" + Constants.BUNDLE_SYMBOLICNAME_ATTRIBUTE + "=" +
-    b.getSymbolicName() + ";" + Constants.BUNDLE_VERSION_ATTRIBUTE 
-    + "=" + b.getVersion();
-    
-    assertSame(1, wc.getDynamicImports().size());
-    assertTrue(wc.getDynamicImports().contains(importString));
+    assertSame(string.length, wc.getDynamicImports().size());
+    for(String s : string) {
+      assertTrue(wc.getDynamicImports().contains(s));
+    }
   }
 
   private void assertNotCalled(WovenClass wc) {
