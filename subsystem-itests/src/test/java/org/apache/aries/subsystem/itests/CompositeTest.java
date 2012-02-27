b3ac9f364780d04df4615aa00a0ec4cee6770a60
John Ross <jwross@apache.org>
2012-02-27 23:16:45 +0000

    ARIES-825: Update subsystems to latest Subsystem, Resolver, and Repository APIs.
    (1) Initial support for Require-Bundle header in composites.
    (2) Added new composite test.
    
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk/subsystem@1294392 13f79535-47bb-0310-9956-ffa450edef68

--- subsystem-itests/src/test/java/org/apache/aries/subsystem/itests/CompositeTest.java
+++ subsystem-itests/src/test/java/org/apache/aries/subsystem/itests/CompositeTest.java
@@ -2,8 +2,13 @@ package org.apache.aries.subsystem.itests;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import org.apache.aries.unittest.fixture.ArchiveFixture;
+import org.apache.aries.unittest.fixture.ArchiveFixture.JarFixture;
+import org.apache.aries.unittest.fixture.ArchiveFixture.ManifestFixture;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,6 +20,15 @@ import org.osgi.service.subsystem.SubsystemConstants;
 
 @RunWith(JUnit4TestRunner.class)
 public class CompositeTest extends SubsystemTest {
+	private static final String BUNDLE_A = "org.apache.aries.subsystem.itests.bundle.a";
+	private static final String BUNDLE_B = "org.apache.aries.subsystem.itests.bundle.b";
+	private static final String BUNDLE_C = "org.apache.aries.subsystem.itests.bundle.c";
+	private static final String BUNDLE_D = "org.apache.aries.subsystem.itests.bundle.d";
+	private static final String BUNDLE_E = "org.apache.aries.subsystem.itests.bundle.e";
+	private static final String COMPOSITE_B = "org.apache.aries.subsystem.itests.subsystem.composite.b";
+	private static final String COMPOSITE_C = "org.apache.aries.subsystem.itests.subsystem.composite.c";
+	private static final String COMPOSITE_D = "org.apache.aries.subsystem.itests.subsystem.composite.d";
+	
 	private static boolean createdTestFiles;
 	
 	@Before
@@ -24,109 +38,129 @@ public class CompositeTest extends SubsystemTest {
 		createBundleA();
 		createBundleB();
 		createBundleC();
+		createBundleD();
 		createBundleE();
+		createCompositeB();
 		createCompositeC();
 		createCompositeD();
 		createdTestFiles = true;
 	}
 	
+	private static void createBundle(String symbolicName) throws IOException {
+		createBundle(symbolicName, null);
+	}
+	
+	private static void createBundle(String symbolicName, Map<String, String> headers) throws IOException {
+		createBundle(symbolicName, null, headers);
+	}
+	
+	private static void createBundle(String symbolicName, String version, Map<String, String> headers) throws IOException {
+		if (headers == null)
+			headers = new HashMap<String, String>();
+		headers.put(Constants.BUNDLE_SYMBOLICNAME, symbolicName);
+		if (version != null)
+			headers.put(Constants.BUNDLE_VERSION, version);
+		createBundle(headers);
+	}
+	
+	private static void createBundle(Map<String, String> headers) throws IOException {
+		String symbolicName = headers.get(Constants.BUNDLE_SYMBOLICNAME);
+		JarFixture bundle = ArchiveFixture.newJar();
+		ManifestFixture manifest = bundle.manifest();
+		for (Entry<String, String> header : headers.entrySet()) {
+			manifest.attribute(header.getKey(), header.getValue());
+		}
+		write(symbolicName, bundle);
+	}
+	
 	private static void createBundleA() throws IOException {
-		write("bundle.a.jar",
-				ArchiveFixture
-						.newJar()
-						.manifest()
-						.symbolicName(
-								"org.apache.aries.subsystem.itests.bundle.a")
-						.attribute(Constants.EXPORT_PACKAGE,
-								"org.apache.aries.subsystem.itests.bundle.a.x;version=\"1.0\"")
-						.end());
+		Map<String, String> headers = new HashMap<String, String>();
+		headers.put(Constants.EXPORT_PACKAGE, BUNDLE_A + ".x;version=\"1.0\"");
+		createBundle(BUNDLE_A, "1.0.0", headers);
 	}
 	
 	private static void createBundleB() throws IOException {
-		write("bundle.b.jar",
-				ArchiveFixture
-						.newJar()
-						.manifest()
-						.symbolicName(
-								"org.apache.aries.subsystem.itests.bundle.b")
-						.version("1.0.0")
-						.attribute(Constants.PROVIDE_CAPABILITY,
-								"y; y=test; version:Version=1.0")
-						.end());
+		Map<String, String> headers = new HashMap<String, String>();
+		headers.put(Constants.PROVIDE_CAPABILITY, "y; y=test; version:Version=1.0");
+		createBundle(BUNDLE_B, "1.0.0", headers);
 	}
 	
 	private static void createBundleC() throws IOException {
-		write("bundle.c.jar",
-				ArchiveFixture
-						.newJar()
-						.manifest()
-						.symbolicName(
-								"org.apache.aries.subsystem.itests.bundle.c")
-						.attribute(Constants.IMPORT_PACKAGE,
-								"org.apache.aries.subsystem.itests.bundle.a.x;version=\"[1.0,2.0)\"")
-						.end());
+		Map<String, String> headers = new HashMap<String, String>();
+		headers.put(Constants.IMPORT_PACKAGE, BUNDLE_A + ".x;version=\"[1.0,2.0)\"");
+		createBundle(BUNDLE_C, headers);
+	}
+	
+	private static void createBundleD() throws IOException {
+		Map<String, String> headers = new HashMap<String, String>();
+		headers.put(Constants.REQUIRE_BUNDLE, BUNDLE_A);
+		createBundle(BUNDLE_D, headers);
 	}
 	
 	private static void createBundleE() throws IOException {
-		write("bundle.e.jar",
-				ArchiveFixture
-						.newJar()
-						.manifest()
-						.symbolicName(
-								"org.apache.aries.subsystem.itests.bundle.e")
-						.version("1.0.0")
-						.attribute(Constants.REQUIRE_CAPABILITY,
-								"y; filter:=(y=test)")
-						.end());
+		Map<String, String> headers = new HashMap<String, String>();
+		headers.put(Constants.REQUIRE_CAPABILITY, "y; filter:=(y=test)");
+		createBundle(BUNDLE_E, headers);
+	}
+	
+	private static void createCompositeB() throws IOException {
+		createCompositeBManifest();
+		createSubsystem(COMPOSITE_B, "COMPOSITE.B.MF");
 	}
 	
 	private static void createCompositeC() throws IOException {
 		createCompositeCManifest();
-		write("composite.c.esa",
-				ArchiveFixture.newZip().binary("OSGI-INF/SUBSYSTEM.MF",
-						new FileInputStream("COMPOSITE.C.MF")));
+		createSubsystem(COMPOSITE_C, "COMPOSITE.C.MF");
 	}
 	
 	private static void createCompositeD() throws IOException {
 		createCompositeDManifest();
-		write("composite.d.esa",
-				ArchiveFixture.newZip().binary("OSGI-INF/SUBSYSTEM.MF",
-						new FileInputStream("COMPOSITE.D.MF")));
+		createSubsystem(COMPOSITE_D, "COMPOSITE.D.MF");
+	}
+	
+	private static void createCompositeBManifest() throws IOException {
+		Map<String, String> attributes = new HashMap<String, String>();
+		attributes.put(SubsystemConstants.SUBSYSTEM_SYMBOLICNAME, COMPOSITE_B);
+		attributes.put(SubsystemConstants.SUBSYSTEM_TYPE, SubsystemConstants.SUBSYSTEM_TYPE_COMPOSITE);
+		attributes.put(Constants.REQUIRE_BUNDLE, BUNDLE_A + "; bundle-version=\"[1.0, 2.0)\", does.not.exist; bundle-version=\"[1.0, 2.0)\"");
+		createManifest("COMPOSITE.B.MF", attributes);
 	}
 	
 	private static void createCompositeCManifest() throws IOException {
-		write("COMPOSITE.C.MF",
-				ArchiveFixture
-						.newJar()
-						.manifest()
-						.attribute(SubsystemConstants.SUBSYSTEM_SYMBOLICNAME,
-								"org.apache.aries.subsystem.itests.subsystem.composite.c")
-						.attribute(SubsystemConstants.SUBSYSTEM_TYPE,
-								SubsystemConstants.SUBSYSTEM_TYPE_COMPOSITE)
-						.attribute(Constants.IMPORT_PACKAGE,
-								"org.apache.aries.subsystem.itests.bundle.a.x, does.not.exist; a=b"));
+		Map<String, String> attributes = new HashMap<String, String>();
+		attributes.put(SubsystemConstants.SUBSYSTEM_SYMBOLICNAME, COMPOSITE_C);
+		attributes.put(SubsystemConstants.SUBSYSTEM_TYPE, SubsystemConstants.SUBSYSTEM_TYPE_COMPOSITE);
+		attributes.put(Constants.IMPORT_PACKAGE, BUNDLE_A + ".x, does.not.exist; a=b");
+		createManifest("COMPOSITE.C.MF", attributes);
 	}
 	
 	private static void createCompositeDManifest() throws IOException {
-		write("COMPOSITE.D.MF",
-				ArchiveFixture
-						.newJar()
-						.manifest()
-						.attribute(SubsystemConstants.SUBSYSTEM_SYMBOLICNAME,
-								"org.apache.aries.subsystem.itests.subsystem.composite.d")
-						.attribute(SubsystemConstants.SUBSYSTEM_TYPE,
-								SubsystemConstants.SUBSYSTEM_TYPE_COMPOSITE)
-						.attribute(Constants.REQUIRE_CAPABILITY,
-								"y; filter:=\"(y=test)\", does.not.exist; filter:=\"(a=b)\""));
+		Map<String, String> attributes = new HashMap<String, String>();
+		attributes.put(SubsystemConstants.SUBSYSTEM_SYMBOLICNAME, COMPOSITE_D);
+		attributes.put(SubsystemConstants.SUBSYSTEM_TYPE, SubsystemConstants.SUBSYSTEM_TYPE_COMPOSITE);
+		attributes.put(Constants.REQUIRE_CAPABILITY, "y; filter:=\"(y=test)\", does.not.exist; filter:=\"(a=b)\"");
+		createManifest("COMPOSITE.D.MF", attributes);
+	}
+	
+	private static void createManifest(String name, Map<String, String> headers) throws IOException {
+		ManifestFixture manifest = ArchiveFixture.newJar().manifest();
+		for (Entry<String, String> header : headers.entrySet()) {
+			manifest.attribute(header.getKey(), header.getValue());
+		}
+		write(name, manifest);
+	}
+	
+	private static void createSubsystem(String name, String manifest) throws IOException {
+		write(name, ArchiveFixture.newZip().binary("OSGI-INF/SUBSYSTEM.MF", new FileInputStream(manifest)));
 	}
 	
 	@Test
 	public void testImportPackage() throws Exception {
-		Bundle bundleA = installBundleFromFile("bundle.a.jar");
+		Bundle bundleA = installBundleFromFile(BUNDLE_A);
 		try {
-			Subsystem compositeC = installSubsystemFromFile("composite.c.esa");
+			Subsystem compositeC = installSubsystemFromFile(COMPOSITE_C);
 			try {
-				Bundle bundleC = installBundleFromFile("bundle.c.jar", compositeC);
+				Bundle bundleC = installBundleFromFile(BUNDLE_C, compositeC);
 				try {
 					startBundle(bundleC, compositeC);
 				}
@@ -144,12 +178,35 @@ public class CompositeTest extends SubsystemTest {
 	}
 	
 	@Test
-	public void testRequireCapability() throws Exception {
-		Bundle bundleB = installBundleFromFile("bundle.b.jar");
+	public void testRequireBundle() throws Exception {
+		Bundle bundleA = installBundleFromFile(BUNDLE_A);
 		try {
-			Subsystem compositeD = installSubsystemFromFile("composite.d.esa");
+			Subsystem compositeB = installSubsystemFromFile(COMPOSITE_B);
 			try {
-				Bundle bundleE = installBundleFromFile("bundle.e.jar", compositeD);
+				Bundle bundleD = installBundleFromFile(BUNDLE_D, compositeB);
+				try {
+					startBundle(bundleD, compositeB);
+				}
+				finally {
+					bundleD.uninstall();
+				}
+			}
+			finally {
+				uninstallScopedSubsystem(compositeB);
+			}
+		}
+		finally {
+			bundleA.uninstall();
+		}
+	}
+	
+	@Test
+	public void testRequireCapability() throws Exception {
+		Bundle bundleB = installBundleFromFile(BUNDLE_B);
+		try {
+			Subsystem compositeD = installSubsystemFromFile(COMPOSITE_D);
+			try {
+				Bundle bundleE = installBundleFromFile(BUNDLE_E, compositeD);
 				try {
 					startBundle(bundleE, compositeD);
 				}
