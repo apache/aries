8a63b951b46a90623e91c04cacf1fc6d74e18da0
Emily Jiang <ejiang@apache.org>
2012-03-06 14:31:13 +0000

    ARIES-832: JPA load-time enhancement fails for a WAB (A patch from Brian)
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk/jpa/jpa-container@1297497 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/aries/jpa/container/PersistenceBundleLifecycleTest.java
+++ src/test/java/org/apache/aries/jpa/container/PersistenceBundleLifecycleTest.java
@@ -406,8 +406,6 @@ public class PersistenceBundleLifecycleTest
     //Now try Resolving
     Skeleton.getSkeleton(persistenceBundle).setReturnValue(new MethodCall(Bundle.class, "getState"), Bundle.RESOLVED);
     mgr.modifiedBundle(persistenceBundle, new BundleEvent(BundleEvent.RESOLVED, persistenceBundle), o);
-    
-    testSuccessfulCreationEvent(ref, extenderContext, 1);
     BundleContextMock.assertNoServiceExists(EntityManagerFactory.class.getName());
     
     
@@ -489,7 +487,6 @@ public class PersistenceBundleLifecycleTest
     mgr.modifiedBundle(persistenceBundle, new BundleEvent(BundleEvent.RESOLVED, persistenceBundle), getTrackedObject());
     
     //We will have created the EMF a total of 2 times
-    testSuccessfulCreationEvent(ref, extenderContext, 2);
     BundleContextMock.assertNoServiceExists(EntityManagerFactory.class.getName());
   }
   
