228fff947ef2b1de5c54417025e9bcd22fd5dbab
John Ross <jwross@apache.org>
2012-03-21 21:56:13 +0000

    ARIES-825: Update subsystems to latest Subsystem, Resolver, and Repository APIs.
    Added the ability to compute the dependencies of an application and expose them as requirements on the associated non-runtime resource so that a parent application's share policy is computed correctly.
    
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk/subsystem@1303594 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subsystem-core/src/main/java/org/apache/aries/subsystem/core/resource/CompositeRepository.java
@@ -0,0 +1,46 @@
+package org.apache.aries.subsystem.core.resource;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.osgi.resource.Capability;
+import org.osgi.resource.Requirement;
+import org.osgi.service.repository.Repository;
+
+public class CompositeRepository implements Repository {
+	private final Collection<Repository> repositories;
+	
+	public CompositeRepository(Repository...repositories) {
+		this(Arrays.asList(repositories));
+	}
+	
+	public CompositeRepository(Collection<Repository> repositories) {
+		this.repositories = repositories;
+	}
+	
+	public Collection<Capability> findProviders(Requirement requirement) {
+		Set<Capability> result = new HashSet<Capability>();
+		for (Repository repository : repositories) {
+			Map<Requirement, Collection<Capability>> map = repository.findProviders(Collections.singleton(requirement));
+			Collection<Capability> capabilities = map.get(requirement);
+			if (capabilities == null)
+				continue;
+			result.addAll(capabilities);
+		}
+		return result;	
+	}
+	
+	@Override
+	public Map<Requirement, Collection<Capability>> findProviders(
+			Collection<? extends Requirement> requirements) {
+		Map<Requirement, Collection<Capability>> result = new HashMap<Requirement, Collection<Capability>>();
+		for (Requirement requirement : requirements)
+			result.put(requirement, findProviders(requirement));
+		return result;
+	}
+}
