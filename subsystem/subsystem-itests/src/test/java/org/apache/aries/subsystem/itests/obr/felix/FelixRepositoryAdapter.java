7405a20064e873669f7a66b3162db0c293759b8a
John Ross <jwross@apache.org>
2012-04-09 15:28:00 +0000

    ARIES-825: Update subsystems to latest Subsystem, Resolver, and Repository APIs.
    Removed dependency on felix bundle repository from core. It's only needed in itests.
    
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk@1311281 13f79535-47bb-0310-9956-ffa450edef68

--- subsystem/subsystem-itests/src/test/java/org/apache/aries/subsystem/itests/obr/felix/FelixRepositoryAdapter.java
+++ subsystem/subsystem-itests/src/test/java/org/apache/aries/subsystem/itests/obr/felix/FelixRepositoryAdapter.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.aries.subsystem.core.obr.felix;
+package org.apache.aries.subsystem.itests.obr.felix;
 
 import static org.apache.aries.application.utils.AppConstants.LOG_ENTRY;
 import static org.apache.aries.application.utils.AppConstants.LOG_EXIT;
