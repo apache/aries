2e22c555410bf47c306c250652722234fee6a713
John Ross <jwross@apache.org>
2012-04-11 23:26:48 +0000

    ARIES-825: Update subsystems to latest Subsystem, Resolver, and Repository APIs.
    Added initial support for Preferred-Provider.
    
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk/subsystem/subsystem-core@1325064 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/aries/subsystem/core/internal/SubsystemManifestValidator.java
+++ src/main/java/org/apache/aries/subsystem/core/internal/SubsystemManifestValidator.java
@@ -1,5 +1,6 @@
 package org.apache.aries.subsystem.core.internal;
 
+import org.apache.aries.subsystem.core.archive.PreferredProviderHeader;
 import org.apache.aries.subsystem.core.archive.SubsystemContentHeader;
 import org.apache.aries.subsystem.core.archive.SubsystemManifest;
 import org.osgi.framework.VersionRange;
@@ -8,6 +9,7 @@ import org.osgi.service.subsystem.SubsystemException;
 
 public class SubsystemManifestValidator {
 	public static void validate(AriesSubsystem subsystem, SubsystemManifest manifest) {
+		validatePreferredProviderHeader(manifest.getPreferredProviderHeader());
 		if (subsystem.isComposite()) {
 			SubsystemContentHeader header = manifest.getSubsystemContentHeader();
 			if (header == null)
@@ -26,6 +28,7 @@ public class SubsystemManifestValidator {
 		}
 	}
 	
+	// TODO Replace this with the new isExact() method on OSGi VersionRange.
 	private static boolean isExactVersion(VersionRange range) {
 		if (range.getLeftType() == VersionRange.LEFT_CLOSED
 		          && range.getLeft().equals(range.getRight())
@@ -33,5 +36,17 @@ public class SubsystemManifestValidator {
 		     return true;
 		}
 		return false;
+	}
+	
+	private static void validatePreferredProviderHeader(PreferredProviderHeader header) {
+		if (header == null)
+			return;
+		for (PreferredProviderHeader.Clause clause : header.getClauses()) {
+			String type = (String)clause.getAttribute(PreferredProviderHeader.Clause.ATTRIBUTE_TYPE).getValue();
+			if (!(SubsystemConstants.SUBSYSTEM_TYPE_COMPOSITE.equals(type) ||
+					SubsystemConstants.SUBSYSTEM_TYPE_FEATURE.equals(type) ||
+					Constants.ResourceTypeBundle.equals(type)))
+				throw new SubsystemException("Unsupported " + PreferredProviderHeader.NAME + " type: " + type);
+		}
 	}
 }
