ff1fbbe88244603fa92133ae43431ed56ae4de9c
John Ross <jwross@apache.org>
2012-05-08 18:31:15 +0000

    ARIES-825: Added initial support for honoring start-order directive when starting and stopping subsystems.
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk/subsystem@1335681 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subsystem-core/src/main/java/org/apache/aries/subsystem/core/internal/StartResourceComparator.java
@@ -0,0 +1,27 @@
+package org.apache.aries.subsystem.core.internal;
+
+import java.util.Comparator;
+
+import org.apache.aries.subsystem.core.archive.SubsystemContentHeader;
+import org.osgi.resource.Resource;
+
+public class StartResourceComparator implements Comparator<Resource> {
+	private final SubsystemContentHeader header;
+	
+	public StartResourceComparator(SubsystemContentHeader header) {
+		this.header = header;
+	}
+	@Override
+	public int compare(Resource r1, Resource r2) {
+		Integer r1StartOrder = getStartOrder(r1);
+		Integer r2StartOrder = getStartOrder(r2);
+		return r1StartOrder.compareTo(r2StartOrder);
+	}
+	
+	private Integer getStartOrder(Resource r) {
+		SubsystemContentHeader.Content content = header.getContent(r);
+		if (content == null)
+			return 0;
+		return content.getStartOrder();
+	}
+}
