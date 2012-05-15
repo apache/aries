6cb060da1561d0ec1600418a859f8257aaf79d5f
John Ross <jwross@apache.org>
2012-05-15 18:58:42 +0000

    ARIES-825: Initial support for security.
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk/subsystem/subsystem-core@1338828 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/aries/subsystem/core/internal/InstallAction.java
@@ -0,0 +1,101 @@
+package org.apache.aries.subsystem.core.internal;
+
+import java.io.InputStream;
+import java.security.AccessControlContext;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+
+import org.osgi.service.coordinator.Coordination;
+import org.osgi.service.coordinator.CoordinationException;
+import org.osgi.service.subsystem.SubsystemException;
+
+public class InstallAction implements PrivilegedAction<AriesSubsystem> {
+	private final InputStream content;
+	private final AccessControlContext context;
+	private final Coordination coordination;
+	private final boolean embedded;
+	private final String location;
+	private final AriesSubsystem parent;
+	
+	public InstallAction(String location, InputStream content, AriesSubsystem parent, AccessControlContext context) {
+		this(location, content, parent, context, null, false);
+	}
+	
+	public InstallAction(String location, InputStream content, AriesSubsystem parent, AccessControlContext context, Coordination coordination, boolean embedded) {
+		this.location = location;
+		this.content = content;
+		this.parent = parent;
+		this.context = context;
+		this.coordination = coordination;
+		this.embedded = embedded;
+	}
+	
+	@Override
+	public AriesSubsystem run() {
+		AriesSubsystem result = null;
+		Coordination coordination = this.coordination;
+		if (coordination == null)
+			coordination = Activator.getInstance().getCoordinator().create(parent.getSymbolicName() + '-' + parent.getSubsystemId(), 0);
+		try {
+			TargetRegion region = new TargetRegion(parent);
+			SubsystemResource ssr = new SubsystemResource(location, content, parent);
+			result = AriesSubsystem.locationToSubsystem.get(location);
+			if (result != null) {
+				checkLifecyclePermission(result);
+				if (!region.contains(result))
+					throw new SubsystemException("Location already exists but existing subsystem is not part of target region: " + location);
+				if (!(result.getSymbolicName().equals(ssr.getSubsystemManifest().getSubsystemSymbolicNameHeader().getSymbolicName())
+						&& result.getVersion().equals(ssr.getSubsystemManifest().getSubsystemVersionHeader().getVersion())
+						&& result.getType().equals(ssr.getSubsystemManifest().getSubsystemTypeHeader().getType())))
+					throw new SubsystemException("Location already exists but symbolic name, version, and type are not the same: " + location);
+				parent.subsystemInstalling(result);
+				parent.subsystemInstalled(result);
+				return result;
+			}
+			result = (AriesSubsystem)region.find(
+					ssr.getSubsystemManifest().getSubsystemSymbolicNameHeader().getSymbolicName(), 
+					ssr.getSubsystemManifest().getSubsystemVersionHeader().getVersion());
+			if (result != null) {
+				checkLifecyclePermission(result);
+				if (!result.getType().equals(ssr.getSubsystemManifest().getSubsystemTypeHeader().getType()))
+					throw new SubsystemException("Subsystem already exists in target region but has a different type: " + location);
+				parent.subsystemInstalling(result);
+				parent.subsystemInstalled(result);
+				return result;
+			}
+			result = new AriesSubsystem(ssr, parent);
+			checkLifecyclePermission(result);
+			parent.installResource(result, coordination, false);
+		}
+		catch (Throwable t) {
+			coordination.fail(t);
+		}
+		finally {
+			try {
+				coordination.end();
+			}
+			catch (CoordinationException e) {
+				Throwable t = e.getCause();
+				if (t instanceof SubsystemException)
+					throw (SubsystemException)t;
+				if (t instanceof SecurityException)
+					throw (SecurityException)t;
+				throw new SubsystemException(t);
+			}
+		}
+		return result;
+	}
+
+	private void checkLifecyclePermission(final AriesSubsystem subsystem) {
+		if (embedded)
+			return;
+		AccessController.doPrivileged(new PrivilegedAction<Object>() {
+			@Override
+			public Object run() {
+				SecurityManager.checkLifecyclePermission(subsystem);
+				return null;
+			}
+		},
+		context);
+	}
+}
