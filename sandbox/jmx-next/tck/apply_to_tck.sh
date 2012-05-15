228f2b2446184a700c2684aa345c91ac8ae3fe01
A. J. David Bosschaert <davidb@apache.org>
2012-05-15 07:42:13 +0000

    Move JMX-Next to 1.1.0-SNAPSHOT (assuming that the existing JMX implementation will be released under 1.0.0 soon).
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk@1338575 13f79535-47bb-0310-9956-ffa450edef68

--- sandbox/jmx-next/tck/apply_to_tck.sh
+++ sandbox/jmx-next/tck/apply_to_tck.sh
@@ -9,7 +9,7 @@ JMX_TARGET_DIR=$OSGI_GIT_DIR/licensed/repo
 #LIB_FILE=$LIB_BASE/org.apache.aries.impl.jmx-4.2.0.lib
 
 #mkdir -p -v $JMX_TARGET_DIR/org.apache.aries.jmx
-cp $ARIES_DIR/sandbox/jmx-next/jmx-bundle/target/org.apache.aries.jmx-0.3.1-SNAPSHOT.jar $JMX_TARGET_DIR/org.apache.aries.jmx/org.apache.aries.jmx-0.3.1.jar
+cp $ARIES_DIR/sandbox/jmx-next/jmx-bundle/target/org.apache.aries.jmx-1.1.0-SNAPSHOT.jar $JMX_TARGET_DIR/org.apache.aries.jmx/org.apache.aries.jmx-1.1.0.jar
 
 #mkdir -p -v $JMX_TARGET_DIR/org.apache.aries.util
 cp $ARIES_DIR/util/util/target/org.apache.aries.util-0.5-SNAPSHOT.jar $JMX_TARGET_DIR/org.apache.aries.util/org.apache.aries.util-0.5.0.jar
