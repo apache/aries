d109b97c83af618a0701026a34ecedc3b24a758c
John Ross <jwross@apache.org>
2012-06-10 21:04:03 +0000

    ARIES-825: Multiple changes. (1) Fixed ClassCastException when import package header clause declares the version attribute. (2) Now accepts all unknown headers as is with no syntax validation instead of failing the installation. (3) Fixed StackOverflowError caused by regex due to complex pattern combined with lengthy export package headers.
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk/subsystem@1348661 13f79535-47bb-0310-9956-ffa450edef68

--- subsystem-core/src/main/java/org/apache/aries/subsystem/core/archive/GenericClause.java
+++ subsystem-core/src/main/java/org/apache/aries/subsystem/core/archive/GenericClause.java
@@ -22,9 +22,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class GenericClause implements Clause {
-	// TODO The introduction of the whitespace (\\s*) before a parameter was added to get around a grammatically
-	// incorrect clause in Equinox: org.eclipse.osgi; singleton:=true;deployed-version=3.7.0.v20110221;type=osgi.bundle.
-	// Note the space before the singleton directive.
 	private static final String REGEX = '(' + Grammar.PATH + "(?:;" + Grammar.PATH + ")*)(?:;\\s*(" + Grammar.PARAMETER + "))*";
 	
 	private static final Pattern PATTERN = Pattern.compile(REGEX);
