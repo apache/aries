96f32610828df7ca6dd5667bcf5d50f0ebdc7c05
Mark Nuttall <mnuttall@apache.org>
2012-07-03 15:39:17 +0000

    Aries-865: Add tests for OSGi Subsystem CTT section 4B
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk@1356793 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subsystem/subsystem-itests/src/test/java/org/apache/aries/subsystem/ctt/itests/SubsystemDependencyTestBase.java
@@ -0,0 +1,246 @@
+package org.apache.aries.subsystem.ctt.itests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.osgi.framework.namespace.BundleNamespace.BUNDLE_NAMESPACE;
+import static org.osgi.framework.namespace.PackageNamespace.PACKAGE_NAMESPACE;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.aries.subsystem.itests.SubsystemTest;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.Constants;
+import org.osgi.framework.wiring.BundleWire;
+import org.osgi.framework.wiring.BundleWiring;
+import org.osgi.service.subsystem.Subsystem;
+
+/*
+ * A set of tests to cover OSGi Subsystems CTT section 4, "Subsystem Dependency Tests"
+ * This is going to look a bit like ProvisionPolicyTest with a bit of 
+ * DependencyLifecycle thrown in. 
+ * 
+ * 	- The following bundles are used for the tests
+	- Bundle A that export package x
+	- Bundle B that provides capability y
+	- Bundle C that imports package x
+	- Bundle D that requires bundle A
+	- Bundle E that requires capability y
+	- Bundle F that export package x
+	- Bundle G that provides capability y
+	- The following repositories are defined
+	- Repository R1
+	  - Bundle A
+	  - Bundle B
+	  - Bundle C
+	  - Bundle D
+	  - Bundle E
+	  - Bundle F
+	  - Bundle G
+	- Repository R2
+	  - Bundle A
+	  - Bundle B
+	  - Bundle C
+	  - Bundle D
+	  - Bundle E
+ */
+
+public abstract class SubsystemDependencyTestBase extends SubsystemTest 
+{
+	protected static String BUNDLE_A = "sdt_bundle.a.jar";
+	protected static String BUNDLE_B = "sdt_bundle.b.jar";
+	protected static String BUNDLE_C = "sdt_bundle.c.jar";
+	protected static String BUNDLE_D = "sdt_bundle.d.jar";
+	protected static String BUNDLE_E = "sdt_bundle.e.jar";
+	protected static String BUNDLE_F = "sdt_bundle.f.jar";
+	protected static String BUNDLE_G = "sdt_bundle.g.jar";
+
+	@BeforeClass
+	public static void createStaticResources() throws Exception
+	{ 
+		createBundleA();
+		createBundleB();
+		createBundleC();
+		createBundleD();
+		createBundleE();
+		createBundleF();
+		createBundleG();
+	}
+	
+	@Before
+	public void setUp() throws Exception
+	{
+		super.setUp();
+	}
+	
+	@After
+	public void tearDown() 
+	{ 
+		super.tearDown();
+	}
+	
+	
+	private static void createBundleA() throws Exception
+	{ 
+		Map<String, String> headers = new HashMap<String, String>();
+		headers.put(Constants.BUNDLE_VERSION, "1.0.0");
+		headers.put(Constants.EXPORT_PACKAGE, "x");
+		createBundle(BUNDLE_A, headers);
+	}
+	
+	private static void createBundleB() throws Exception
+	{
+		Map<String, String> headers = new HashMap<String, String>();
+		headers.put(Constants.BUNDLE_VERSION, "1.0.0");
+		headers.put(Constants.PROVIDE_CAPABILITY, "y;bug=true"); // TODO: see comment below about bug=true
+		createBundle(BUNDLE_B, headers);
+	}
+	
+	private static void createBundleC() throws Exception
+	{
+		Map<String, String> headers = new HashMap<String, String>();
+		headers.put(Constants.BUNDLE_VERSION, "1.0.0");
+		headers.put(Constants.IMPORT_PACKAGE, "x");
+		createBundle(BUNDLE_C, headers);
+	}
+	
+	private static void createBundleD() throws Exception
+	{
+		Map<String, String> headers = new HashMap<String, String>();
+		headers.put(Constants.BUNDLE_VERSION, "1.0.0");
+		headers.put(Constants.REQUIRE_BUNDLE, BUNDLE_A);
+		createBundle(BUNDLE_D, headers);
+	}
+	
+	private static void createBundleE() throws Exception 
+	{
+		Map<String, String> headers = new HashMap<String, String>();
+		headers.put(Constants.BUNDLE_VERSION, "1.0.0");
+		headers.put(Constants.REQUIRE_CAPABILITY, "y;filter:=\"(bug=true)\"");
+		// TODO:
+		/*
+		 * According to the OSGi Core Release 5 spec section 3.3.6 page 35, 
+		 *   "A filter is optional, if no filter directive is specified the requirement always matches."
+		 *  
+		 * If omitted, we first get an NPE in DependencyCalculator.MissingCapability.initializeAttributes(). 
+		 * If that's fixed, we get exceptions of the form, 
+		 * 
+		 *  Caused by: java.lang.IllegalArgumentException: The filter must not be null.
+		 *    at org.eclipse.equinox.internal.region.StandardRegionFilterBuilder.allow(StandardRegionFilterBuilder.java:49)
+		 *    at org.apache.aries.subsystem.core.internal.SubsystemResource.setImportIsolationPolicy(SubsystemResource.java:655)
+	     * 
+	     * This looks to be an Equinox defect - at least in the level of 3.8.0 currently being used by these tests. 
+		 */
+		createBundle(BUNDLE_E, headers);
+	}
+
+	private static void createBundleF() throws Exception 
+	{
+		Map<String, String> headers = new HashMap<String, String>();
+		headers.put(Constants.BUNDLE_VERSION, "1.0.0");
+		headers.put(Constants.EXPORT_PACKAGE, "x");
+		createBundle(BUNDLE_F, headers);
+	}
+	
+	private static void createBundleG() throws Exception 
+	{
+		Map<String, String> headers = new HashMap<String, String>();
+		headers.put(Constants.BUNDLE_VERSION, "1.0.0");
+		headers.put(Constants.PROVIDE_CAPABILITY, "y;bug=true");      // TODO: see comment above about bug=true
+		createBundle(BUNDLE_G, headers);
+	}
+	
+	protected void registerRepositoryR1() throws Exception
+	{ 
+		registerRepositoryService(BUNDLE_A, BUNDLE_B, 
+				BUNDLE_C, BUNDLE_D, BUNDLE_E, BUNDLE_F, BUNDLE_G);
+	}
+	
+	protected void registerRepositoryR2() throws Exception
+	{
+		registerRepositoryService(BUNDLE_A, BUNDLE_B, 
+				BUNDLE_C, BUNDLE_D, BUNDLE_E);
+	}
+	
+
+	/**
+	 * Check that wiredBundleName in subsystem s is wired to a single package, 
+	 * expectedPackage, from expectedProvidingBundle
+	 * @param s
+	 * @param wiredBundleName
+	 * @param expectedPackage
+	 * @param expectedProvidingBundle
+	 */
+	protected void verifySinglePackageWiring (Subsystem s, String wiredBundleName, String expectedPackage, String expectedProvidingBundle)
+	{
+		Bundle wiredBundle = getBundle(s, wiredBundleName);
+		assertNotNull ("Bundle not found", wiredBundleName);
+
+		BundleWiring wiring = wiredBundle.adapt(BundleWiring.class);
+		List<BundleWire> wiredPackages = wiring.getRequiredWires(PACKAGE_NAMESPACE);
+		assertEquals ("Only one package expected", 1, wiredPackages.size());
+		
+		String packageName = (String) 
+			wiredPackages.get(0).getCapability().getAttributes().get(PACKAGE_NAMESPACE);
+		assertEquals ("Wrong package found", expectedPackage, packageName);
+		
+		String providingBundle = wiredPackages.get(0).getProvider().getSymbolicName();
+		assertEquals ("Package provided by wrong bundle", expectedProvidingBundle, providingBundle);
+	}
+	
+	/**
+	 * Verify that the Require-Bundle of wiredBundleName in subsystem s is met by a wire
+	 * to expectedProvidingBundleName
+	 * @param s
+	 * @param wiredBundleName
+	 * @param expectedProvidingBundleName
+	 */
+	protected void verifyRequireBundleWiring (Subsystem s, String wiredBundleName, String expectedProvidingBundleName)
+	{
+		Bundle wiredBundle = getBundle(s, BUNDLE_D);
+		assertNotNull ("Target bundle " + wiredBundleName + " not found", wiredBundle);
+	
+		BundleWiring wiring = wiredBundle.adapt(BundleWiring.class);
+		List<BundleWire> wiredBundles = wiring.getRequiredWires(BUNDLE_NAMESPACE);
+		assertEquals ("Only one bundle expected", 1, wiredBundles.size());
+	
+		String requiredBundleName = (String)
+			wiredBundles.get(0).getCapability().getAttributes().get(BUNDLE_NAMESPACE);
+		assertEquals ("Wrong bundle requirement", BUNDLE_A, requiredBundleName);
+	
+		String providingBundle = wiredBundles.get(0).getProvider().getSymbolicName();
+		assertEquals ("Wrong bundle provider", expectedProvidingBundleName, providingBundle);
+	}
+	
+	/**
+	 * Verify that a bundle with wiredBundleName imports a single capability in namespace
+	 * from expectedProvidingBundleName
+	 * @param s
+	 * @param wiredBundleName
+	 * @param namespace
+	 * @param expectedProvidingBundleName
+	 */
+	protected void verifyCapabilityWiring (Subsystem s, String wiredBundleName, 
+			String namespace, String expectedProvidingBundleName)
+	{
+		Bundle wiredBundle = getBundle(s, wiredBundleName);
+		assertNotNull ("Targt bundle " + wiredBundleName + " not found", wiredBundleName);
+		
+		BundleWiring wiring = wiredBundle.adapt(BundleWiring.class);
+		List<BundleWire> wiredProviders = wiring.getRequiredWires(namespace);
+		assertEquals("Only one wire for capability namespace " + namespace +" expected", 
+				1, wiredProviders.size());
+		
+		String capabilityNamespace = (String)
+			wiredProviders.get(0).getCapability().getNamespace();
+		assertEquals ("Wrong namespace", namespace, capabilityNamespace);
+		
+		String providingBundle = wiredProviders.get(0).getProvider().getSymbolicName();
+		assertEquals ("Wrong bundle provider", expectedProvidingBundleName, providingBundle);
+	}
+
+}
---
+++ subsystem/subsystem-itests/src/test/java/org/apache/aries/subsystem/ctt/itests/SubsystemDependency_4ATest.java
@@ -0,0 +1,141 @@
+package org.apache.aries.subsystem.ctt.itests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.osgi.framework.namespace.BundleNamespace.BUNDLE_NAMESPACE;
+import static org.osgi.framework.namespace.PackageNamespace.PACKAGE_NAMESPACE;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.wiring.BundleWire;
+import org.osgi.framework.wiring.BundleWiring;
+import org.osgi.service.subsystem.Subsystem;
+import org.osgi.service.subsystem.SubsystemConstants;
+
+/*
+ * First block: section 4A
+ * 
+A) Test a transitively closed subsystem deploys no transitive resources
+ - Register repository R1
+ - Using the Root subsystem, install a scoped subsystem with the following content bundles and no local repository
+   - Bundle A
+   - Bundle B
+   - Bundle C
+   - Bundle D
+   - Bundle E
+ - Verify the wiring of C, D and E wire to A->x, A, B->y respectively
+ - Verify no new bundles are installed into the Root subsystem (particularly bundles F and G)
+ 
+*/
+
+
+@RunWith(JUnit4TestRunner.class)
+public class SubsystemDependency_4ATest extends SubsystemDependencyTestBase 
+{
+	protected static String APPLICATION_A="sdt_application.a.esa";
+
+	@BeforeClass
+	public static void createTestApplication() throws Exception
+	{ 
+		createTestApplicationA();
+	}
+	
+	@Before
+	public void setUp() throws Exception
+	{
+		super.setUp();
+		registerRepositoryR1();
+	}
+	
+	@Test
+	public void verifyBundleCWiredToPackageXFromBundleA() throws Exception
+	{ 
+		Subsystem s = installSubsystemFromFile(APPLICATION_A);
+		startSubsystem(s);
+		
+		verifySinglePackageWiring (s, BUNDLE_C, "x", BUNDLE_A);
+ 
+		stopSubsystem(s);
+	}
+	
+	@Test
+	public void verifyBundleDWiredToBundleA() throws Exception
+	{ 
+		Subsystem s = installSubsystemFromFile(APPLICATION_A);
+		startSubsystem(s);
+		verifyRequireBundleWiring (s, BUNDLE_D, BUNDLE_A);
+		stopSubsystem(s);
+	}
+	
+	@Test
+	public void verifyBundleEWiredToCapability_yFromBundleB() throws Exception
+	{
+		Subsystem s = installSubsystemFromFile (APPLICATION_A);
+		startSubsystem(s);
+		verifyCapabilityWiring (s, BUNDLE_E, "y", BUNDLE_B);
+		stopSubsystem(s);
+	}
+	
+	/*
+	 * Verify no new bundles are installed into the Root subsystem 
+	 * (particularly bundles F and G)
+ 	 * 
+ 	 * As of the time of writing, the Root subsystem should contain 23 bundles: 
+ 	 * org.eclipse.osgi
+     * org.ops4j.pax.exam
+     * org.ops4j.pax.exam.junit.extender
+     * org.ops4j.pax.exam.junit.extender.impl
+     * org.ops4j.pax.logging.pax-logging-api
+     * org.ops4j.pax.logging.pax-logging-service
+     * org.ops4j.pax.url.mvn
+     * org.eclipse.osgi.services
+     * org.eclipse.equinox.region
+     * org.apache.aries.testsupport.unit
+     * org.apache.aries.application.api
+     * org.apache.aries.util
+     * org.apache.aries.application.utils
+     * org.apache.felix.bundlerepository
+     * org.apache.felix.resolver
+     * org.eclipse.equinox.coordinator
+     * org.eclipse.equinox.event
+     * org.apache.aries.subsystem.api
+     * org.apache.aries.subsystem.core
+     * com.springsource.org.junit
+     * org.ops4j.pax.exam.rbc
+     * org.osgi.service.subsystem.region.context.0
+     * pax-exam-probe
+     * 
+	 */
+	@Test
+	public void verifyNoUnexpectedBundlesProvisioned() 
+	{ 
+		Bundle[] bundles = bundleContext.getBundles();
+		assertEquals ("Wrong number of bundles in the Root subsystem", 23, bundles.length);
+		for (Bundle b: bundles) {
+			assertTrue ("Bundle F should not have been provisioned!", !b.getSymbolicName().equals(BUNDLE_F));
+			assertTrue ("Bundle G should not have been provisioned!", !b.getSymbolicName().equals(BUNDLE_G));
+		}
+	}
+
+	private static void createTestApplicationA() throws Exception
+	{
+		Map<String, String> attributes = new HashMap<String, String>();
+		attributes.put(SubsystemConstants.SUBSYSTEM_SYMBOLICNAME, APPLICATION_A);
+		attributes.put(SubsystemConstants.SUBSYSTEM_TYPE, SubsystemConstants.SUBSYSTEM_TYPE_APPLICATION);
+		String appContent = BUNDLE_A +","+ BUNDLE_B + "," + BUNDLE_C
+			+ "," + BUNDLE_D + "," + BUNDLE_E;
+		attributes.put(SubsystemConstants.SUBSYSTEM_CONTENT, appContent);
+		createManifest(APPLICATION_A + ".mf", attributes);
+		createSubsystem(APPLICATION_A);
+	}
+
+}
