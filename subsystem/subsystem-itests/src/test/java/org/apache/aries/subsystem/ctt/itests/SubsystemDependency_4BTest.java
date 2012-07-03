a4add43434bf4820950f66648a8196e4cb15f4f6
Mark Nuttall <mnuttall@apache.org>
2012-07-03 15:39:17 +0000

    Aries-865: Add tests for OSGi Subsystem CTT section 4B
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk@1356793 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subsystem/subsystem-itests/src/test/java/org/apache/aries/subsystem/ctt/itests/SubsystemDependency_4BTest.java
@@ -0,0 +1,109 @@
+package org.apache.aries.subsystem.ctt.itests;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.Constants;
+import org.osgi.service.subsystem.Subsystem;
+import org.osgi.service.subsystem.SubsystemConstants;
+
+/*
+ * B) Test with no pre-installed transitive resources
+     - Register repository R2
+     - Using the Root subsystem, install a scoped subsystem with the following content bundles and no local repository
+       - Bundle C
+       - Bundle D
+       - Bundle E
+     - Verify that bundles A and B got installed into the Root Subsystem
+     - Verify the wiring of C, D and E wire to A->x, A, B->y respectively
+ */
+
+@RunWith(JUnit4TestRunner.class)
+public class SubsystemDependency_4BTest extends SubsystemDependencyTestBase 
+{
+
+	protected static String APPLICATION_B="sdt_application.a.esa";
+	
+	@BeforeClass
+	public static void createTestApp() throws Exception
+	{ 
+		createTestApplicationB();
+	}
+	
+	@Before
+	public void setUp() throws Exception
+	{ 
+		super.setUp();
+		registerRepositoryR2();
+	}
+	
+	// - Verify that bundles A and B got installed into the Root Subsystem
+	@Test
+	public void verifyBundlesAandBinstalledIntoRootRegion() throws Exception
+	{
+		System.out.println ("Into verifyBundlesAandBinstalledIntoRootRegion");
+		
+		Subsystem s = installSubsystemFromFile(APPLICATION_B);
+		startSubsystem(s);
+		Bundle[] bundles = bundleContext.getBundles();
+		Collection<String> bundleNames = new ArrayList<String>();
+		for (Bundle b : bundles) { 
+			bundleNames.add(b.getSymbolicName());
+		}
+		assertTrue ("Bundle A should have been provisioned to the root region", bundleNames.contains(BUNDLE_A));
+		assertTrue ("Bundle B should have been provisioned to the root region", bundleNames.contains(BUNDLE_B));
+	}
+	
+	@Test
+	public void verifyBundleCWiredToPackage_xFromBundleA() throws Exception
+	{
+		Subsystem s = installSubsystemFromFile(APPLICATION_B);
+		startSubsystem(s);
+		verifySinglePackageWiring (s, BUNDLE_C, "x", BUNDLE_A);
+		stopSubsystem(s);
+	}
+	
+	@Test
+	public void verifyBundleDWiredToBundleA() throws Exception
+	{ 
+		Subsystem s = installSubsystemFromFile(APPLICATION_B);
+		startSubsystem(s);
+		verifyRequireBundleWiring (s, BUNDLE_D, BUNDLE_A);
+		stopSubsystem(s);
+	}
+
+	@Test
+	public void verifyBundleEWiredToCapability_yFromBundleB() throws Exception
+	{
+		Subsystem s = installSubsystemFromFile(APPLICATION_B);
+		startSubsystem(s);
+		verifyCapabilityWiring (s, BUNDLE_E, "y", BUNDLE_B);
+		stopSubsystem(s);
+	}
+	
+	private static void createTestApplicationB() throws Exception
+	{ 
+		Map<String, String> attributes = new HashMap<String, String>();
+		attributes.put(SubsystemConstants.SUBSYSTEM_SYMBOLICNAME, APPLICATION_B);
+		attributes.put(SubsystemConstants.SUBSYSTEM_TYPE, SubsystemConstants.SUBSYSTEM_TYPE_COMPOSITE);
+		String appContent = BUNDLE_C + ";" + Constants.VERSION_ATTRIBUTE + "=\"[1.0.0,1.0.0]\""
+			+ ", " + BUNDLE_D + ";" + Constants.VERSION_ATTRIBUTE + "=\"[1.0.0,1.0.0]\""
+			+ ", " + BUNDLE_E + ";" + Constants.VERSION_ATTRIBUTE + "=\"[1.0.0,1.0.0]\"";
+		attributes.put(SubsystemConstants.SUBSYSTEM_CONTENT, appContent);
+		attributes.put(Constants.IMPORT_PACKAGE, "x");
+		attributes.put(Constants.REQUIRE_BUNDLE, BUNDLE_A);
+		attributes.put(Constants.REQUIRE_CAPABILITY, "y;filter:=\"(bug=true)\"");
+		createManifest(APPLICATION_B + ".mf", attributes);
+		createSubsystem(APPLICATION_B);
+	}
+}
