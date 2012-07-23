fc74a1233272c1971453091e6c2ab77b33effadd
Guillaume Nodet <gnodet@apache.org>
2012-07-23 23:06:01 +0000

    [ARIES-878] Threading issues with <cm:managed-properties />
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk@1364831 13f79535-47bb-0310-9956-ffa450edef68

---
+++ blueprint/blueprint-cm/src/test/java/org/apache/aries/blueprint/compendium/cm/Foo.java
@@ -0,0 +1,56 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.aries.blueprint.compendium.cm;
+
+import java.util.Properties;
+
+public class Foo implements FooInterface {
+
+    public Foo() {
+    }
+
+    private int a;
+    private String b;
+    private Properties props;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int i) {
+        a = i;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String i) {
+        b = i;
+    }
+
+    public Properties getProps() {
+        return props;
+    }
+
+    public void setProps(Properties props) {
+        this.props = props;
+    }
+}
+
