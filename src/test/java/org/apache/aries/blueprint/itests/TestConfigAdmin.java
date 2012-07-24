6376a9f1e7930e5dc31c99d34b54d091640600da
Guillaume Nodet <gnodet@apache.org>
2012-07-24 12:38:36 +0000

    Another peek at fixing the integration tests.
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk/blueprint/blueprint-itests@1365011 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/aries/blueprint/itests/TestConfigAdmin.java
+++ src/test/java/org/apache/aries/blueprint/itests/TestConfigAdmin.java
@@ -163,10 +163,11 @@ public class TestConfigAdmin extends AbstractIntegrationTest {
         BlueprintContainer blueprintContainer = Helper.getBlueprintContainerForBundle(context(), "org.apache.aries.blueprint.sample");
         assertNotNull(blueprintContainer);
 
-//        Thread.sleep(100);
-
         // Make sure only one service is registered
         // Ask the service registry, not the container, since the container might have got it wrong :)
+        Foo foo = context().getService(Foo.class, "(service.pid=blueprint-sample-managed-service-factory.*)");
+        assertNotNull(foo);
+
         ServiceReference[] refs = context().getAllServiceReferences(Foo.class.getName(), "(service.pid=blueprint-sample-managed-service-factory.*)");
         
         assertNotNull("No services were registered for the managed service factory", refs);
