ad737c071a2c462866f45a8293042797ce54ecc0
A. J. David Bosschaert <davidb@apache.org>
2012-08-14 10:10:49 +0000

    A minor change to the script that copies the SPI-Fly bundle to the OSGi CT location.
    The SPI-Fly version in this commit passes the OSGi ServiceLoader Mediator CT.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk@1372821 13f79535-47bb-0310-9956-ffa450edef68

--- spi-fly/tck/apply-to-tck.sh
+++ spi-fly/tck/apply-to-tck.sh
@@ -1,11 +1,15 @@
+# This script modifies the SPI-Fly bundle metadata slightly so that it can be copied into the OSGi CT. 
+# It also copies the jar in place.
+
 # Fill in the variable below, e.g. OSGI_GIT_DIR=/Users/david/clones/osgi-build
 OSGI_GIT_DIR=...the root of your OSGi build clone...
+VERSION=1.0.0
 
 # Updated the BSN of the implemetation bundle to org.apache.aries.spifly.dynamic as on some Mac OSX having the bundle suffix
 # causes confusion
 rm META-INF/MANIFEST.MF
-cp ../spi-fly-dynamic-bundle/target/org.apache.aries.spifly.dynamic.bundle-0.4-SNAPSHOT.jar org.apache.aries.spifly.dynamic-0.4.0.jar
-jar xvf org.apache.aries.spifly.dynamic-0.4.0.jar META-INF/MANIFEST.MF
+cp ../spi-fly-dynamic-bundle/target/org.apache.aries.spifly.dynamic.bundle-$VERSION-SNAPSHOT.jar org.apache.aries.spifly.dynamic-$VERSION.jar
+jar xvf org.apache.aries.spifly.dynamic-$VERSION.jar META-INF/MANIFEST.MF
 cat META-INF/MANIFEST.MF | sed s/org.apache.aries.spifly.dynamic.bundle/org.apache.aries.spifly.dynamic/ > UPDATED_MANIFEST.MF
-jar uvfm org.apache.aries.spifly.dynamic-0.4.0.jar UPDATED_MANIFEST.MF
-cp org.apache.aries.spifly.dynamic-0.4.0.jar $OSGI_GIT_DIR/licensed/repo/org.apache.aries.spifly.dynamic
+jar uvfm org.apache.aries.spifly.dynamic-$VERSION.jar UPDATED_MANIFEST.MF
+cp org.apache.aries.spifly.dynamic-$VERSION.jar $OSGI_GIT_DIR/licensed/repo/org.apache.aries.spifly.dynamic
