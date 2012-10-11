b0fe472d36ba074cd57d17c477ad575b7a939f05
John Ross <jwross@apache.org>
2012-10-11 23:05:04 +0000

    Test support for any location string.
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk@1397359 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subsystem/subsystem-core/src/test/java/org/apache/aries/subsystem/core/internal/LocationTest.java
@@ -0,0 +1,37 @@
+package org.apache.aries.subsystem.core.internal;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
+
+import java.net.MalformedURLException;
+
+import org.junit.Test;
+
+public class LocationTest {
+	@Test
+	public void testAnyLocationString() {
+		String locationStr = "anyLocation";
+		try {
+			Location location = new Location(locationStr);
+			assertNull("Wrong symbolic name", location.getSymbolicName());
+			assertEquals("Wrong value", locationStr, location.getValue());
+			assertNull("Wrong version", location.getVersion());
+			try {
+				location.open();
+				fail("Opening a location that does not represent a URL should fail");
+			}
+			catch (MalformedURLException e) {
+				// Okay
+			}
+			catch (Throwable t) {
+				t.printStackTrace();
+				fail("Wrong exception");
+			}
+		}
+		catch (Throwable t) {
+			t.printStackTrace();
+			fail("Any location string must be supported");
+		}
+	}
+}
