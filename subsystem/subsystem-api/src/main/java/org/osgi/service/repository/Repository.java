ae1df1b8d1b07505d0ef9bb8253cf2f42d5dd988
John Ross <jwross@apache.org>
2012-11-26 16:42:30 +0000

    [ARIES-965] Include the org.osgi.service.repository package and both import and export it.
    Subsystems requires the API but not an implementation. The package was therefore included as a convenience for
    users who also do not need a repository implementation.
    
    Also added missing packageinfo files.
    
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk@1413711 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subsystem/subsystem-api/src/main/java/org/osgi/service/repository/Repository.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) OSGi Alliance (2006, 2012). All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// This document is an experimental draft to enable interoperability
+// between bundle repositories. There is currently no commitment to 
+// turn this draft into an official specification.  
+
+package org.osgi.service.repository;
+
+import java.util.Collection;
+import java.util.Map;
+import org.osgi.resource.Capability;
+import org.osgi.resource.Requirement;
+import org.osgi.resource.Resource;
+
+/**
+ * A repository service that contains {@link Resource resources}.
+ * 
+ * <p>
+ * Repositories may be registered as services and may be used as by a resolve
+ * context during resolver operations.
+ * 
+ * <p>
+ * Repositories registered as services may be filtered using standard service
+ * properties.
+ * 
+ * @ThreadSafe
+ * @noimplement
+ * @version $Id$
+ */
+public interface Repository {
+	/**
+	 * Service property to provide URLs related to this repository.
+	 * 
+	 * <p>
+	 * The value of this property must be of type {@code String},
+	 * {@code String[]}, or {@code Collection<String>}.
+	 */
+	String	URL	= "repository.url";
+
+	/**
+	 * Find the capabilities that match the specified requirements.
+	 * 
+	 * @param requirements The requirements for which matching capabilities
+	 *        should be returned. Must not be {@code null}.
+	 * @return A map of matching capabilities for the specified requirements.
+	 *         Each specified requirement must appear as a key in the map. If
+	 *         there are no matching capabilities for a specified requirement,
+	 *         then the value in the map for the specified requirement must be
+	 *         an empty collection. The returned map is the property of the
+	 *         caller and can be modified by the caller.
+	 */
+	Map<Requirement, Collection<Capability>> findProviders(Collection<? extends Requirement> requirements);
+}
