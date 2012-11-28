962cd0c2decf35f02e586d00e44b2e63b645ce5f
John Ross <jwross@apache.org>
2012-11-28 16:41:29 +0000

    [ARIES-968] Consider using a "multiple attempts" strategy versus the current locking strategy when updating sharing policies.
    The synchronization has	been replaced with a retry strategy. If	the region digraph fails to replace because something else
    modified it after the copy was made, up	to 9 additional	attempts will be made, for a total of 10. This has the advantage of
    avoiding a bottleneck as well as protecting against modifications from actors other than subsystems. If replacement still
    fails after the 10th attempt, a SubsystemException is thrown.
    
    Also created a more robust test for adding requirements. This required some fixes to the copy/replace algorithm in order to
    account for bundle ids and child connections.
    
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk/subsystem@1414804 13f79535-47bb-0310-9956-ffa450edef68

--- subsystem-itests/src/test/java/org/apache/aries/subsystem/itests/AriesSubsystemTest.java
+++ subsystem-itests/src/test/java/org/apache/aries/subsystem/itests/AriesSubsystemTest.java
@@ -1,5 +1,6 @@
 package org.apache.aries.subsystem.itests;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.fail;
 
@@ -18,14 +19,15 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.junit.MavenConfiguredJUnit4TestRunner;
+import org.osgi.framework.Constants;
+import org.osgi.framework.Version;
+import org.osgi.framework.namespace.IdentityNamespace;
 import org.osgi.framework.namespace.PackageNamespace;
 import org.osgi.resource.Requirement;
 import org.osgi.resource.Resource;
 import org.osgi.service.subsystem.Subsystem;
 import org.osgi.service.subsystem.SubsystemConstants;
 import org.osgi.service.subsystem.SubsystemException;
-
-import aQute.lib.osgi.Constants;
 
 @RunWith(MavenConfiguredJUnit4TestRunner.class)
 public class AriesSubsystemTest extends SubsystemTest {
@@ -35,10 +37,20 @@ public class AriesSubsystemTest extends SubsystemTest {
 	 */
 	private static final String APPLICATION_A = "application.a.esa";
 	/*
+	 * Subsystem-SymbolicName: application.b.esa
+	 * Subsystem-Content: bundle.b.jar
+	 */
+	private static final String APPLICATION_B = "application.b.esa";
+	/*
 	 * Bundle-SymbolicName: bundle.a.jar
 	 * Import-Package: org.osgi.framework,org.osgi.resource
 	 */
 	private static final String BUNDLE_A = "bundle.a.jar";
+	/*
+	 * Bundle-SymbolicName: bundle.b.jar
+	 * Import-Package: org.osgi.resource
+	 */
+	private static final String BUNDLE_B = "bundle.b.jar";
 	/*
 	 * Subsystem-SymbolicName: composite.a.esa
 	 * Subsystem-Type: osgi.subsystem.composite
@@ -50,10 +62,21 @@ public class AriesSubsystemTest extends SubsystemTest {
 		createSubsystem(APPLICATION_A, BUNDLE_A);
 	}
 	
+	private static void createApplicationB() throws IOException {
+		createApplicationBManifest();
+		createSubsystem(APPLICATION_B, BUNDLE_B);
+	}
+	
 	private static void createApplicationAManifest() throws IOException {
 		Map<String, String> attributes = new HashMap<String, String>();
 		attributes.put(SubsystemConstants.SUBSYSTEM_SYMBOLICNAME, APPLICATION_A);
 		createManifest(APPLICATION_A + ".mf", attributes);
+	}
+	
+	private static void createApplicationBManifest() throws IOException {
+		Map<String, String> attributes = new HashMap<String, String>();
+		attributes.put(SubsystemConstants.SUBSYSTEM_SYMBOLICNAME, APPLICATION_B);
+		createManifest(APPLICATION_B + ".mf", attributes);
 	}
 	
 	private static void createBundleA() throws IOException {
@@ -62,15 +85,24 @@ public class AriesSubsystemTest extends SubsystemTest {
 		createBundle(BUNDLE_A, headers);
 	}
 	
+	private static void createBundleB() throws IOException {
+		Map<String, String> headers = new HashMap<String, String>();
+		headers.put(Constants.IMPORT_PACKAGE, "org.osgi.resource");
+		createBundle(BUNDLE_B, headers);
+	}
+	
 	private static void createCompositeA() throws IOException {
 		createCompositeAManifest();
-		createSubsystem(COMPOSITE_A);
+		createSubsystem(COMPOSITE_A, BUNDLE_B, APPLICATION_B);
 	}
 	
 	private static void createCompositeAManifest() throws IOException {
 		Map<String, String> attributes = new HashMap<String, String>();
 		attributes.put(SubsystemConstants.SUBSYSTEM_SYMBOLICNAME, COMPOSITE_A);
 		attributes.put(SubsystemConstants.SUBSYSTEM_TYPE, SubsystemConstants.SUBSYSTEM_TYPE_COMPOSITE);
+		attributes.put(SubsystemConstants.SUBSYSTEM_CONTENT, 
+				BUNDLE_B + ';' + IdentityNamespace.CAPABILITY_VERSION_ATTRIBUTE + "=\"[0,0]\","
+				+ APPLICATION_B + ';' + IdentityNamespace.CAPABILITY_VERSION_ATTRIBUTE + "=\"[0,0]\";" + IdentityNamespace.CAPABILITY_TYPE_ATTRIBUTE + '=' + SubsystemConstants.SUBSYSTEM_TYPE_APPLICATION);
 		attributes.put(Constants.IMPORT_PACKAGE, "org.osgi.resource");
 		createManifest(COMPOSITE_A + ".mf", attributes);
 	}
@@ -81,7 +113,9 @@ public class AriesSubsystemTest extends SubsystemTest {
 		if (createdTestFiles)
 			return;
 		createBundleA();
+		createBundleB();
 		createApplicationA();
+		createApplicationB();
 		createCompositeA();
 		createdTestFiles = true;
 	}
@@ -90,31 +124,62 @@ public class AriesSubsystemTest extends SubsystemTest {
 		super.setUp();
 	}
 	
+	/*
+	 * Test the AriesSubsystem.addRequirements(Collection<Requirement>) method.
+	 * 
+	 * There are several things to consider for this test.
+	 * 
+	 * (1) Installing a child subsystem before the requirement has been added
+	 *     should fail.
+	 * (2) Installing a child subsystem after the requirement has been added
+	 *     should succeed.
+	 * (3) The newly created region should contain all of the bundles from the
+	 *     old one.
+	 * (4) The connections between the subsystem with the added requirement and
+	 *     its parents should be reestablished.
+	 * (5) The connections between the subsystem with the added requirement and
+	 *     its children should be reestablished.
+	 */
 	@Test
 	public void testAddRequirements() throws Exception {
-		Subsystem compositeA = installSubsystemFromFile(COMPOSITE_A);
+		AriesSubsystem compositeA = (AriesSubsystem)installSubsystemFromFile(COMPOSITE_A);
 		try {
+			startSubsystem(compositeA);
+			assertCompositeABefore(compositeA);
+			// Test that the installation of applicationA fails.
 			try {
 				installSubsystemFromFile(compositeA, APPLICATION_A);
-				fail("Subsystem should not have installed");
+				fail("Subsystem should not have installed due to unresolved org.osgi.framework package requirement");
 			} catch (SubsystemException e) {
 				// Okay.
 			}
+			// Add the org.osgi.framework package requirement.
 			Requirement requirement = new BasicRequirement.Builder()
-					.namespace(PackageNamespace.PACKAGE_NAMESPACE)
-					.directive(
-							PackageNamespace.REQUIREMENT_FILTER_DIRECTIVE, 
-							"(osgi.wiring.package=org.osgi.framework)")
+				.namespace(PackageNamespace.PACKAGE_NAMESPACE)
+				.directive(
+					PackageNamespace.REQUIREMENT_FILTER_DIRECTIVE, 
+					"(osgi.wiring.package=org.osgi.framework)")
 					.resource(EasyMock.createMock(Resource.class))
 					.build();
-			((AriesSubsystem)compositeA).addRequirements(Collections.singleton(requirement));
+			compositeA.addRequirements(Collections.singleton(requirement));
+			// Test that the bundles were copied over to the newly created region.
+			assertCompositeABefore(compositeA);
+			// Test that the parent connections were reestablished.
+			assertRefreshAndResolve(Collections.singletonList(getConstituentAsBundle(compositeA, BUNDLE_B, null, null)));
+			// Test that the child connections were reestablished.
+			assertRefreshAndResolve(Collections.singletonList(getConstituentAsBundle(getConstituentAsSubsystem(compositeA, APPLICATION_B, null, SubsystemConstants.SUBSYSTEM_TYPE_APPLICATION), BUNDLE_B, null, null)));
+			// Test that the installation of applicationA succeeds.
+			AriesSubsystem applicationA;
 			try {
-				installSubsystemFromFile(compositeA, APPLICATION_A);
+				applicationA = (AriesSubsystem)installSubsystemFromFile(compositeA, APPLICATION_A);
+				startSubsystem(applicationA);
 			} catch (SubsystemException e) {
-				fail("Subsystem should have installed");
+				fail("Subsystem should have installed and started");
 			}
-		} finally {
-			uninstallSubsystemSilently(compositeA);
+			assertCompositeAAfter(compositeA);
+		}
+		finally {
+			stopAndUninstallSubsystemSilently(compositeA);
 		}
 	}
 	
@@ -148,5 +213,48 @@ public class AriesSubsystemTest extends SubsystemTest {
 			fail(AriesSubsystem.class.getName() + " service not registered");
 		}
 		assertSame("Services should be the same instance", root1, root2);
+	}
+	
+	private void assertCompositeAAfter(Subsystem compositeA) {
+		// applicationA, applicationB, bundleB, region context bundle
+		assertConstituents(4, compositeA);
+		assertConstituent(compositeA, APPLICATION_A, null, SubsystemConstants.SUBSYSTEM_TYPE_APPLICATION);
+		assertConstituent(compositeA, APPLICATION_B, null, SubsystemConstants.SUBSYSTEM_TYPE_APPLICATION);
+		assertConstituent(compositeA, BUNDLE_B);
+		assertNotNull("Bundle not in region", getRegion(compositeA).getBundle(BUNDLE_B, Version.emptyVersion));
+		assertConstituent(compositeA, "org.osgi.service.subsystem.region.context.1", Version.parseVersion("1"));
+		// applicationA, applicationB
+		assertChildren(2, compositeA);
+		assertApplicationA(assertChild(compositeA, APPLICATION_A));
+		assertApplicationB(assertChild(compositeA, APPLICATION_B));
+	}
+	
+	private void assertCompositeABefore(Subsystem compositeA) {
+		// applicationB, bundleB, region context bundle
+		assertConstituents(3, compositeA);
+		assertConstituent(compositeA, APPLICATION_B, null, SubsystemConstants.SUBSYSTEM_TYPE_APPLICATION);
+		assertConstituent(compositeA, BUNDLE_B);
+		assertNotNull("Bundle not in region", getRegion(compositeA).getBundle(BUNDLE_B, Version.emptyVersion));
+		assertConstituent(compositeA, "org.osgi.service.subsystem.region.context.1", Version.parseVersion("1"));
+		// applicationB
+		assertChildren(1, compositeA);
+		assertApplicationB(assertChild(compositeA, APPLICATION_B));
+	}
+	
+	private void assertApplicationA(Subsystem applicationA) {
+		// bundleA, region context bundle
+		assertConstituents(2, applicationA);
+		assertConstituent(applicationA, BUNDLE_A);
+		// The subsystem id is 4 instead of 3 due to the first installation that failed.
+		assertConstituent(applicationA, "org.osgi.service.subsystem.region.context.4", Version.parseVersion("1"));
+		assertChildren(0, applicationA);
+	}
+	
+	private void assertApplicationB(Subsystem applicationB) {
+		// bundleB, region context bundle
+		assertConstituents(2, applicationB);
+		assertConstituent(applicationB, BUNDLE_B);
+		assertConstituent(applicationB, "org.osgi.service.subsystem.region.context.2", Version.parseVersion("1"));
+		assertChildren(0, applicationB);
 	}
 }
