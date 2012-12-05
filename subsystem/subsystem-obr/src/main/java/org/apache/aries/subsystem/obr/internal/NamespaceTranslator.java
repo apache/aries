6c69a67f7764fa45a2acb193d55bbede19a7452b
John Ross <jwross@apache.org>
2012-12-05 16:53:04 +0000

    Integrate Felix Repository Admin with Subsystems.
    The subsystem-obr bundle provides an org.osgi.service.repository.Repository wrapper around registered RepositoryAdmin services.
    This makes bundles within Felix OBR available to resolving subsystems.
    
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk@1417524 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subsystem/subsystem-obr/src/main/java/org/apache/aries/subsystem/obr/internal/NamespaceTranslator.java
@@ -0,0 +1,47 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.aries.subsystem.obr.internal;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.osgi.framework.namespace.BundleNamespace;
+import org.osgi.framework.namespace.HostNamespace;
+import org.osgi.framework.namespace.PackageNamespace;
+import org.osgi.namespace.service.ServiceNamespace;
+
+public class NamespaceTranslator {
+	private static final Map<String, String> namespaces = computeNamespaces();
+	
+	private static Map<String, String> computeNamespaces() {
+		Map<String, String> result = new HashMap<String, String>(8);
+		result.put(PackageNamespace.PACKAGE_NAMESPACE, org.apache.felix.bundlerepository.Capability.PACKAGE);
+		result.put(ServiceNamespace.SERVICE_NAMESPACE, org.apache.felix.bundlerepository.Capability.SERVICE);
+		result.put(BundleNamespace.BUNDLE_NAMESPACE, org.apache.felix.bundlerepository.Capability.BUNDLE);
+		result.put(HostNamespace.HOST_NAMESPACE, org.apache.felix.bundlerepository.Capability.FRAGMENT);
+		result.put(org.apache.felix.bundlerepository.Capability.PACKAGE, PackageNamespace.PACKAGE_NAMESPACE);
+		result.put(org.apache.felix.bundlerepository.Capability.SERVICE, ServiceNamespace.SERVICE_NAMESPACE);
+		result.put(org.apache.felix.bundlerepository.Capability.BUNDLE, BundleNamespace.BUNDLE_NAMESPACE);
+		result.put(org.apache.felix.bundlerepository.Capability.FRAGMENT, HostNamespace.HOST_NAMESPACE);
+		return Collections.unmodifiableMap(result);
+	}
+	
+	public static String translate(String namespace) {
+		String result = namespaces.get(namespace);
+		if (result == null)
+			result = namespace;
+		return result;
+	}
+}
