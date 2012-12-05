106d37852cb025ceaf44bd957b26eb6515646eb9
John Ross <jwross@apache.org>
2012-12-05 16:52:51 +0000

    Make service capabilities from preexisting root region bundles available to the system repository.
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk/subsystem@1417523 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subsystem-core/src/main/java/org/apache/aries/subsystem/core/internal/BundleRevisionResource.java
@@ -0,0 +1,129 @@
+package org.apache.aries.subsystem.core.internal;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.aries.application.modelling.ExportedService;
+import org.apache.aries.application.modelling.ImportedService;
+import org.apache.aries.application.modelling.ModelledResource;
+import org.apache.aries.application.modelling.ModelledResourceManager;
+import org.apache.aries.application.modelling.ModellerException;
+import org.osgi.framework.wiring.BundleRevision;
+import org.osgi.namespace.service.ServiceNamespace;
+import org.osgi.resource.Capability;
+import org.osgi.resource.Namespace;
+import org.osgi.resource.Requirement;
+import org.osgi.resource.Resource;
+import org.osgi.service.subsystem.SubsystemException;
+
+public class BundleRevisionResource implements Resource {
+	private final BundleRevision revision;
+	
+	private volatile ModelledResource resource;
+	
+	public BundleRevisionResource(BundleRevision revision) {
+		if (revision == null)
+			throw new NullPointerException();
+		this.revision = revision;
+	}
+
+	@Override
+	public List<Capability> getCapabilities(String namespace) {
+		if (namespace == null) {
+			List<Capability> rCaps = revision.getCapabilities(namespace);
+			List<Capability> sCaps = computeServiceCapabilities();
+			List<Capability> result = new ArrayList<Capability>(rCaps.size() + sCaps.size());
+			result.addAll(rCaps);
+			result.addAll(sCaps);
+			return Collections.unmodifiableList(result);
+		}
+		if (ServiceNamespace.SERVICE_NAMESPACE.equals(namespace))
+			return Collections.unmodifiableList(computeServiceCapabilities());
+		return revision.getCapabilities(namespace);
+	}
+
+	@Override
+	public List<Requirement> getRequirements(String namespace) {
+		if (namespace == null) {
+			List<Requirement> rReqs = revision.getRequirements(namespace);
+			List<Requirement> sReqs = computeServiceRequirements();
+			List<Requirement> result = new ArrayList<Requirement>(rReqs.size() + sReqs.size());
+			result.addAll(rReqs);
+			result.addAll(sReqs);
+			return Collections.unmodifiableList(result);
+		}
+		if (ServiceNamespace.SERVICE_NAMESPACE.equals(namespace))
+			return Collections.unmodifiableList(computeServiceRequirements());
+		return revision.getRequirements(namespace);
+	}
+	
+	private ModelledResource computeModelledResource() {
+		Activator activator = Activator.getInstance();
+		ModelledResourceManager manager = activator.getModelledResourceManager();
+		BundleDirectory directory = new BundleDirectory(revision.getBundle());
+		try {
+			return manager.getModelledResource(directory);
+		}
+		catch (ModellerException e) {
+			throw new SubsystemException(e);
+		}
+	}
+	
+	private List<Capability> computeServiceCapabilities() {
+		ModelledResource resource = getModelledResource();
+		Collection<? extends ExportedService> services = resource.getExportedServices();
+		if (services.isEmpty())
+			return Collections.emptyList();
+		List<Capability> result = new ArrayList<Capability>(services.size());
+		for (ExportedService service : services)
+			result.add(new BasicCapability.Builder()
+					.namespace(ServiceNamespace.SERVICE_NAMESPACE)
+					.attribute(ServiceNamespace.CAPABILITY_OBJECTCLASS_ATTRIBUTE, new ArrayList<String>(service.getInterfaces()))
+					.attributes(service.getServiceProperties())
+					.resource(this)
+					.build());
+		return result;
+	}
+	
+	private List<Requirement> computeServiceRequirements() {
+		ModelledResource resource = getModelledResource();
+		Collection<? extends ImportedService> services = resource.getImportedServices();
+		if (services.isEmpty())
+			return Collections.emptyList();
+		List<Requirement> result = new ArrayList<Requirement>(services.size());
+		for (ImportedService service : services) {
+			StringBuilder builder = new StringBuilder("(&(")
+					.append(ServiceNamespace.CAPABILITY_OBJECTCLASS_ATTRIBUTE)
+					.append('=')
+					.append(service.getInterface())
+					.append(')');
+			String filter = service.getFilter();
+			if (filter != null)
+				builder.append('(').append(filter).append(')');
+			builder.append(')');
+			result.add(new BasicRequirement.Builder()
+					.namespace(ServiceNamespace.SERVICE_NAMESPACE)
+					.directive(Namespace.REQUIREMENT_FILTER_DIRECTIVE, builder.toString())
+					.directive(
+							Namespace.REQUIREMENT_RESOLUTION_DIRECTIVE, 
+							service.isOptional() ? Namespace.RESOLUTION_OPTIONAL : Namespace.RESOLUTION_MANDATORY)
+					.resource(this)
+					.build());
+		}
+		return result;
+	}
+
+	private ModelledResource getModelledResource() {
+		ModelledResource result = resource;
+		if (result == null) {
+			synchronized (this) {
+				result = resource;
+				if (result == null)
+					resource = result = computeModelledResource();
+			}
+		}
+		return result;
+	}
+}
