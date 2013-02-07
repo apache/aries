0ef78dc80e3b9ab1e160359cb9f9144fd0ed6b5b
John Ross <jwross@apache.org>
2013-02-07 19:34:36 +0000

    Bundle event hook NPE and unresolved persisted subsystems.
    Fixed NPE in bundle event hook when processing asynchronous installed events whose bundle or origin bundle revision
    was null because the bundle was uninstalled.
    
    Fixed issue where persisted subsystems were registered in the RESOLVED state before they were actually resolved.
    
    Added bundle event hook tests for null bundle and origin bundle revisions when processing asynchronous events.
    
    Fixed issue in itests where subsystems other than root could be returned from utility method.
    
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk@1443681 13f79535-47bb-0310-9956-ffa450edef68

--- subsystem/subsystem-itests/src/test/java/org/apache/aries/subsystem/itests/BundleEventHookTest.java
+++ subsystem/subsystem-itests/src/test/java/org/apache/aries/subsystem/itests/BundleEventHookTest.java
@@ -18,25 +18,36 @@
  */
 package org.apache.aries.subsystem.itests;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
+import org.eclipse.equinox.region.RegionDigraph;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.junit.MavenConfiguredJUnit4TestRunner;
 import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleException;
 import org.osgi.framework.ServiceEvent;
 import org.osgi.framework.ServiceListener;
+import org.osgi.framework.ServiceReference;
+import org.osgi.framework.namespace.IdentityNamespace;
+import org.osgi.service.subsystem.Subsystem;
+import org.osgi.service.subsystem.SubsystemConstants;
 
 @RunWith(MavenConfiguredJUnit4TestRunner.class)
 public class BundleEventHookTest extends SubsystemTest {
@@ -44,6 +55,10 @@ public class BundleEventHookTest extends SubsystemTest {
 	 * Bundle-SymbolicName: bundle.a.jar
 	 */
 	private static final String BUNDLE_A = "bundle.a.jar";
+	/*
+	 * Bundle-SymbolicName: bundle.b.jar
+	 */
+	private static final String BUNDLE_B = "bundle.b.jar";
 	
 	@Before
 	public static void createApplications() throws Exception {
@@ -51,11 +66,16 @@ public class BundleEventHookTest extends SubsystemTest {
 			return;
 		}
 		createBundleA();
+		createBundleB();
 		createdApplications = true;
 	}
 	
 	private static void createBundleA() throws IOException {
 		createBundle(BUNDLE_A);
+	}
+	
+	private static void createBundleB() throws IOException {
+		createBundle(BUNDLE_B);
 	}
     
     /*
@@ -123,5 +143,108 @@ public class BundleEventHookTest extends SubsystemTest {
     	finally {
     		executor.shutdownNow();
     	}
+    }
+    
+    /*
+     * Because bundle events are queued for later asynchronous processing while
+     * the root subsystem is initializing, it is possible to see an installed
+     * event for a bundle that has been uninstalled (i.e. the bundle revision
+     * will be null). These events should be ignored.
+     */
+    @Test
+    public void testIgnoreUninstalledBundleInAsyncInstalledEvent() throws Exception {
+    	final Bundle core = getSubsystemCoreBundle();
+    	core.stop();
+    	final AtomicReference<Bundle> a = new AtomicReference<Bundle>();
+    	bundleContext.addServiceListener(
+    			new ServiceListener() {
+					@Override
+					public void serviceChanged(ServiceEvent event) {
+						if ((event.getType() & (ServiceEvent.REGISTERED | ServiceEvent.MODIFIED)) == 0)
+							return;
+						if (a.get() != null)
+							// We've been here before and already done what needs doing.
+							return;
+						ServiceReference<Subsystem> sr = (ServiceReference<Subsystem>)event.getServiceReference();
+						Subsystem s = bundleContext.getService(sr);
+						try {
+							// Queue up the installed event.
+							a.set(core.getBundleContext().installBundle(BUNDLE_A, new FileInputStream(BUNDLE_A)));
+							// Ensure the bundle will be uninstalled before the event is processed.
+							a.get().uninstall();
+						}
+						catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+    			}, 
+    			"(&(objectClass=org.osgi.service.subsystem.Subsystem)(subsystem.id=0)(subsystem.state=RESOLVED))");
+    	try {
+    		// Before the fix, this would fail due to an NPE resulting from a
+    		// null bundle revision.
+    		core.start();
+    	}
+    	catch (BundleException e) {
+    		e.printStackTrace();
+    		fail("Subsystems failed to handle an asynchronous bundle installed event after the bundle was uninstalled");
+    	}
+    	assertBundleState(a.get(), Bundle.UNINSTALLED);
+    	Subsystem root = getRootSubsystem();
+    	assertState(Subsystem.State.ACTIVE, root);
+    	assertNotConstituent(root, a.get().getSymbolicName());
+    }
+    
+    /*
+     * Because bundle events are queued for later asynchronous processing while
+     * the root subsystem is initializing, it is possible to see an installed
+     * event whose origin bundle has been uninstalled (i.e. the origin bundle's
+     * revision will be null). These events should result in the installed
+     * bundle being associated with the root subsystem.
+     */
+    @Test
+    public void testIgnoreUninstalledOriginBundleInAsyncInstalledEvent() throws Exception {
+    	final Bundle core = getSubsystemCoreBundle();
+    	core.stop();
+    	final Bundle b = bundleContext.installBundle(BUNDLE_B, new FileInputStream(BUNDLE_B));
+    	// Ensure bundle B has a context.
+    	b.start();
+    	final AtomicReference<Bundle> a = new AtomicReference<Bundle>();
+    	bundleContext.addServiceListener(
+    			new ServiceListener() {
+					@Override
+					public void serviceChanged(ServiceEvent event) {
+						if ((event.getType() & (ServiceEvent.REGISTERED | ServiceEvent.MODIFIED)) == 0)
+							return;
+						if (a.get() != null)
+							// We've been here before and already done what needs doing.
+							return;
+						ServiceReference<Subsystem> sr = (ServiceReference<Subsystem>)event.getServiceReference();
+						Subsystem s = bundleContext.getService(sr);
+						try {
+							// Queue up the installed event for bundle A using B's context.
+							a.set(b.getBundleContext().installBundle(BUNDLE_A, new FileInputStream(BUNDLE_A)));
+							// Ensure the origin bundle will be uninstalled before the event is processed.
+							b.uninstall();
+						}
+						catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+    			}, 
+    			"(&(objectClass=org.osgi.service.subsystem.Subsystem)(subsystem.id=0)(subsystem.state=RESOLVED))");
+    	try {
+    		// Before the fix, this would fail due to an NPE resulting from a
+    		// null bundle revision.
+    		core.start();
+    	}
+    	catch (BundleException e) {
+    		e.printStackTrace();
+    		fail("Subsystems failed to handle an asynchronous bundle installed event after the origin bundle was uninstalled");
+    	}
+    	assertBundleState(a.get(), Bundle.INSTALLED);
+    	assertBundleState(b, Bundle.UNINSTALLED);
+    	Subsystem root = getRootSubsystem();
+    	assertState(Subsystem.State.ACTIVE, root);
+    	assertConstituent(root, a.get().getSymbolicName());
     }
 }
