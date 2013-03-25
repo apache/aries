17c505f01e393dfba2944f0ddbba4c731ad191b1
Guillaume Nodet <gnodet@apache.org>
2013-03-25 15:05:56 +0000

    initial spike of ARIES-1038 so that blueprint-noosgi can be easily used inside a servlet engine without an OSGi environment; but booting up all the META-INF/blueprint.xml files and performing the DI on everything using blueprint-noosgi
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk/blueprint/blueprint-web@1460705 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/aries/blueprint/web/BlueprintContextListener.java
@@ -0,0 +1,64 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.aries.blueprint.web;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.ServletException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Set;
+import org.apache.aries.blueprint.container.BlueprintContainerImpl;
+
+/**
+ * Initialises all the blueprint XML files called <code>META-INF/blueprint.xml</code> on the classpath
+ */
+public class BlueprintContextListener implements ServletContextListener {
+    public static final String CONTAINER_ATTRIBUTE = "org.apache.aries.blueprint.container";
+
+    public void contextInitialized(ServletContextEvent event) {
+        ServletContext servletContext = event.getServletContext();
+        String blueprintPath = "META-INF/blueprint.xml";
+        Set<URL> resourcePathSet = servletContext.getResourcePaths(blueprintPath);
+        List<URL> resourcePaths = new ArrayList<URL>(resourcePathSet);
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        try {
+            Enumeration<URL> resources = classLoader.getResources(blueprintPath);
+            while (resources.hasMoreElements()) {
+                resourcePaths.add(resources.nextElement());
+            }
+            servletContext.log("Loading Blueprint contexts " + resourcePaths);
+            BlueprintContainerImpl container = new BlueprintContainerImpl(classLoader, resourcePaths);
+            servletContext.setAttribute(CONTAINER_ATTRIBUTE, container);
+        } catch (Exception e) {
+            servletContext.log("Failed to startup blueprint container. " + e, e);
+        }
+    }
+
+    public void contextDestroyed(ServletContextEvent event) {
+        ServletContext servletContext = event.getServletContext();
+        Object container = servletContext.getAttribute(CONTAINER_ATTRIBUTE);
+        if (container instanceof BlueprintContainerImpl) {
+            BlueprintContainerImpl blueprint = (BlueprintContainerImpl) container;
+            blueprint.destroy();
+        }
+    }
+}
