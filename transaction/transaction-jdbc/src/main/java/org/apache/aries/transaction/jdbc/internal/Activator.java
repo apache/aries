5fb4442b79806cfe5d3613cdc5e7c2a0fac26424
Guillaume Nodet <gnodet@apache.org>
2013-05-15 15:08:12 +0000

    [ARIES-1070] Provide a correct JDBC wrapping for XA data sources
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk@1482888 13f79535-47bb-0310-9956-ffa450edef68

--- transaction/transaction-jdbc/src/main/java/org/apache/aries/transaction/jdbc/internal/Activator.java
+++ transaction/transaction-jdbc/src/main/java/org/apache/aries/transaction/jdbc/internal/Activator.java
@@ -18,14 +18,11 @@
  */
 package org.apache.aries.transaction.jdbc.internal;
 
-import java.util.Hashtable;
-import javax.sql.DataSource;
-import javax.sql.XADataSource;
-import javax.transaction.TransactionManager;
-
+import org.apache.aries.blueprint.NamespaceHandler;
+import org.apache.aries.transaction.AriesTransactionManager;
+import org.apache.xbean.blueprint.context.impl.XBeanNamespaceHandler;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.Constants;
 import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.framework.ServiceEvent;
 import org.osgi.framework.ServiceListener;
@@ -33,118 +30,131 @@ import org.osgi.framework.ServiceReference;
 import org.osgi.framework.ServiceRegistration;
 import org.osgi.util.tracker.ServiceTracker;
 import org.osgi.util.tracker.ServiceTrackerCustomizer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class Activator implements BundleActivator, ServiceTrackerCustomizer, ServiceListener
-{
-  private TransactionManager tm;
-  private ServiceTracker t;
-  private ServiceReference ref;
-  private BundleContext context;
-  
-  public void start(BundleContext ctx)
-  {
-    context = ctx;
-    
-    t = new ServiceTracker(ctx, javax.sql.XADataSource.class.getName(), this);
-    
-    try {
-      ctx.addServiceListener(this, "(objectClass=javax.transaction.TransactionManager)");
-    } catch (InvalidSyntaxException e) {
+import javax.transaction.TransactionManager;
+import java.util.Hashtable;
+
+public class Activator implements BundleActivator, ServiceTrackerCustomizer, ServiceListener {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Activator.class);
+
+    private AriesTransactionManager tm;
+    private ServiceTracker t;
+    private ServiceReference ref;
+    private BundleContext context;
+    private ServiceRegistration nshReg;
+
+    public void start(BundleContext ctx) {
+        context = ctx;
+
+        // Expose blueprint namespace handler if xbean is present
+        try {
+            nshReg = JdbcNamespaceHandler.register(ctx);
+        } catch (NoClassDefFoundError e) {
+            LOGGER.warn("Unable to register JDBC blueprint namespace handler (xbean-blueprint not available).");
+        } catch (Exception e) {
+            LOGGER.error("Unable to register JDBC blueprint namespace handler", e);
+        }
+
+        t = new ServiceTracker(ctx, javax.sql.XADataSource.class.getName(), this);
+
+        try {
+            ctx.addServiceListener(this, "(objectClass=" + AriesTransactionManager.class.getName() + ")");
+        } catch (InvalidSyntaxException e) {
+        }
+        ref = ctx.getServiceReference(TransactionManager.class.getName());
+        if (ref != null) {
+            tm = (AriesTransactionManager) ctx.getService(ref);
+        }
+
+        if (tm != null) {
+            t.open();
+        }
     }
-    ref = ctx.getServiceReference(TransactionManager.class.getName());
-    if (ref != null) {
-      tm = (TransactionManager) ctx.getService(ref);
+
+    public void stop(BundleContext ctx) {
+        // it is possible these are not cleaned by serviceChanged method when the
+        // tm service is still active
+        if (t != null) {
+            t.close();
+        }
+        if (ref != null) {
+            context.ungetService(ref);
+        }
+        if (nshReg != null) {
+            nshReg.unregister();
+        }
     }
-    
-    if (tm != null) {
-      t.open();
+
+    public Object addingService(ServiceReference ref) {
+        try {
+            LOGGER.info("Wrapping XADataSource " + ref);
+            ManagedDataSourceFactory mdsf = new ManagedDataSourceFactory(ref, tm);
+            return mdsf.register();
+        } catch (Exception e) {
+            LOGGER.warn("Error wrapping XADataSource " + ref, e);
+            return null;
+        }
     }
-  }
 
-  public void stop(BundleContext ctx)
-  {
-      // it is possible these are not cleaned by serviceChanged method when the
-      // tm service is still active
-      if (t != null) {
-          t.close();
-      }
-      if (ref != null) {
-          context.ungetService(ref);
-      }
-  }
+    public void modifiedService(ServiceReference ref, Object service) {
+        ServiceRegistration reg = (ServiceRegistration) service;
 
-  public Object addingService(ServiceReference ref)
-  {
-    BundleContext ctx = ref.getBundle().getBundleContext();
+        Hashtable<String, Object> map = new Hashtable<String, Object>();
+        for (String key : ref.getPropertyKeys()) {
+            map.put(key, ref.getProperty(key));
+        }
+        map.put("aries.xa.aware", "true");
 
-    Hashtable<String, Object> map = new Hashtable<String, Object>();
-    for (String key : ref.getPropertyKeys()) {
-      map.put(key, ref.getProperty(key));
+        reg.setProperties(map);
     }
-    map.put("aries.xa.aware", "true");
-    
-    // make the ranking for our new better wrappered data source higher so
-    // it is the default object looked up using osgi.service.jndi.name.
-    Object rankingProp = map.get(Constants.SERVICE_RANKING);
-    
-    int ranking = 1000;
-    
-    if (rankingProp != null) ranking = ((Integer)rankingProp) + 1000;
-    
-    map.put(Constants.SERVICE_RANKING, ranking);
 
-    XADatasourceEnlistingWrapper wrapper = new XADatasourceEnlistingWrapper();
-    wrapper.setTransactionManager(tm);
-    wrapper.setDataSource((XADataSource) ctx.getService(ref));
-
-    ServiceRegistration reg = ctx.registerService(DataSource.class.getName(), wrapper, map); 
-
-    return reg;
-  }
- 
-  public void modifiedService(ServiceReference ref, Object service)
-  {
-    ServiceRegistration reg = (ServiceRegistration) service;
-    
-    Hashtable<String, Object> map = new Hashtable<String, Object>();
-    for (String key : ref.getPropertyKeys()) {
-      map.put(key, ref.getProperty(key));
+    public void removedService(ServiceReference ref, Object service) {
+        safeUnregisterService((ServiceRegistration) service);
     }
-    map.put("aries.xa.aware", "true");
 
-    reg.setProperties(map);
-  }
+    public void serviceChanged(ServiceEvent event) {
+        if (event.getType() == ServiceEvent.REGISTERED && tm == null) {
+            ref = event.getServiceReference();
+            tm = (AriesTransactionManager) context.getService(ref);
 
-  public void removedService(ServiceReference ref, Object service)
-  {
-    safeUnregisterService((ServiceRegistration)service);
-  }
-
-  public void serviceChanged(ServiceEvent event)
-  {
-    if (event.getType() == ServiceEvent.REGISTERED && tm == null) {
-      ref = event.getServiceReference();
-      tm = (TransactionManager) context.getService(ref);
-      
-      if (tm == null) ref = null;
-      else t.open();
-    } else if (event.getType() == ServiceEvent.UNREGISTERING && tm != null &&
-        ref.getProperty("service.id").equals(event.getServiceReference().getProperty("service.id"))) {
-      t.close();
-      context.ungetService(ref);
-      ref = null;
-      tm = null;
+            if (tm == null) ref = null;
+            else t.open();
+        } else if (event.getType() == ServiceEvent.UNREGISTERING && tm != null &&
+                ref.getProperty("service.id").equals(event.getServiceReference().getProperty("service.id"))) {
+            t.close();
+            context.ungetService(ref);
+            ref = null;
+            tm = null;
+        }
     }
-  }
 
-  static void safeUnregisterService(ServiceRegistration reg)
-  {
-    if(reg != null) {
-      try {
-        reg.unregister();
-      } catch (IllegalStateException e) {
-        //This can be safely ignored
-      }
+    static void safeUnregisterService(ServiceRegistration reg) {
+        if (reg != null) {
+            try {
+                reg.unregister();
+            } catch (IllegalStateException e) {
+                //This can be safely ignored
+            }
+        }
     }
-  }
+
+    static class JdbcNamespaceHandler {
+
+        public static ServiceRegistration register(BundleContext context) throws Exception {
+            XBeanNamespaceHandler nsh = new XBeanNamespaceHandler(
+                    "http://aries.apache.org/xmlns/transaction-jdbc/2.0",
+                    "org.apache.aries.transaction.jdbc.xsd",
+                    context.getBundle(),
+                    "META-INF/services/org/apache/xbean/spring/http/aries.apache.org/xmlns/transaction-jdbc/2.0"
+            );
+            Hashtable<String, Object> props = new Hashtable<String, Object>();
+            props.put("osgi.service.blueprint.namespace", "http://aries.apache.org/xmlns/transaction-jdbc/2.0");
+            return context.registerService(NamespaceHandler.class.getName(), nsh, props);
+        }
+
+    }
+
 }
---
+++ transaction/transaction-jdbc/src/main/java/org/apache/aries/transaction/jdbc/internal/ConnectionManagerFactory.java
@@ -0,0 +1,267 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.aries.transaction.jdbc.internal;
+
+import org.apache.aries.transaction.AriesTransactionManager;
+import org.apache.geronimo.connector.outbound.GenericConnectionManager;
+import org.apache.geronimo.connector.outbound.SubjectSource;
+import org.apache.geronimo.connector.outbound.connectionmanagerconfig.LocalTransactions;
+import org.apache.geronimo.connector.outbound.connectionmanagerconfig.NoPool;
+import org.apache.geronimo.connector.outbound.connectionmanagerconfig.NoTransactions;
+import org.apache.geronimo.connector.outbound.connectionmanagerconfig.PartitionedPool;
+import org.apache.geronimo.connector.outbound.connectionmanagerconfig.PoolingSupport;
+import org.apache.geronimo.connector.outbound.connectionmanagerconfig.SinglePool;
+import org.apache.geronimo.connector.outbound.connectionmanagerconfig.TransactionSupport;
+import org.apache.geronimo.connector.outbound.connectionmanagerconfig.XATransactions;
+import org.apache.geronimo.connector.outbound.connectiontracking.ConnectionTrackingCoordinator;
+import org.apache.geronimo.connector.outbound.connectiontracking.GeronimoTransactionListener;
+import org.apache.geronimo.transaction.manager.TransactionManagerMonitor;
+
+import javax.resource.spi.ConnectionManager;
+import javax.resource.spi.ManagedConnectionFactory;
+
+public class ConnectionManagerFactory {
+
+    private AriesTransactionManager transactionManager;
+    private ManagedConnectionFactory managedConnectionFactory;
+
+    private TransactionSupport transactionSupport;
+    private String transaction;
+
+    private PoolingSupport poolingSupport;
+    private boolean pooling = true;
+    private String partitionStrategy; //: none, by-subject, by-connector-properties
+    private int poolMaxSize = 10;
+    private int poolMinSize = 0;
+    private boolean allConnectionsEqual = true;
+    private int connectionMaxWaitMilliseconds = 5000;
+    private int connectionMaxIdleMinutes = 15;
+
+    private SubjectSource subjectSource;
+
+    private ConnectionTrackingCoordinator connectionTracker;
+    private TransactionManagerMonitor transactionManagerMonitor;
+    private GenericConnectionManager connectionManager;
+
+    public ConnectionManager getConnectionManager() {
+        return connectionManager;
+    }
+
+    public void init() throws Exception {
+        if (transactionManager == null) {
+            throw new IllegalArgumentException("transactionManager must be set");
+        }
+        if (managedConnectionFactory == null) {
+            throw new IllegalArgumentException("managedConnectionFactory must be set");
+        }
+        // Apply the default value for property if necessary
+        if (transactionSupport == null) {
+            // No transaction
+            if (transaction == null || "local".equalsIgnoreCase(transaction)) {
+                transactionSupport = LocalTransactions.INSTANCE;
+            } else if ("none".equalsIgnoreCase(transaction)) {
+                transactionSupport = NoTransactions.INSTANCE;
+            } else if ("xa".equalsIgnoreCase(transaction)) {
+                transactionSupport = new XATransactions(true, false);
+            } else {
+                throw new IllegalArgumentException("Unknown transaction type " + transaction + " (must be local, none or xa)");
+            }
+        }
+        if (poolingSupport == null) {
+            // No pool
+            if (!pooling) {
+                poolingSupport = new NoPool();
+            } else {
+                if (partitionStrategy == null || "none".equalsIgnoreCase(partitionStrategy)) {
+
+                    // unpartitioned pool
+                    poolingSupport = new SinglePool(poolMaxSize,
+                            poolMinSize,
+                            connectionMaxWaitMilliseconds,
+                            connectionMaxIdleMinutes,
+                            allConnectionsEqual,
+                            !allConnectionsEqual,
+                            false);
+
+                } else if ("by-connector-properties".equalsIgnoreCase(partitionStrategy)) {
+
+                    // partition by connector properties such as username and password on a jdbc connection
+                    poolingSupport = new PartitionedPool(poolMaxSize,
+                            poolMinSize,
+                            connectionMaxWaitMilliseconds,
+                            connectionMaxIdleMinutes,
+                            allConnectionsEqual,
+                            !allConnectionsEqual,
+                            false,
+                            true,
+                            false);
+                } else if ("by-subject".equalsIgnoreCase(partitionStrategy)) {
+
+                    // partition by caller subject
+                    poolingSupport = new PartitionedPool(poolMaxSize,
+                            poolMinSize,
+                            connectionMaxWaitMilliseconds,
+                            connectionMaxIdleMinutes,
+                            allConnectionsEqual,
+                            !allConnectionsEqual,
+                            false,
+                            false,
+                            true);
+                } else {
+                    throw new IllegalArgumentException("Unknown partition strategy " + partitionStrategy + " (must be none, by-connector-properties or by-subject)");
+                }
+            }
+        }
+        if (connectionTracker == null) {
+            connectionTracker = new ConnectionTrackingCoordinator();
+        }
+        if (transactionManagerMonitor == null) {
+            transactionManagerMonitor = new GeronimoTransactionListener(connectionTracker);
+            transactionManager.addTransactionAssociationListener(transactionManagerMonitor);
+        }
+        if (connectionManager == null) {
+            // Instantiate the Geronimo Connection Manager
+            connectionManager = new GenericConnectionManager(
+                    transactionSupport,
+                    poolingSupport,
+                    subjectSource,
+                    connectionTracker,
+                    transactionManager,
+                    managedConnectionFactory,
+                    getClass().getName(),
+                    getClass().getClassLoader());
+
+            connectionManager.doStart();
+        }
+    }
+
+    public void destroy() throws Exception {
+        if (connectionManager != null) {
+            connectionManager.doStop();
+            connectionManager = null;
+        }
+        if (transactionManagerMonitor != null && transactionManager != null) {
+            transactionManager.removeTransactionAssociationListener(transactionManagerMonitor);
+        }
+    }
+
+    public AriesTransactionManager getTransactionManager() {
+        return transactionManager;
+    }
+
+    public void setTransactionManager(AriesTransactionManager transactionManager) {
+        this.transactionManager = transactionManager;
+    }
+
+    public ManagedConnectionFactory getManagedConnectionFactory() {
+        return managedConnectionFactory;
+    }
+
+    public void setManagedConnectionFactory(ManagedConnectionFactory managedConnectionFactory) {
+        this.managedConnectionFactory = managedConnectionFactory;
+    }
+
+    public TransactionSupport getTransactionSupport() {
+        return transactionSupport;
+    }
+
+    public void setTransactionSupport(TransactionSupport transactionSupport) {
+        this.transactionSupport = transactionSupport;
+    }
+
+    public String getTransaction() {
+        return transaction;
+    }
+
+    public void setTransaction(String transaction) {
+        this.transaction = transaction;
+    }
+
+    public PoolingSupport getPoolingSupport() {
+        return poolingSupport;
+    }
+
+    public void setPoolingSupport(PoolingSupport poolingSupport) {
+        this.poolingSupport = poolingSupport;
+    }
+
+    public boolean isPooling() {
+        return pooling;
+    }
+
+    public void setPooling(boolean pooling) {
+        this.pooling = pooling;
+    }
+
+    public String getPartitionStrategy() {
+        return partitionStrategy;
+    }
+
+    public void setPartitionStrategy(String partitionStrategy) {
+        this.partitionStrategy = partitionStrategy;
+    }
+
+    public int getPoolMaxSize() {
+        return poolMaxSize;
+    }
+
+    public void setPoolMaxSize(int poolMaxSize) {
+        this.poolMaxSize = poolMaxSize;
+    }
+
+    public int getPoolMinSize() {
+        return poolMinSize;
+    }
+
+    public void setPoolMinSize(int poolMinSize) {
+        this.poolMinSize = poolMinSize;
+    }
+
+    public boolean isAllConnectionsEqual() {
+        return allConnectionsEqual;
+    }
+
+    public void setAllConnectionsEqual(boolean allConnectionsEqual) {
+        this.allConnectionsEqual = allConnectionsEqual;
+    }
+
+    public int getConnectionMaxWaitMilliseconds() {
+        return connectionMaxWaitMilliseconds;
+    }
+
+    public void setConnectionMaxWaitMilliseconds(int connectionMaxWaitMilliseconds) {
+        this.connectionMaxWaitMilliseconds = connectionMaxWaitMilliseconds;
+    }
+
+    public int getConnectionMaxIdleMinutes() {
+        return connectionMaxIdleMinutes;
+    }
+
+    public void setConnectionMaxIdleMinutes(int connectionMaxIdleMinutes) {
+        this.connectionMaxIdleMinutes = connectionMaxIdleMinutes;
+    }
+
+    public SubjectSource getSubjectSource() {
+        return subjectSource;
+    }
+
+    public void setSubjectSource(SubjectSource subjectSource) {
+        this.subjectSource = subjectSource;
+    }
+}
