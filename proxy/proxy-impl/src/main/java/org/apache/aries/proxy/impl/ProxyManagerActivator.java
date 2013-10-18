c7411e8631c2a3e66c0d49fad9f8b5a287c5ac81
Daniel Kulp <dkulp@apache.org>
2013-10-18 19:30:39 +0000

    [ARIES-1126] Prevent an NPE in ProxyWeavingHook and allow a setting to bypass it completely
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk@1533600 13f79535-47bb-0310-9956-ffa450edef68

--- proxy/proxy-impl/src/main/java/org/apache/aries/proxy/impl/ProxyManagerActivator.java
+++ proxy/proxy-impl/src/main/java/org/apache/aries/proxy/impl/ProxyManagerActivator.java
@@ -18,8 +18,6 @@
  */
 package org.apache.aries.proxy.impl;
 
-import java.util.Dictionary;
-import java.util.Hashtable;
 
 import org.apache.aries.proxy.ProxyManager;
 import org.osgi.framework.BundleActivator;
@@ -55,14 +53,7 @@ public class ProxyManagerActivator implements BundleActivator
         //if ASM is available then we should also try weaving
         Class<?> cls = Class.forName("org.apache.aries.proxy.impl.weaving.ProxyWeavingHook",
         		true, ProxyManagerActivator.class.getClassLoader());
-        Dictionary<String,String> props = new Hashtable<String,String>();
-        // SubsystemResource.java also uses this constant. 
-        //   While it could be turned into a static final constant, note that this
-        //   is also a non-standard workaround in the absence of a solution in the spec. 
-        // See the associated OSGi spec bug. 
-        props.put("osgi.woven.packages", "org.apache.aries.proxy.weaving,org.apache.aries.proxy");
-        context.registerService("org.osgi.framework.hooks.weaving.WeavingHook",
-            cls.getConstructor(BundleContext.class).newInstance(context), props);
+        cls.getConstructor(BundleContext.class).newInstance(context);
       } catch (Throwable t) {
         //We don't care about this, we just won't have registered the hook
       }
