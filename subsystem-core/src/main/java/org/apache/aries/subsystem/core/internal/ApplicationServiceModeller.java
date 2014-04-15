227b2a6fdc484cf3acc868f3c057b2ede194688b
Guillaume Nodet <gnodet@apache.org>
2014-04-15 11:04:40 +0000

    [ARIES-1172] Make subsystems independent of application modeller
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk/subsystem@1587513 13f79535-47bb-0310-9956-ffa450edef68

---
+++ subsystem-core/src/main/java/org/apache/aries/subsystem/core/internal/ApplicationServiceModeller.java
@@ -0,0 +1,95 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.aries.subsystem.core.internal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.aries.application.modelling.ExportedService;
+import org.apache.aries.application.modelling.ImportedService;
+import org.apache.aries.application.modelling.ModelledResourceManager;
+import org.apache.aries.application.modelling.ModellerException;
+import org.apache.aries.application.modelling.ParsedServiceElements;
+import org.apache.aries.util.filesystem.IDirectory;
+import org.osgi.namespace.service.ServiceNamespace;
+import org.osgi.resource.Capability;
+import org.osgi.resource.Namespace;
+import org.osgi.resource.Requirement;
+import org.osgi.resource.Resource;
+import org.osgi.service.subsystem.SubsystemException;
+
+public class ApplicationServiceModeller implements ServiceModeller {
+
+    private final ModelledResourceManager manager;
+
+    public ApplicationServiceModeller(Object manager) {
+        this.manager = (ModelledResourceManager) manager;
+    }
+
+    @Override
+    public ServiceModel computeRequirementsAndCapabilities(Resource resource, IDirectory directory) throws SubsystemException {
+        try {
+            ServiceModelImpl model = new ServiceModelImpl();
+            ParsedServiceElements elements = manager.getServiceElements(directory);
+            for (ExportedService service : elements.getServices()) {
+                model.capabilities.add(new BasicCapability.Builder()
+                        .namespace(ServiceNamespace.SERVICE_NAMESPACE)
+                        .attribute(ServiceNamespace.CAPABILITY_OBJECTCLASS_ATTRIBUTE, new ArrayList<String>(service.getInterfaces()))
+                        .attributes(service.getServiceProperties())
+                        .resource(resource)
+                        .build());
+            }
+            for (ImportedService service : elements.getReferences()) {
+                StringBuilder builder = new StringBuilder("(&(")
+                        .append(ServiceNamespace.CAPABILITY_OBJECTCLASS_ATTRIBUTE)
+                        .append('=')
+                        .append(service.getInterface())
+                        .append(')');
+                String filter = service.getFilter();
+                if (filter != null)
+                    builder.append(filter);
+                builder.append(')');
+                model.requirements.add(new BasicRequirement.Builder()
+                        .namespace(ServiceNamespace.SERVICE_NAMESPACE)
+                        .directive(Namespace.REQUIREMENT_FILTER_DIRECTIVE, builder.toString())
+                        .directive(
+                                Namespace.REQUIREMENT_RESOLUTION_DIRECTIVE,
+                                service.isOptional() ? Namespace.RESOLUTION_OPTIONAL : Namespace.RESOLUTION_MANDATORY)
+                        .directive(
+                                Namespace.REQUIREMENT_CARDINALITY_DIRECTIVE,
+                                service.isMultiple() ? Namespace.CARDINALITY_MULTIPLE : Namespace.CARDINALITY_SINGLE)
+                        .resource(resource)
+                        .build());
+            }
+            return model;
+        } catch (ModellerException e) {
+            throw new SubsystemException(e);
+        }
+    }
+
+    static class ServiceModelImpl implements ServiceModel {
+        final List<Requirement> requirements = new ArrayList<Requirement>();
+        final List<Capability> capabilities = new ArrayList<Capability>();
+        @Override
+        public List<Requirement> getServiceRequirements() {
+            return requirements;
+        }
+
+        @Override
+        public List<Capability> getServiceCapabilities() {
+            return capabilities;
+        }
+    }
+
+}
