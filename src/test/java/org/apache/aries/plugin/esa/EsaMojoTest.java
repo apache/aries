100c61eabb67205ec7b813a7d4be152cc321448f
Jarek Gawor <gawor@apache.org>
2014-04-23 19:42:51 +0000

    ARIES-1175: Support for adding custom headers
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk/esa-maven-plugin@1589501 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/aries/plugin/esa/EsaMojoTest.java
+++ src/test/java/org/apache/aries/plugin/esa/EsaMojoTest.java
@@ -452,6 +452,54 @@ public class EsaMojoTest
 
     }
 
+    public void testCustomInstructions()
+        throws Exception
+    {
+        File testPom = new File( getBasedir(),
+                                 "target/test-classes/unit/basic-esa-custom-instructions/plugin-config.xml" );
+
+        EsaMojo mojo = ( EsaMojo ) lookupMojo( "esa", testPom );
+
+        assertNotNull( mojo );
+
+        String finalName = ( String ) getVariableValueFromObject( mojo, "finalName" );
+
+        String workDir = ( String ) getVariableValueFromObject( mojo, "workDirectory" );
+
+        String outputDir = ( String ) getVariableValueFromObject( mojo, "outputDirectory" );
+
+        mojo.execute();
+
+
+        //check the generated esa file
+        File esaFile = new File( outputDir, finalName + ".esa" );
+
+        assertTrue( esaFile.exists() );
+
+        //expected files/directories inside the esa file
+        List expectedFiles = new ArrayList();
+
+        expectedFiles.add( "OSGI-INF/SUBSYSTEM.MF" );
+        expectedFiles.add( "OSGI-INF/" );
+        expectedFiles.add( "maven-artifact01-1.0-SNAPSHOT.jar" );
+        expectedFiles.add( "maven-artifact02-1.0-SNAPSHOT.jar" );
+
+        ZipFile esa = new ZipFile( esaFile );
+        
+        Enumeration entries = esa.getEntries();
+
+        assertTrue( entries.hasMoreElements() );
+
+        int missing = getSizeOfExpectedFiles(entries, expectedFiles);
+        assertEquals("Missing files: " + expectedFiles,  0, missing);
+
+        // Test for the Foo header
+        testForHeader(esa, "Foo", "Foo: bar");
+
+        // Test for the MyHeader header
+        testForHeader(esa, "MyHeader", "MyHeader: myValue");
+    }
+
     private int getSizeOfExpectedFiles( Enumeration entries, List expectedFiles )
     {
         while( entries.hasMoreElements() )
