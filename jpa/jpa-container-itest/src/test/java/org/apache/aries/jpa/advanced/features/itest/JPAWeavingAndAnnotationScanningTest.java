ece43addee2a4a860151dc6709eecfbf3727d5a7
Christian Schneider <cschneider@apache.org>
2014-06-12 08:13:07 +0000

    ARIES-1207 Upgrade jpa tests to pax exam 3 and refactorings
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk@1602089 13f79535-47bb-0310-9956-ffa450edef68

--- jpa/jpa-container-itest/src/test/java/org/apache/aries/jpa/advanced/features/itest/JPAWeavingAndAnnotationScanningTest.java
+++ jpa/jpa-container-itest/src/test/java/org/apache/aries/jpa/advanced/features/itest/JPAWeavingAndAnnotationScanningTest.java
@@ -16,71 +16,32 @@
 package org.apache.aries.jpa.advanced.features.itest;
 
 import static org.junit.Assert.assertEquals;
-import static org.ops4j.pax.exam.CoreOptions.equinox;
-import static org.apache.aries.itest.ExtraOptions.*;
-import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.vmOption;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
-import org.apache.aries.itest.AbstractIntegrationTest;
-import org.apache.aries.jpa.container.PersistenceUnitConstants;
 import org.apache.aries.jpa.container.advanced.itest.bundle.entities.Car;
+import org.apache.aries.jpa.itest.AbstractJPAItest;
 import org.junit.Test;
-import org.ops4j.pax.exam.Option;
-import org.ops4j.pax.exam.container.def.PaxRunnerOptions;
 
-public abstract class JPAWeavingAndAnnotationScanningTest extends AbstractIntegrationTest {
+public abstract class JPAWeavingAndAnnotationScanningTest extends AbstractJPAItest {
 
-  @Test
-  public void testAnnotatedClassFound() throws Exception {
-    EntityManagerFactory emf = context().getService(EntityManagerFactory.class, "(&(osgi.unit.name=test-unit)(" + PersistenceUnitConstants.CONTAINER_MANAGED_PERSISTENCE_UNIT + "=true))");
-    
-    EntityManager em = emf.createEntityManager();
-    
-    em.getTransaction().begin();
-    
-    Car c = new Car();
-    
-    c.setColour("Blue");
-    c.setNumberPlate("AB11CDE");
-    c.setNumberOfSeats(7);
-    c.setEngineSize(1900);
-    
-    em.persist(c);
-    
-    em.getTransaction().commit();
-    
-    assertEquals(7, em.find(Car.class, "AB11CDE").getNumberOfSeats());
-  }
-    
-  @org.ops4j.pax.exam.junit.Configuration
-  public static Option[] configuration() {
-    return testOptions(
-        transactionBootDelegation(),
-        paxLogging("INFO"),
-        
-        // Bundles
-        mavenBundle("commons-lang", "commons-lang"),
-        mavenBundle("commons-collections", "commons-collections"),
-        mavenBundle("commons-pool", "commons-pool"),
-        mavenBundle("org.apache.aries", "org.apache.aries.util"),
-        mavenBundle("org.ow2.asm", "asm-all"),
-        mavenBundle("org.apache.aries.proxy", "org.apache.aries.proxy.api"),
-        mavenBundle("org.apache.aries.proxy", "org.apache.aries.proxy.impl"),
-        mavenBundle("org.apache.aries.jpa", "org.apache.aries.jpa.api"),
-        mavenBundle("org.apache.aries.jpa", "org.apache.aries.jpa.container"),
-        mavenBundle("org.apache.derby", "derby"),
-        mavenBundle("org.apache.geronimo.specs", "geronimo-jta_1.1_spec"),
-        mavenBundle("org.apache.geronimo.specs", "geronimo-jpa_2.0_spec"),
+	@Test
+	public void testAnnotatedClassFound() throws Exception {
+		EntityManagerFactory emf = getEMF(TEST_UNIT);
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
 
-        mavenBundle("org.osgi", "org.osgi.compendium"),
+		Car c = new Car();
+		c.setColour("Blue");
+		c.setNumberPlate("AB11CDE");
+		c.setNumberOfSeats(7);
+		c.setEngineSize(1900);
+		em.persist(c);
 
-//        vmOption ("-Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5006"),
-        
-        mavenBundle("org.apache.aries.jpa", "org.apache.aries.jpa.container.advanced.itest.bundle"),
-        
-        PaxRunnerOptions.rawPaxRunnerOption("config", "classpath:ss-runner.properties"),
-        equinox().version("3.7.0.v20110613"));
-  }
+		em.getTransaction().commit();
+
+		assertEquals(7, em.find(Car.class, "AB11CDE").getNumberOfSeats());
+	}
+
 }
