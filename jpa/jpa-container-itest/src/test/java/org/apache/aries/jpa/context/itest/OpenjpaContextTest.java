bddb54bc29629908207a2200a9f86e6222367589
Christian Schneider <cschneider@apache.org>
2014-06-12 08:13:07 +0000

    ARIES-1207 Upgrade jpa tests to pax exam 3 and refactorings
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk@1602089 13f79535-47bb-0310-9956-ffa450edef68

--- jpa/jpa-container-itest/src/test/java/org/apache/aries/jpa/context/itest/OpenjpaContextTest.java
+++ jpa/jpa-container-itest/src/test/java/org/apache/aries/jpa/context/itest/OpenjpaContextTest.java
@@ -16,21 +16,21 @@
 package org.apache.aries.jpa.context.itest;
 
 import static org.ops4j.pax.exam.CoreOptions.options;
-import static org.apache.aries.itest.ExtraOptions.*;
 
-import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.Option;
-import org.ops4j.pax.exam.junit.Configuration;
-import org.ops4j.pax.exam.junit.JUnit4TestRunner;
 
-@RunWith(JUnit4TestRunner.class)
 public class OpenjpaContextTest extends JPAContextTest {
+
     @Configuration
-    public static Option[] openjpaConfig() {
-        return options(        
-                mavenBundle("org.apache.servicemix.bundles", "org.apache.servicemix.bundles.serp"),
-                mavenBundle("org.apache.openjpa", "openjpa"),
-                mavenBundle("org.apache.aries.jpa", "org.apache.aries.jpa.container.itest.bundle")
+    public Option[] configuration() {
+        return options(
+        		baseOptions(),
+        		ariesJpa(),
+        		transactionWrapper(),
+        		openJpa(),
+                testBundle()
         );
     }
+
 }
