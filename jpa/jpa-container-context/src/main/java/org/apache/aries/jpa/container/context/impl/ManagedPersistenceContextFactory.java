a5133a16e002f415fed46036c1dfec34434bae7d
Jean-Baptiste Onofre <jbonofre@apache.org>
2014-07-15 14:47:01 +0000

    [ARIES-1047] Add JPA 2.1 support
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk@1610718 13f79535-47bb-0310-9956-ffa450edef68

--- jpa/jpa-container-context/src/main/java/org/apache/aries/jpa/container/context/impl/ManagedPersistenceContextFactory.java
+++ jpa/jpa-container-context/src/main/java/org/apache/aries/jpa/container/context/impl/ManagedPersistenceContextFactory.java
@@ -26,11 +26,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
 
-import javax.persistence.Cache;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.PersistenceContextType;
-import javax.persistence.PersistenceUnitUtil;
+import javax.persistence.*;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.metamodel.Metamodel;
 
@@ -42,111 +38,135 @@ import org.apache.aries.jpa.container.context.transaction.impl.JTAPersistenceCon
 import org.osgi.framework.ServiceReference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 /**
  * A factory that can lazily create managed persistence contexts.
  * This is registered in the Service registry to be looked up by blueprint.
  * The EntityManagerFactory interface is used to ensure a shared class space
  * with the client. Only the createEntityManager() method is supported.
- * 
+ * <p/>
  * Also this class receives a callback on cleanup
  */
 public class ManagedPersistenceContextFactory implements EntityManagerFactory, DestroyCallback {
-  /** Logger */
-  private static final Logger _logger = LoggerFactory.getLogger("org.apache.aries.jpa.container.context");
-  
-  private final ServiceReference emf;
-  private final Map<String, Object> properties;
-  private final JTAPersistenceContextRegistry registry;
-  private final PersistenceContextType type;
-  private final AtomicLong activeCount = new AtomicLong(0);
-  private final String unitName;
-  
-  private final AtomicReference<QuiesceTidyUp> tidyUp = new AtomicReference<QuiesceTidyUp>();
-  
-  public ManagedPersistenceContextFactory(String name, ServiceReference unit,
-      Map<String, Object> props, JTAPersistenceContextRegistry contextRegistry) {
-      unitName = name;
-      emf = unit;
-      //Take a copy of the Map so that we don't modify the original
-      properties = new HashMap<String, Object>(props);
-      registry = contextRegistry;
-      //Remove our internal property so that it doesn't get passed on the createEntityManager call
-      type = (PersistenceContextType) properties.remove(PersistenceContextProvider.PERSISTENCE_CONTEXT_TYPE);
-  }
+    /**
+     * Logger
+     */
+    private static final Logger _logger = LoggerFactory.getLogger("org.apache.aries.jpa.container.context");
 
-  public EntityManager createEntityManager() {
-    if(_logger.isDebugEnabled()) {
-      _logger.debug("Creating a container managed entity manager for the perstence unit {} with the following properties {}",
-          new Object[] {emf, properties});
-    }
-    //Getting the BundleContext is a privileged operation that the 
-    //client might not be able to do.
-    EntityManagerFactory factory = AccessController.doPrivileged(
-        new PrivilegedAction<EntityManagerFactory>() {
-          public EntityManagerFactory run() {
-            return (EntityManagerFactory) emf.getBundle().getBundleContext().getService(emf);
-          }
-        });
-    
-    if(type == PersistenceContextType.TRANSACTION || type == null)
-      return new JTAEntityManager(factory, properties, registry, activeCount, this);
-    else {
-      _logger.error(NLS.MESSAGES.getMessage("extended.em.not.supported"));
-      return null;
+    private final ServiceReference emf;
+    private final Map<String, Object> properties;
+    private final JTAPersistenceContextRegistry registry;
+    private final PersistenceContextType type;
+    private final AtomicLong activeCount = new AtomicLong(0);
+    private final String unitName;
+
+    private final AtomicReference<QuiesceTidyUp> tidyUp = new AtomicReference<QuiesceTidyUp>();
+
+    public ManagedPersistenceContextFactory(String name, ServiceReference unit,
+                                            Map<String, Object> props, JTAPersistenceContextRegistry contextRegistry) {
+        unitName = name;
+        emf = unit;
+        //Take a copy of the Map so that we don't modify the original
+        properties = new HashMap<String, Object>(props);
+        registry = contextRegistry;
+        //Remove our internal property so that it doesn't get passed on the createEntityManager call
+        type = (PersistenceContextType) properties.remove(PersistenceContextProvider.PERSISTENCE_CONTEXT_TYPE);
     }
 
-  }
+    public EntityManager createEntityManager() {
+        if (_logger.isDebugEnabled()) {
+            _logger.debug("Creating a container managed entity manager for the perstence unit {} with the following properties {}",
+                    new Object[]{emf, properties});
+        }
+        //Getting the BundleContext is a privileged operation that the
+        //client might not be able to do.
+        EntityManagerFactory factory = AccessController.doPrivileged(
+                new PrivilegedAction<EntityManagerFactory>() {
+                    public EntityManagerFactory run() {
+                        return (EntityManagerFactory) emf.getBundle().getBundleContext().getService(emf);
+                    }
+                });
 
-  public void close() {
-    throw new UnsupportedOperationException();
-  }
-  
-  public EntityManager createEntityManager(Map arg0) {
-    throw new UnsupportedOperationException();
-  }
+        if (type == PersistenceContextType.TRANSACTION || type == null)
+            return new JTAEntityManager(factory, properties, registry, activeCount, this);
+        else {
+            _logger.error(NLS.MESSAGES.getMessage("extended.em.not.supported"));
+            return null;
+        }
 
-  public Cache getCache() {
-    throw new UnsupportedOperationException();
-  }
-
-  public CriteriaBuilder getCriteriaBuilder() {
-    throw new UnsupportedOperationException();
-  }
-
-  public Metamodel getMetamodel() {
-    throw new UnsupportedOperationException();
-  }
-
-  public PersistenceUnitUtil getPersistenceUnitUtil() {
-    throw new UnsupportedOperationException();
-  }
-
-  public Map<String, Object> getProperties() {
-    throw new UnsupportedOperationException();
-  }
-
-  public boolean isOpen() {
-    throw new UnsupportedOperationException();
-  }
-
-  /**
-   * Register an async Quiesce operation with this peristence context
-   * @param tidyUp
-   */
-  public void quiesce(QuiesceTidyUp tidyUp) {
-    this.tidyUp.compareAndSet(null, tidyUp);
-    if(activeCount.get() == 0) {
-      tidyUp.unitQuiesced(unitName);
     }
-  }
 
-  /**
-   * Quiesce this unit after the last context is destroyed
-   */
-  public void callback() {
-    if(tidyUp.get() != null && activeCount.get() == 0) {
-      tidyUp.get().unitQuiesced(unitName);
+    public void close() {
+        throw new UnsupportedOperationException();
     }
-  }
-  
+
+    public EntityManager createEntityManager(Map arg0) {
+        throw new UnsupportedOperationException();
+    }
+
+    public Cache getCache() {
+        throw new UnsupportedOperationException();
+    }
+
+    public CriteriaBuilder getCriteriaBuilder() {
+        throw new UnsupportedOperationException();
+    }
+
+    public Metamodel getMetamodel() {
+        throw new UnsupportedOperationException();
+    }
+
+    public PersistenceUnitUtil getPersistenceUnitUtil() {
+        throw new UnsupportedOperationException();
+    }
+
+    public Map<String, Object> getProperties() {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean isOpen() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Register an async Quiesce operation with this peristence context
+     *
+     * @param tidyUp
+     */
+    public void quiesce(QuiesceTidyUp tidyUp) {
+        this.tidyUp.compareAndSet(null, tidyUp);
+        if (activeCount.get() == 0) {
+            tidyUp.unitQuiesced(unitName);
+        }
+    }
+
+    /**
+     * Quiesce this unit after the last context is destroyed
+     */
+    public void callback() {
+        if (tidyUp.get() != null && activeCount.get() == 0) {
+            tidyUp.get().unitQuiesced(unitName);
+        }
+    }
+
+    public <T> void addNamedEntityGraph(String arg0, EntityGraph<T> arg1) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void addNamedQuery(String arg0, Query arg1) {
+        throw new UnsupportedOperationException();
+    }
+
+    public EntityManager createEntityManager(SynchronizationType arg0) {
+        throw new UnsupportedOperationException();
+    }
+
+    public EntityManager createEntityManager(SynchronizationType arg0, Map arg1) {
+        throw new UnsupportedOperationException();
+    }
+
+    public <T> T unwrap(Class<T> arg0) {
+        throw new UnsupportedOperationException();
+    }
+
 }
