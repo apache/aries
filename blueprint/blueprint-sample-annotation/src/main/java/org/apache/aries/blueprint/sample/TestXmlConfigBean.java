beb8062f69b3d12bfc479a6814b30a8194f59346
Guillaume Nodet <gnodet@apache.org>
2015-07-10 13:25:13 +0000

    [ARIES-1324] Allow to use Blueprint annotations and configuration files simultaneously
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk@1690254 13f79535-47bb-0310-9956-ffa450edef68

---
+++ blueprint/blueprint-sample-annotation/src/main/java/org/apache/aries/blueprint/sample/TestXmlConfigBean.java
@@ -0,0 +1,17 @@
+package org.apache.aries.blueprint.sample;
+
+/**
+ * Created by Maxim Becker on 12.04.15.
+ */
+public class TestXmlConfigBean {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
