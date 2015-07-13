33009ada37fe3a8363009059919e831f4e549705
Guillaume Nodet <gnodet@apache.org>
2015-07-13 07:44:53 +0000

    [ARIES-1319] Performance improvements: reduce memory allocation in Collaborator (service invocation flow), replace locking with a AtomicInteger in QuiesceInterceptor/ServiceRecipe logic to reduce threads contention
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk@1690576 13f79535-47bb-0310-9956-ffa450edef68

---
+++ blueprint/blueprint-core/src/main/java/org/apache/aries/blueprint/proxy/SingleInterceptorCollaborator.java
@@ -0,0 +1,119 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.aries.blueprint.proxy;
+
+import java.io.Serializable;
+import java.lang.reflect.Method;
+
+import org.apache.aries.blueprint.Interceptor;
+import org.apache.aries.proxy.InvocationListener;
+import org.osgi.service.blueprint.reflect.ComponentMetadata;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A collaborator which ensures preInvoke and postInvoke occur before and after
+ * method invocation
+ */
+public class SingleInterceptorCollaborator implements InvocationListener, Serializable {
+
+    /** Serial version UID for this class */
+    private static final long serialVersionUID = -58189302118314469L;
+
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(Collaborator.class);
+
+    private transient Interceptor interceptor;
+    private transient ComponentMetadata cm;
+
+    private static final Object NON_INVOKED = new Object();
+
+    public SingleInterceptorCollaborator(ComponentMetadata cm, Interceptor interceptor) {
+        this.cm = cm;
+        this.interceptor = interceptor;
+    }
+
+    /**
+     * Invoke the preCall method on the interceptor
+     * 
+     * @param o
+     *            : The Object being invoked
+     * @param m
+     *            : method
+     * @param parameters
+     *            : method paramters
+     * @throws Throwable
+     */
+    public Object preInvoke(Object o, Method m, Object[] parameters)
+            throws Throwable {
+        Object callToken = NON_INVOKED;
+        try {
+              callToken = interceptor.preCall(cm, m, parameters);
+        } catch (Throwable t) {
+            postInvokeExceptionalReturn(callToken, o, m, t);
+            throw t;
+        }
+        return callToken;
+    }
+
+    /**
+     * Called when the method is called and returned normally
+     * 
+     * @param cm
+     *            : component metadata
+     * @param method
+     *            : method
+     * @param returnType
+     *            : return type
+     * @throws Throwable
+     */
+    public void postInvoke(Object token, Object o, Method method, 
+         Object returnType) throws Throwable {
+
+        if(token != NON_INVOKED) {
+            try {
+                interceptor.postCallWithReturn(cm, method, returnType, token);
+            } catch (Throwable t) {
+                LOGGER.debug("postCallInterceptorWithReturn", t);
+                throw t;
+            }
+        }
+    }
+
+    /**
+     * Called when the method is called and returned with an exception
+     * 
+     * @param cm
+     *            : component metadata
+     * @param method
+     *            : method
+     * @param exception
+     *            : exception thrown
+     */
+    public void postInvokeExceptionalReturn(Object token, Object o, Method method,
+                 Throwable exception) throws Throwable {
+        try {
+            interceptor.postCallWithException(cm, method, exception, token);
+        } catch (Throwable t) {
+            // log the exception
+            LOGGER.debug("postCallInterceptorWithException", t);
+            throw t;
+        }
+    }
+}
