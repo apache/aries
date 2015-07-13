14ceeb149cd56e4dd377baec423e5d3bb63eaf99
Timothy James Ward <timothyjward@apache.org>
2015-07-13 15:18:13 +0000

    Add ASL v2 headers to the async service
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk@1690716 13f79535-47bb-0310-9956-ffa450edef68

--- async/promise-api/src/test/java/org/apache/aries/async/promise/test/PromisesTest.java
+++ async/promise-api/src/test/java/org/apache/aries/async/promise/test/PromisesTest.java
@@ -1,16 +1,36 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.aries.async.promise.test;
 
-import org.osgi.util.promise.Deferred;
-import org.osgi.util.promise.FailedPromisesException;
-import org.osgi.util.promise.Promise;
-import org.osgi.util.promise.Promises;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
+import org.osgi.util.promise.Deferred;
+import org.osgi.util.promise.FailedPromisesException;
+import org.osgi.util.promise.Promise;
+import org.osgi.util.promise.Promises;
 
 public class PromisesTest {
 
@@ -57,7 +77,6 @@ public class PromisesTest {
 
         Promise<List<String>> latch = null;
         if (!preResolve) {
-            @SuppressWarnings("unchecked")
             Promise<List<String>> latch2 = Promises.all(pv);
             latch = latch2;
 
@@ -80,7 +99,6 @@ public class PromisesTest {
         }
 
         if (preResolve) {
-            @SuppressWarnings("unchecked")
             Promise<List<String>> latch2 = Promises.all(pv);
             latch = latch2;
         }
