972fafca777163a58018569862e92c0a057f3803
Timothy James Ward <timothyjward@apache.org>
2015-07-13 15:18:13 +0000

    Add ASL v2 headers to the async service
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk@1690716 13f79535-47bb-0310-9956-ffa450edef68

--- async/promise-api/src/main/java/org/osgi/util/promise/Success.java
+++ async/promise-api/src/main/java/org/osgi/util/promise/Success.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) OSGi Alliance 2015. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.osgi.util.promise;
 
 /**
@@ -10,7 +25,7 @@ package org.osgi.util.promise;
  * @param <T> The value type of the resolved Promise passed as input to this callback.
  * @param <R> The value type of the returned Promise from this callback.
  */
-//@org.osgi.annotation.versioning.ConsumerType
+@org.osgi.annotation.versioning.ConsumerType
 public interface Success<T,R> {
     /**
      * Success callback for a Promise.
