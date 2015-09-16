ae050beef408113cc9394f5feb19275c1454831f
Christian Schneider <cschneider@apache.org>
2015-09-16 14:16:54 +0000

    [ARIES-1405] Only use blueprint service proxies
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk/jpa@1703414 13f79535-47bb-0310-9956-ffa450edef68

--- jpa-blueprint/src/main/java/org/apache/aries/jpa/blueprint/impl/AnnotationScanner.java
+++ jpa-blueprint/src/main/java/org/apache/aries/jpa/blueprint/impl/AnnotationScanner.java
@@ -1,48 +1,48 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.aries.jpa.blueprint.impl;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.AccessibleObject;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.PersistenceContext;
-import javax.persistence.PersistenceUnit;
-
-import org.apache.aries.jpa.supplier.EmSupplier;
-
 public class AnnotationScanner {
-    private final List<Class<?>> managedJpaClasses;
-    
-    public AnnotationScanner() {
-        managedJpaClasses = Arrays.asList(EntityManagerFactory.class, EntityManager.class, EmSupplier.class);
-    }
-    
-    public List<AccessibleObject> getJpaAnnotatedMembers(Class<?> c) {
+
+    public List<AccessibleObject> getJpaAnnotatedMembers(Class<?> c, Class<? extends Annotation> annotation) {
         final List<AccessibleObject> jpaAnnotated = new ArrayList<AccessibleObject>();
 
         Class<?> cl = c;
         if (c != Object.class) {
             while (cl != Object.class) {
                 for (Field field : cl.getDeclaredFields()) {
-                    if (field.getAnnotation(PersistenceContext.class) != null
-                        || field.getAnnotation(PersistenceUnit.class) != null) {
+                    if (field.isAnnotationPresent(annotation)) {
+                        field.setAccessible(true);
                         jpaAnnotated.add(field);
                     }
                 }
 
                 for (Method method : cl.getDeclaredMethods()) {
-                    if (method.getAnnotation(PersistenceContext.class) != null
-                        || method.getAnnotation(PersistenceUnit.class) != null) {
-
-                        Class<?>[] pType = method.getParameterTypes();
-                        if (method.getName().startsWith("set") && pType.length == 1
-                            && managedJpaClasses.contains(pType[0])) {
-                            jpaAnnotated.add(method);
-                        }
+                    if ((method.isAnnotationPresent(annotation)) && method.getName().startsWith("set") && method.getParameterTypes().length == 1) {
+                        jpaAnnotated.add(method);
                     }
                 }
 
@@ -51,5 +51,28 @@ public class AnnotationScanner {
         }
 
         return jpaAnnotated;
+    }
+
+    public String getName(AccessibleObject member) {
+        if (member instanceof Field) {
+            return ((Field)member).getName();
+        } else if (member instanceof Method) {
+            Method method = (Method)member;
+            String name = method.getName();
+            if (!name.startsWith("set")) {
+                return null;
+            }
+            return name. substring(3, 4).toLowerCase() + name.substring(4);
+        }
+        throw new IllegalArgumentException();
+    }
+    
+    public Class<?> getType(AccessibleObject member) {
+        if (member instanceof Field) {
+            return ((Field)member).getType();
+        } else if (member instanceof Method) {
+            return ((Method)member).getParameterTypes()[0];
+        }
+        throw new IllegalArgumentException();
     }
 }
