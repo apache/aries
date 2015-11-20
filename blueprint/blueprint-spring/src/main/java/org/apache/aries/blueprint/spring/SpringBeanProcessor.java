0dc65c011f1ab70768e78343e127af6942047c9e
Guillaume Nodet <gnodet@apache.org>
2015-11-20 16:45:33 +0000

    [ARIES-1456] Support spring beans and spring custom namespaces (work in progress)
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk@1715391 13f79535-47bb-0310-9956-ffa450edef68

---
+++ blueprint/blueprint-spring/src/main/java/org/apache/aries/blueprint/spring/SpringBeanProcessor.java
@@ -0,0 +1,124 @@
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.aries.blueprint.spring;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.aries.blueprint.BeanProcessor;
+import org.apache.aries.blueprint.ComponentDefinitionRegistry;
+import org.apache.aries.blueprint.ComponentDefinitionRegistryProcessor;
+import org.apache.aries.blueprint.ExtendedBeanMetadata;
+import org.apache.aries.blueprint.services.ExtendedBlueprintContainer;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.wiring.BundleWiring;
+import org.osgi.service.blueprint.reflect.BeanMetadata;
+import org.springframework.beans.factory.Aware;
+import org.springframework.beans.factory.BeanClassLoaderAware;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.beans.factory.config.DestructionAwareBeanPostProcessor;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.support.AbstractApplicationContext;
+
+public class SpringBeanProcessor implements BeanProcessor, ComponentDefinitionRegistryProcessor {
+
+    private final BundleContext bundleContext;
+    private final ExtendedBlueprintContainer blueprintContainer;
+    private final SpringApplicationContext applicationContext;
+    boolean creatingProcessor;
+
+    public SpringBeanProcessor(
+            BundleContext bundleContext,
+            ExtendedBlueprintContainer blueprintContainer,
+            SpringApplicationContext applicationContext) {
+        this.bundleContext = bundleContext;
+        this.blueprintContainer = blueprintContainer;
+        this.applicationContext = applicationContext;
+    }
+
+    @Override
+    public void process(ComponentDefinitionRegistry componentDefinitionRegistry) {
+        applicationContext.process();
+    }
+
+    @Override
+    public Object beforeInit(Object o, String s, BeanCreator beanCreator, BeanMetadata beanMetadata) {
+        if (o instanceof Aware) {
+            if (o instanceof BeanNameAware) {
+                ((BeanNameAware) o).setBeanName(s);
+            }
+            if (o instanceof BeanClassLoaderAware) {
+                ClassLoader cl = bundleContext.getBundle().adapt(BundleWiring.class).getClassLoader();
+                ((BeanClassLoaderAware) o).setBeanClassLoader(cl);
+            }
+            if (o instanceof BeanFactoryAware) {
+                ((BeanFactoryAware) o).setBeanFactory(applicationContext.getBeanFactory());
+            }
+        }
+        return applicationContext.getBeanFactory().applyBeanPostProcessorsBeforeInitialization(o, s);
+    }
+
+    @Override
+    public Object afterInit(Object o, String s, BeanCreator beanCreator, BeanMetadata beanMetadata) {
+        return applicationContext.getBeanFactory().applyBeanPostProcessorsAfterInitialization(o, s);
+    }
+
+    @Override
+    public void beforeDestroy(Object o, String s) {
+        for (BeanPostProcessor processor : applicationContext.getBeanFactory().getBeanPostProcessors()) {
+            if (processor instanceof DestructionAwareBeanPostProcessor) {
+                ((DestructionAwareBeanPostProcessor) processor).postProcessBeforeDestruction(o, s);
+            }
+        }
+    }
+
+    @Override
+    public void afterDestroy(Object o, String s) {
+    }
+
+    private <T> List<T> getProcessors(Class<T> type) {
+        List<T> processors = new ArrayList<T>();
+        if (!creatingProcessor) {
+            creatingProcessor = true;
+            for (BeanMetadata bean : blueprintContainer.getMetadata(BeanMetadata.class)) {
+                Class clazz = null;
+                if (bean instanceof ExtendedBeanMetadata) {
+                    clazz = ((ExtendedBeanMetadata) bean).getRuntimeClass();
+                }
+                if (clazz == null && bean.getClassName() != null) {
+                    try {
+                        clazz = bundleContext.getBundle().loadClass(bean.getClassName());
+                    } catch (ClassNotFoundException e) {
+                    }
+                }
+                if (clazz == null) {
+                    continue;
+                }
+                if (type.isAssignableFrom(clazz)) {
+                    Object p = blueprintContainer.getComponentInstance(bean.getId());
+                    processors.add(type.cast(p));
+                }
+            }
+            creatingProcessor = false;
+        }
+        return processors;
+    }
+
+}
---
+++ blueprint/blueprint-spring/src/main/java/org/apache/aries/blueprint/spring/SpringExtension.java
@@ -0,0 +1,116 @@
+/**
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.aries.blueprint.spring;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import org.apache.aries.blueprint.NamespaceHandler;
+import org.apache.felix.utils.extender.Extension;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceRegistration;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.BeanDefinitionParserDelegate;
+
+/**
+ * Spring extension.
+ * Each spring namespace is wrapped in a blueprint namespace.
+ *
+ * @see BlueprintNamespaceHandler
+ */
+public class SpringExtension implements Extension {
+
+    public static final String SPRING_HANDLERS = "META-INF/spring.handlers";
+    public static final String SPRING_SCHEMAS = "META-INF/spring.schemas";
+
+    private final Bundle bundle;
+    private final List<ServiceRegistration<NamespaceHandler>> registrations;
+
+    public SpringExtension(Bundle bundle) {
+        this.bundle = bundle;
+        this.registrations = new ArrayList<ServiceRegistration<NamespaceHandler>>();
+    }
+
+    @Override
+    public void start() throws Exception {
+        Map<String, NamespaceHandler> handlers = new HashMap<String, NamespaceHandler>();
+        Properties props = loadSpringHandlers();
+        Properties schemas = loadSpringSchemas();
+        for (String key : props.stringPropertyNames()) {
+            String clazzName = props.getProperty(key);
+            org.springframework.beans.factory.xml.NamespaceHandler springHandler
+                    = (org.springframework.beans.factory.xml.NamespaceHandler) bundle.loadClass(clazzName).newInstance();
+            springHandler.init();
+            NamespaceHandler wrapper = new BlueprintNamespaceHandler(bundle, schemas, springHandler);
+            handlers.put(key, wrapper);
+        }
+        if (bundle == FrameworkUtil.getBundle(BeanFactory.class)) {
+            org.springframework.beans.factory.xml.NamespaceHandler springHandler
+                    = new BeansNamespaceHandler();
+            NamespaceHandler wrapper = new BlueprintNamespaceHandler(bundle, schemas, springHandler);
+            handlers.put(BeanDefinitionParserDelegate.BEANS_NAMESPACE_URI, wrapper);
+        }
+        for (Map.Entry<String, NamespaceHandler> entry : handlers.entrySet()) {
+            Hashtable<String, String> svcProps = new Hashtable<String, String>();
+            svcProps.put("osgi.service.blueprint.namespace", entry.getKey());
+            ServiceRegistration<NamespaceHandler> reg =
+                    bundle.getBundleContext().registerService(NamespaceHandler.class, entry.getValue(),
+                            svcProps);
+            registrations.add(reg);
+        }
+    }
+
+    private Properties loadSpringHandlers() throws IOException {
+        Properties props = new Properties();
+        URL url = bundle.getResource(SPRING_HANDLERS);
+        InputStream is = url.openStream();
+        try {
+            props.load(is);
+        } finally {
+            is.close();
+        }
+        return props;
+    }
+
+    private Properties loadSpringSchemas() throws IOException {
+        Properties props = new Properties();
+        URL url = bundle.getResource(SPRING_SCHEMAS);
+        InputStream is = url.openStream();
+        try {
+            props.load(is);
+        } finally {
+            is.close();
+        }
+        return props;
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        for (ServiceRegistration<NamespaceHandler> reg : registrations) {
+            reg.unregister();
+        }
+    }
+
+}
