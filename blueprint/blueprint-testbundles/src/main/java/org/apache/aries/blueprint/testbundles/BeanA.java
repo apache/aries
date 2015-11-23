41714143e120b95302cb905326a7492ed8a73f5a
Guillaume Nodet <gnodet@apache.org>
2015-11-23 13:54:50 +0000

    [ARIES-1456] Extract the blueprint spring test in its own bundle
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk@1715814 13f79535-47bb-0310-9956-ffa450edef68

--- blueprint/blueprint-testbundles/src/main/java/org/apache/aries/blueprint/testbundles/BeanA.java
+++ blueprint/blueprint-testbundles/src/main/java/org/apache/aries/blueprint/testbundles/BeanA.java
@@ -14,7 +14,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package org.apache.aries.blueprint.spring.pojos;
+package org.apache.aries.blueprint.testbundles;
 
 public class BeanA {
 
