24f3aa281734b6c8e76b3a6b7d100919b9ac91f0
A. J. David Bosschaert <davidb@apache.org>
2015-11-30 17:35:35 +0000

    ARIES-1461 Support for SPI Fly headers added via fragments.
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk/spi-fly/spi-fly-dynamic-bundle@1717300 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/aries/spifly/dynamic/ClientWeavingHookGenericCapabilityTest.java
+++ src/test/java/org/apache/aries/spifly/dynamic/ClientWeavingHookGenericCapabilityTest.java
@@ -57,6 +57,7 @@ import org.osgi.framework.BundleReference;
 import org.osgi.framework.Version;
 import org.osgi.framework.hooks.weaving.WeavingHook;
 import org.osgi.framework.hooks.weaving.WovenClass;
+import org.osgi.framework.wiring.BundleRevision;
 import org.osgi.framework.wiring.BundleWiring;
 
 public class ClientWeavingHookGenericCapabilityTest {
@@ -664,6 +665,7 @@ public class ClientWeavingHookGenericCapabilityTest {
         EasyMock.expect(consumerBundle.getHeaders()).andReturn(headers).anyTimes();
         EasyMock.expect(consumerBundle.getBundleContext()).andReturn(bc).anyTimes();
         EasyMock.expect(consumerBundle.getBundleId()).andReturn(Long.MAX_VALUE).anyTimes();
+        EasyMock.expect(consumerBundle.adapt(BundleRevision.class)).andReturn(null).anyTimes();
         EasyMock.replay(consumerBundle);
 
         List<Bundle> allBundles = new ArrayList<Bundle>(Arrays.asList(otherBundles));
