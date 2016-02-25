64c31b6e8f7107dae3e00c9fea2487421891a255
Timothy James Ward <timothyjward@apache.org>
2016-02-25 14:08:14 +0000

    [tx-control] Testing of nested transactions
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk/tx-control@1732308 13f79535-47bb-0310-9956-ffa450edef68

--- tx-control-itests/src/test/java/org/apache/aries/tx/control/itests/SimpleTransactionTest.java
+++ tx-control-itests/src/test/java/org/apache/aries/tx/control/itests/SimpleTransactionTest.java
@@ -29,6 +29,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Arrays;
 import java.util.Properties;
 
 import javax.inject.Inject;
@@ -118,6 +119,105 @@ public class SimpleTransactionTest extends AbstractIntegrationTest {
 			rs.next();
 			return rs.getInt(1);
 		}));
+	}
+
+	@Test
+	public void testNestedTx() {
+		txControl.required(() -> {
+			connection.createStatement()
+				.execute("Insert into TEST_TABLE values ( 'Hello World!' )");
+			
+			txControl.requiresNew(() -> connection.createStatement()
+					.execute("Insert into TEST_TABLE values ( 'Hello Nested World!' )"));
+			
+			return null;
+		});
+		
+		String[] results = txControl.notSupported(() -> {
+				Statement s = connection.createStatement();
+				
+				ResultSet rs = s.executeQuery("Select count(*) from TEST_TABLE");
+				rs.next();
+				int count = rs.getInt(1);
+				
+				rs = s.executeQuery("Select message from TEST_TABLE ORDER BY message");
+				
+				String[] result = new String[2];
+				rs.next();
+				result[0] = "" + count + ": " + rs.getString(1);
+				rs.next();
+				result[1] = "" + count + ": " + rs.getString(1);
+				return result;
+			});
+		
+		System.out.println(Arrays.toString(results));
+		
+		assertEquals("2: Hello Nested World!", results[0]);
+		assertEquals("2: Hello World!", results[1]);
+	}
+	
+	@Test
+	public void testNestedTxOuterRollback() {
+		txControl.required(() -> {
+				// This will not end up in the database
+				connection.createStatement()
+					.execute("Insert into TEST_TABLE values ( 'Hello World!' )");
+				
+				// This should only apply to the current transaction level
+				txControl.setRollbackOnly();
+			
+				// This nested transaction will commit
+				txControl.requiresNew(() -> connection.createStatement()
+						.execute("Insert into TEST_TABLE values ( 'Hello Nested World!' )"));
+			
+				return null;
+			});
+		
+		assertEquals("1: Hello Nested World!", txControl.notSupported(() -> {
+				Statement s = connection.createStatement();
+				
+				ResultSet rs = s.executeQuery("Select count(*) from TEST_TABLE");
+				rs.next();
+				int count = rs.getInt(1);
+				
+				rs = s.executeQuery("Select message from TEST_TABLE ORDER BY message");
+				
+				rs.next();
+				return "" + count + ": " + rs.getString(1);
+			}));
+	}
+
+	@Test
+	public void testNestedTxInnerRollback() {
+		txControl.required(() -> {
+			// This will end up in the database
+			connection.createStatement()
+				.execute("Insert into TEST_TABLE values ( 'Hello World!' )");
+			
+						
+			// This nested transaction not commit
+			txControl.requiresNew(() -> {
+					connection.createStatement()
+						.execute("Insert into TEST_TABLE values ( 'Hello Nested World!' )");
+					txControl.setRollbackOnly();
+					return null;
+				});
+			
+			return null;
+		});
+		
+		assertEquals("1: Hello World!", txControl.notSupported(() -> {
+				Statement s = connection.createStatement();
+				
+				ResultSet rs = s.executeQuery("Select count(*) from TEST_TABLE");
+				rs.next();
+				int count = rs.getInt(1);
+				
+				rs = s.executeQuery("Select message from TEST_TABLE ORDER BY message");
+				
+				rs.next();
+				return "" + count + ": " + rs.getString(1);
+			}));
 	}
 
 	@Configuration
