378d0e57f0cc1b686fc2e5803a293e01d205d0ae
Timothy James Ward <timothyjward@apache.org>
2016-03-02 16:06:55 +0000

    [tx-control] Configuration-driven Database connections
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk/tx-control@1733320 13f79535-47bb-0310-9956-ffa450edef68

--- tx-control-itests/src/test/java/org/apache/aries/tx/control/itests/AbstractTransactionTest.java
+++ tx-control-itests/src/test/java/org/apache/aries/tx/control/itests/AbstractTransactionTest.java
@@ -24,9 +24,12 @@ import static org.ops4j.pax.exam.CoreOptions.options;
 import static org.ops4j.pax.exam.CoreOptions.systemProperty;
 import static org.ops4j.pax.exam.CoreOptions.when;
 
+import java.io.File;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Hashtable;
 import java.util.Properties;
 
 import javax.inject.Inject;
@@ -42,8 +45,10 @@ import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.PaxExam;
 import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerClass;
+import org.osgi.service.cm.ConfigurationAdmin;
 import org.osgi.service.jdbc.DataSourceFactory;
 import org.osgi.service.transaction.control.TransactionControl;
+import org.osgi.service.transaction.control.jdbc.JDBCConnectionProvider;
 import org.osgi.service.transaction.control.jdbc.JDBCConnectionProviderFactory;
 
 @RunWith(PaxExam.class)
@@ -51,16 +56,11 @@ import org.osgi.service.transaction.control.jdbc.JDBCConnectionProviderFactory;
 public abstract class AbstractTransactionTest extends AbstractIntegrationTest {
 
 	private static final String REMOTE_DB_PROPERTY = "org.apache.aries.tx.control.itests.remotedb";
+	private static final String CONFIGURED_PROVIDER_PROPERTY = "org.apache.aries.tx.control.itests.configured";
 
 	@Inject
 	protected TransactionControl txControl;
 
-	@Inject
-	JDBCConnectionProviderFactory resourceProviderFactory;
-	
-	@Inject
-	DataSourceFactory dsf;
-	
 	protected Connection connection;
 
 	private Server server;
@@ -69,21 +69,24 @@ public abstract class AbstractTransactionTest extends AbstractIntegrationTest {
 	public void setUp() throws Exception {
 		Properties jdbc = new Properties();
 		
-		Boolean external = Boolean.parseBoolean(REMOTE_DB_PROPERTY);
+		
+		boolean external = System.getProperties().containsKey(REMOTE_DB_PROPERTY);
 		
 		String jdbcUrl;
 		if(external) {
 			server = Server.createTcpServer("-tcpPort", "0");
 			server.start();
 			
-			jdbcUrl = "jdbc:h2:tcp://127.0.0.1:" + server.getPort() + "/target/test/db1";
+			jdbcUrl = "jdbc:h2:tcp://127.0.0.1:" + server.getPort() + "/" + System.getProperty(REMOTE_DB_PROPERTY);
 		} else {
 			jdbcUrl = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
 		}
 		
 		jdbc.setProperty(DataSourceFactory.JDBC_URL, jdbcUrl);
 		
-		connection = resourceProviderFactory.getProviderFor(dsf, jdbc, null).getResource(txControl);
+		boolean configuredProvider = Boolean.getBoolean(CONFIGURED_PROVIDER_PROPERTY);
+		
+		connection = configuredProvider ? configuredConnection(jdbc) : programaticConnection(jdbc);
 		
 		txControl.required(() -> {
 				Statement s = connection.createStatement();
@@ -94,6 +97,28 @@ public abstract class AbstractTransactionTest extends AbstractIntegrationTest {
 				return null;
 			});
 	}
+
+	private Connection programaticConnection(Properties jdbc) {
+		
+		JDBCConnectionProviderFactory resourceProviderFactory = context()
+				.getService(JDBCConnectionProviderFactory.class, 5000);
+		
+		DataSourceFactory dsf = context().getService(DataSourceFactory.class, 5000);
+		
+		return resourceProviderFactory.getProviderFor(dsf, jdbc, null).getResource(txControl);
+	}
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	private Connection configuredConnection(Properties jdbc) throws IOException {
+		
+		jdbc.setProperty(DataSourceFactory.OSGI_JDBC_DRIVER_CLASS, "org.h2.Driver");
+		ConfigurationAdmin cm = context().getService(ConfigurationAdmin.class, 5000);
+		
+		org.osgi.service.cm.Configuration config = cm.createFactoryConfiguration("org.apache.aries.tx.control.jdbc.local", null);
+		config.update((Hashtable)jdbc);
+		
+		return context().getService(JDBCConnectionProvider.class, 5000).getResource(txControl);
+	}
 	
 	@After
 	public void tearDown() {
@@ -101,8 +126,40 @@ public abstract class AbstractTransactionTest extends AbstractIntegrationTest {
 		txControl.required(() -> connection.createStatement()
 				.execute("DROP TABLE TEST_TABLE"));
 		
+		
+		if(Boolean.getBoolean(CONFIGURED_PROVIDER_PROPERTY)) {
+			clearConfiguration();
+		}
+		
 		if(server != null) {
 			server.stop();
+		}
+
+		connection = null;
+	}
+
+	private void clearConfiguration() {
+		ConfigurationAdmin cm = context().getService(ConfigurationAdmin.class, 5000);
+		org.osgi.service.cm.Configuration[] cfgs = null;
+		try {
+			cfgs = cm.listConfigurations(null);
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		if(cfgs != null) {
+			for(org.osgi.service.cm.Configuration cfg : cfgs) {
+				try {
+					cfg.delete();
+				} catch (Exception e) {}
+			}
+			try {
+				Thread.sleep(250);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 
@@ -125,11 +182,7 @@ public abstract class AbstractTransactionTest extends AbstractIntegrationTest {
 				mavenBundle("org.ops4j.pax.logging", "pax-logging-api").versionAsInProject(),
 				mavenBundle("org.ops4j.pax.logging", "pax-logging-service").versionAsInProject()
 				
-		/*
-		 * vmOption
-		 * ("-Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"),
-		 * waitForFrameworkStartup(),
-		 */
+//				,CoreOptions.vmOption("-Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005")
 		);
 	}
 
@@ -148,17 +201,50 @@ public abstract class AbstractTransactionTest extends AbstractIntegrationTest {
 				mavenBundle("org.apache.aries.testsupport", "org.apache.aries.testsupport.unit").versionAsInProject(),
 				localTxControlService(),
 				localJdbcResourceProviderWithH2(),
-				systemProperty(REMOTE_DB_PROPERTY).value("true"),
+				systemProperty(REMOTE_DB_PROPERTY).value(getRemoteDBPath()),
 				when(testSpecificOptions != null).useOptions(testSpecificOptions),
 				mavenBundle("org.ops4j.pax.logging", "pax-logging-api").versionAsInProject(),
 				mavenBundle("org.ops4j.pax.logging", "pax-logging-service").versionAsInProject()
 				
-				/*
-				 * vmOption
-				 * ("-Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"),
-				 * waitForFrameworkStartup(),
-				 */
+//				,CoreOptions.vmOption("-Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005")
 				);
+	}
+
+	@Configuration
+	public Option[] configAdminDrivenConfiguration() {
+		String localRepo = System.getProperty("maven.repo.local");
+		if (localRepo == null) {
+			localRepo = System.getProperty("org.ops4j.pax.url.mvn.localRepository");
+		}
+		
+		Option testSpecificOptions = testSpecificOptions();
+		
+		return options(junitBundles(), systemProperty("org.ops4j.pax.logging.DefaultServiceLog.level").value("INFO"),
+				when(localRepo != null)
+				.useOptions(CoreOptions.vmOption("-Dorg.ops4j.pax.url.mvn.localRepository=" + localRepo)),
+				mavenBundle("org.apache.aries.testsupport", "org.apache.aries.testsupport.unit").versionAsInProject(),
+				localTxControlService(),
+				localJdbcResourceProviderWithH2(),
+				systemProperty(REMOTE_DB_PROPERTY).value(getRemoteDBPath()),
+				mavenBundle("org.apache.felix", "org.apache.felix.configadmin").versionAsInProject(),
+				systemProperty(CONFIGURED_PROVIDER_PROPERTY).value("true"),
+				when(testSpecificOptions != null).useOptions(testSpecificOptions),
+				mavenBundle("org.ops4j.pax.logging", "pax-logging-api").versionAsInProject(),
+				mavenBundle("org.ops4j.pax.logging", "pax-logging-service").versionAsInProject()
+				
+//				,CoreOptions.vmOption("-Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005")
+				);
+	}
+
+	private String getRemoteDBPath() {
+		String fullResourceName = getClass().getName().replace('.', '/') + ".class";
+		
+		String resourcePath = getClass().getResource(getClass().getSimpleName() + ".class").getPath();
+		
+		File testClassesDir = new File(resourcePath.substring(0, resourcePath.length() - fullResourceName.length()));
+		
+		String dbPath = new File(testClassesDir.getParentFile(), "testdb/db1").getAbsolutePath();
+		return dbPath;
 	}
 	
 	public Option localTxControlService() {
