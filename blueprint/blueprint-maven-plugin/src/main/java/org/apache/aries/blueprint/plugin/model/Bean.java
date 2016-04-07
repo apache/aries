aeab80c35e5014b7627546fa3dcaca94f6d8b040
Christian Schneider <cschneider@apache.org>
2016-04-07 09:19:53 +0000

    [ARIES-1521] Inject constructor parameters without @Inject when class has only one constructor
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk@1738115 13f79535-47bb-0310-9956-ffa450edef68

--- blueprint/blueprint-maven-plugin/src/main/java/org/apache/aries/blueprint/plugin/model/Bean.java
+++ blueprint/blueprint-maven-plugin/src/main/java/org/apache/aries/blueprint/plugin/model/Bean.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * <p>
+ * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ * <p/>
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -90,10 +90,11 @@ public class Bean extends BeanRef {
     }
 
     private void resolveConstructorArguments(Matcher matcher) {
-        for (Constructor constructor : clazz.getDeclaredConstructors()) {
+        Constructor<?>[] declaredConstructors = clazz.getDeclaredConstructors();
+        for (Constructor constructor : declaredConstructors) {
             Annotation inject = constructor.getAnnotation(Inject.class);
             Annotation autowired = constructor.getAnnotation(Autowired.class);
-            if (inject != null || autowired != null) {
+            if (inject != null || autowired != null || declaredConstructors.length == 1) {
                 Class[] parameterTypes = constructor.getParameterTypes();
                 Annotation[][] parameterAnnotations = constructor.getParameterAnnotations();
                 for (int i = 0; i < parameterTypes.length; ++i) {
