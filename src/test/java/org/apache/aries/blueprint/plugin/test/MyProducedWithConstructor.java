5a3170144f413071cb4b727969c6795d57692703
Christian Schneider <cschneider@apache.org>
2016-05-02 08:54:36 +0000

    [ARIES-1531] Add arguments to produced bean based on factory method parameters
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk/blueprint/blueprint-maven-plugin@1741938 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/aries/blueprint/plugin/test/MyProducedWithConstructor.java
+++ src/test/java/org/apache/aries/blueprint/plugin/test/MyProducedWithConstructor.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * <p>
+ * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ * <p/>
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -19,26 +19,13 @@
 package org.apache.aries.blueprint.plugin.test;
 
 import org.ops4j.pax.cdi.api.OsgiService;
-import org.springframework.beans.factory.annotation.Value;
 
-import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.inject.Singleton;
 
-@Singleton
-public class MyFactoryBean {
-    
+public class MyProducedWithConstructor {
+
     @Inject
-    ServiceB serviceB;
-
-    @Produces
-    public MyProduced create() {
-        return new MyProduced("My message");
-    }
-
-    @Produces
-    public MyProducedWithConstructor createBeanWithParameters(MyBean1 myBean1, @Value("100") int bla, @OsgiService(filter = "myRef") @Named("ser1") ServiceC myReference) {
-        return new MyProducedWithConstructor(null);
+    public MyProducedWithConstructor(@OsgiService @Named("unknown") ServiceA serviceA) {
     }
 }
