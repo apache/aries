01f3b258520a12693bc4e96ef5708774c68f5673
Guillaume Nodet <gnodet@apache.org>
2016-09-02 08:43:30 +0000

    ARIES-1601 - Fix support for nested property placeholders
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk/blueprint/blueprint-core@1758901 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/aries/blueprint/ext/PropertyPlaceholderTest.java
+++ src/test/java/org/apache/aries/blueprint/ext/PropertyPlaceholderTest.java
@@ -38,6 +38,17 @@ public class PropertyPlaceholderTest extends PropertyPlaceholder {
         bind("prop2","world");
         bind("prop3","10");
         bind("prop4","20");
+        bind("prop5","nested");
+        bind("prop-nested","hello nested world!");
+        bind("prop-recursive-1","${prop-recursive-2}");
+        bind("prop-recursive-2","${prop-recursive-3}");
+        bind("prop-recursive-3","recursive-3");
+
+        bind("animal", "quick brown fox");
+        bind("target", "lazy dog");
+        bind("animal.1", "fox");
+        bind("animal.2", "mouse");
+        bind("species", "2");
     }
     
     @Test
@@ -50,6 +61,37 @@ public class PropertyPlaceholderTest extends PropertyPlaceholder {
     public void multipleProps() {
         sut = makeProperty("say ${prop1} ${prop2}");
         assertEquals("say hello world", sut.getStringValue());
+    }
+
+    @Test
+    public void nestedProps() {
+        sut = makeProperty("${prop-${prop5}}");
+        assertEquals("hello nested world!", sut.getStringValue());
+    }
+
+    @Test
+    public void nestedProps2() {
+        sut = makeProperty("The ${animal.${species}} jumps over the ${target}.");
+        assertEquals("The mouse jumps over the lazy dog.", sut.getStringValue());
+    }
+
+    @Test
+    public void nestedProps3() {
+        sut = makeProperty("The ${animal.${species}} jumps over the ${target}.");
+        bind("species", "1");
+        assertEquals("The fox jumps over the lazy dog.", sut.getStringValue());
+    }
+
+    @Test
+    public void recursiveProps() {
+        sut = makeProperty("${prop-recursive-1}");
+        assertEquals("recursive-3", sut.getStringValue());
+    }
+
+    @Test
+    public void plainText() {
+        sut = makeProperty("plain text");
+        assertEquals("plain text", sut.getStringValue());
     }
     
 //    @Test
