9b69e6757250583420d480e3bcc56e5bb175e80b
Felix Wassmer <felix.wassmer@traxpay.com>
2017-05-29 17:21:31 +0200

    ARIES-1690: improved isRollBackException lookup, since annotation attributes are not accessible in postcall

--- transaction/transaction-blueprint/src/test/java/org/apache/aries/transaction/ComponentTxDataTest.java
+++ transaction/transaction-blueprint/src/test/java/org/apache/aries/transaction/ComponentTxDataTest.java
@@ -87,7 +87,7 @@ public class ComponentTxDataTest {
                 throw new IllegalArgumentException(e1);
             }
         }
-        return txData.getEffectiveType(m);
+        return txData.getEffectiveType(m).getTxType();
     }
 
 }
