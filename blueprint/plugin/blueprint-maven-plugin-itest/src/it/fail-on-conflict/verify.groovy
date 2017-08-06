155d2bde6d8b6cf90bb521f3943db8ba0b60fd85
Dominik Przybysz <alien11689@apache.org>
2017-08-06 10:53:10 +0000

    [ARIES-1735] Detect beans with the same id, but different classes
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk@1804230 13f79535-47bb-0310-9956-ffa450edef68

---
+++ blueprint/plugin/blueprint-maven-plugin-itest/src/it/fail-on-conflict/verify.groovy
@@ -0,0 +1,24 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+def file = new File(basedir, 'target/generated-sources/blueprint/OSGI-INF/blueprint/autowire.xml')
+assert !file.exists()
+def log = new File(basedir, 'build.log')
+log.text.contains('Found two beans with id `t1`, but different classes: [p1.T1, p2.T1]')
+
