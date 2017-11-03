b88ff515a36d977b4e03615f9f3a208012b6c9bc
Carlos Sierra Andrés <csierra@apache.org>
2017-11-03 15:22:56 +0000

    [Component-DSL] Add refresher interface
    this allows users of the API to specify a predicate to trigger removing
    and adding of a CachingServiceReference when the underlying
    ServiceReference has been modified.
    
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk@1814214 13f79535-47bb-0310-9956-ffa450edef68

---
+++ component-dsl/component-dsl/src/main/java/org/apache/aries/osgi/functional/Refresher.java
@@ -0,0 +1,10 @@
+package org.apache.aries.osgi.functional;
+
+import java.util.function.Predicate;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+public interface Refresher<T> extends Predicate<T> {
+
+}
