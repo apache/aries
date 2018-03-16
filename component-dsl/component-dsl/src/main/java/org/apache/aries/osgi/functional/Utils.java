11167205ea3e51d6f5d1dce5a34ddb53bb7b7eac
Carlos Sierra Andrés <csierra@apache.org>
2018-03-16 16:32:43 +0000

    [Component-DSL] Implement accumulations using onlyLast
    This way can be reused to generate lists or maps
    
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk@1827015 13f79535-47bb-0310-9956-ffa450edef68

--- component-dsl/component-dsl/src/main/java/org/apache/aries/osgi/functional/Utils.java
+++ component-dsl/component-dsl/src/main/java/org/apache/aries/osgi/functional/Utils.java
@@ -1,11 +1,18 @@
 package org.apache.aries.osgi.functional;
 
-import org.apache.aries.osgi.functional.internal.AccumulateTransformer;
 import org.apache.aries.osgi.functional.internal.HighestRankingOSGi;
+import org.apache.aries.osgi.functional.internal.OnlyLastPublisher;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
+
+import static org.apache.aries.osgi.functional.OSGi.all;
+import static org.apache.aries.osgi.functional.OSGi.just;
+import static org.apache.aries.osgi.functional.OSGi.nothing;
 
 /**
  * @author Carlos Sierra Andrés
@@ -13,7 +20,39 @@ import java.util.function.Function;
 public interface Utils {
 
     static <T> OSGi<List<T>> accumulate(OSGi<T> program) {
-        return program.transform(new AccumulateTransformer<>());
+        return
+            just(ArrayList<T>::new).flatMap(list ->
+            all(
+                just(ArrayList<T>::new),
+                program.effects(list::add, list::remove).
+                then(just(() -> new ArrayList<>(list)
+                ))
+            ).transform(
+                op -> new OnlyLastPublisher(op, () -> new ArrayList<>(list)))
+            );
+    }
+
+    static <K, V, T extends Comparable<T>> OSGi<Map<K, V>> accumulateMap(
+        OSGi<T> program, Function<T, K> keyFun,
+        Function<T, V> valueFun) {
+
+        return just(HashMap::new).flatMap(map ->
+            all(
+                just(HashMap::new),
+                program.splitBy(
+                    keyFun,
+                    p -> highest(p, Comparator.naturalOrder(), q -> nothing())).
+                    effects(
+                        t -> map.put(keyFun.apply(t), valueFun.apply(t)),
+                        t -> map.remove(keyFun.apply(t))).
+                    then(just(() -> new HashMap<>(map)))
+            ).transform(
+                op -> new OnlyLastPublisher(op, () -> new HashMap<>(map)
+            )));
+    }
+
+    static <T> OSGi<T> onlyLast(OSGi<T> program) {
+        return program.transform(OnlyLastPublisher::new);
     }
 
     static <T> OSGi<T> highest(
