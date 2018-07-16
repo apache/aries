e560fb3212419dfcf730aa191d54e02168655d50
Raymond Augé <rotty3000@apache.org>
2018-07-16 20:03:38 +0000

    ARIES-1814 Solve the start order limitations with SPI Fly by changing the verification approach to rely on uses constraints instead of package attributes
    Signed-off-by: Raymond Auge <rotty3000@apache.org>
    
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk@1836064 13f79535-47bb-0310-9956-ffa450edef68

--- spi-fly/spi-fly-dynamic-bundle/src/test/java/org/apache/aries/spifly/dynamic/ClientWeavingHookOSGi43Test.java
+++ spi-fly/spi-fly-dynamic-bundle/src/test/java/org/apache/aries/spifly/dynamic/ClientWeavingHookOSGi43Test.java
@@ -96,10 +96,9 @@ public class ClientWeavingHookOSGi43Test  {
         Assert.assertEquals("Precondition", 0, wc.getDynamicImports().size());
         wh.weave(wc);
         Assert.assertEquals(1, wc.getDynamicImports().size());
-        String di1 = "org.apache.aries.spifly;bundle-symbolic-name=spifly;bundle-version=1.9.4";
-        String di2 = "org.apache.aries.spifly;bundle-version=1.9.4;bundle-symbolic-name=spifly";
+        String di1 = "org.apache.aries.spifly";
         String di = wc.getDynamicImports().get(0);
-        Assert.assertTrue("Weaving should have added a dynamic import", di1.equals(di) || di2.equals(di));
+        Assert.assertTrue("Weaving should have added a dynamic import", di1.equals(di));
 
         // Invoke the woven class and check that it properly sets the TCCL so that the
         // META-INF/services/org.apache.aries.mytest.MySPI file from impl1 is visible.
