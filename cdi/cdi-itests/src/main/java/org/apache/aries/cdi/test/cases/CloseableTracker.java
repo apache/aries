2354096246554cd37b87b0baff4ace5f4f6fbec9
Raymond Augé <rotty3000@apache.org>
2018-09-25 14:14:12 +0000

    [CDI] header and cleanup
    Signed-off-by: Raymond Auge <rotty3000@apache.org>
    
    git-svn-id: https://svn.apache.org/repos/asf/aries/trunk@1841936 13f79535-47bb-0310-9956-ffa450edef68

--- cdi/cdi-itests/src/main/java/org/apache/aries/cdi/test/cases/CloseableTracker.java
+++ cdi/cdi-itests/src/main/java/org/apache/aries/cdi/test/cases/CloseableTracker.java
@@ -1,3 +1,17 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.aries.cdi.test.cases;
 
 import org.osgi.framework.BundleContext;
@@ -7,22 +21,12 @@ import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
 public class CloseableTracker<S, T> extends ServiceTracker<S, T> implements AutoCloseable {
 
-	@SuppressWarnings("unused")
-	private final StackTraceElement[] caller;
-
 	public CloseableTracker(BundleContext context, Filter filter) {
 		super(context, filter, null);
-		caller = new Exception().getStackTrace();
 	}
 
 	public CloseableTracker(BundleContext context, Filter filter, ServiceTrackerCustomizer<S, T> customizer) {
 		super(context, filter, customizer);
-		caller = new Exception().getStackTrace();
-	}
-
-	@Override
-	public void close() {
-		super.close();
 	}
 
 }
