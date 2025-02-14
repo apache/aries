48fc9a5e4da4f4ae60378bf4d1de43eb1c13305b
Dominik Przybysz <alien11689@gmail.com>
2025-02-14 19:49:19 +0100

    [MAINTENANCE] Update asm and pax-url in web

--- web/web-itests/src/test/java/org/apache/aries/web/converter/itest/WabConverterITest.java
+++ web/web-itests/src/test/java/org/apache/aries/web/converter/itest/WabConverterITest.java
@@ -126,7 +126,10 @@ public class WabConverterITest extends AbstractIntegrationTest {
 				// Bundles
 				mavenBundle("org.apache.aries.web", "org.apache.aries.web.urlhandler"),
 				mavenBundle("org.apache.aries", "org.apache.aries.util"),
-				mavenBundle("org.ow2.asm", "asm-debug-all"),
+				mavenBundle("org.ow2.asm", "asm"),
+				mavenBundle("org.ow2.asm", "asm-commons"),
+				mavenBundle("org.ow2.asm", "asm-tree"),
+				mavenBundle("org.ow2.asm", "asm-analysis"),
 				mavenBundle("org.apache.aries.testsupport",	"org.apache.aries.testsupport.unit"));
 	}
 
