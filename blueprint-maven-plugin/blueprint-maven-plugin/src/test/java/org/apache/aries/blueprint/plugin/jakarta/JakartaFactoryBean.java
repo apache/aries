1592f5211057363415d7692d19faedcabde4f249
Dominik Przybysz <alien11689@gmail.com>
2025-03-10 20:10:47 +0100

    ARIES-2172: Support jakarta.enterprise.cdi-api in blueprint-maven-plugin

---
+++ blueprint-maven-plugin/blueprint-maven-plugin/src/test/java/org/apache/aries/blueprint/plugin/jakarta/JakartaFactoryBean.java
@@ -0,0 +1,42 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.aries.blueprint.plugin.jakarta;
+
+import jakarta.enterprise.inject.Produces;
+import jakarta.inject.Named;
+import jakarta.inject.Singleton;
+import org.apache.aries.blueprint.annotation.config.ConfigProperty;
+import org.apache.aries.blueprint.annotation.service.Reference;
+import org.apache.aries.blueprint.plugin.test.MyBean1;
+import org.apache.aries.blueprint.plugin.test.interfaces.ServiceC;
+
+@Singleton
+public class JakartaFactoryBean {
+
+    @Produces
+    public ProducedBean create() {
+        return new ProducedBean();
+    }
+
+    @Produces
+    @Named("namedProducedBean")
+    public ProducedBean createBeanWithParameters(MyBean1 myBean1, @ConfigProperty("100") int bla, @Reference @Named("refServiceC") ServiceC myReference) {
+        return new ProducedBean();
+    }
+}
