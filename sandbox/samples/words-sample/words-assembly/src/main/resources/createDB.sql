cdea378208b87dba78d61c150533bdaf0cda3918
Dominik Przybysz <alien11689@gmail.com>
2025-03-31 22:58:10 +0200

    ARIES-2176: Remove sandbox directory

--- sandbox/samples/words-sample/words-assembly/src/main/resources/createDB.sql
+++
@@ -1,4 +0,0 @@
-connect 'jdbc:derby:target/wordsDB/;create=true';
-CREATE TABLE Association (word VARCHAR(255) NOT NULL, associated VARCHAR(255), PRIMARY KEY (word));
-DELETE FROM Association;
-exit;
